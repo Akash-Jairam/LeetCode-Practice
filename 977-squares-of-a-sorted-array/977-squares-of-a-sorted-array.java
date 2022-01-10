e14d7c7a2520902566bcd05fa8cd4c437e7a6535
Akash Jairam <Quasar2197@gmail.com>
2022-01-10 09:36:54 -0600

    Time: 39 ms (7.73%), Space: 56.3 MB (5.67%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-       
-        
+        int index = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x,y) -> Integer.compare(x,y));
         for(int i = 0; i < nums.length; i++){
             pq.add(nums[i] * nums[i]);
         }
         
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = pq.remove();
+        while(!pq.isEmpty()){
+           nums[index] = pq.remove();
+           index++;
         }
         
         return nums;

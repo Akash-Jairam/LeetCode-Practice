7ec969568f969e880ca846cca20985166e3288f4
Akash Jairam <Quasar2197@gmail.com>
2022-01-18 16:31:55 -0600

    Time: 4 ms (33.27%), Space: 44.7 MB (30.09%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -15,38 +15,30 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> list = new ArrayList<Double>();
-        if(root == null)
-            return list;
-        
+        List<Double> list = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
+        
+        if(root == null){
+            return list;
+        }
+        
         queue.add(root);
-        double runningTotal = 0;
+        double sum = 0;
         
         while(!queue.isEmpty()){
-            // Gets the size of the queue before we loop over it
-            int size = queue.size();
-            
-            // Iterates over tree nodes over that level
-            for(int i = 0; i < size; ++i){
-                // Take the current node
-                TreeNode node = queue.poll();
-                runningTotal +=node.val;
-                
-                // Add the left and right nodes to the queue thereby increasing its size
-                if(node.left != null){
-                    queue.add(node.left);
-                }
-                
-                if(node.right != null){
-                    queue.add(node.right);
-                }
+        int size = queue.size();
+        for(int i = 0; i < size; ++i){
+            TreeNode node = queue.poll();
+            sum += node.val;
+            if(node.left != null){
+                queue.add(node.left);
             }
-            
-            // Determines the average of said level and resets the runningTotal
-            list.add(runningTotal * 1.0 / size);
-            runningTotal = 0;
-            
+            if(node.right != null){
+                queue.add(node.right);
+            }
+        }
+            list.add(sum / size);
+            sum = 0;
         }
         
         return list;

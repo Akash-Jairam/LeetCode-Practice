dc6dc09707c8398c7adbc483f03fee744aa13fad
Akash Jairam <Quasar2197@gmail.com>
2022-01-23 19:11:51 -0600

    Time: 4 ms (100.00%), Space: 41.5 MB (54.43%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,19 +10,19 @@
 class Solution {
     TreeNode lowestCommonAncestor = null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        findLowestCommonAncestor(root, p, q);
+        findLCA(root, p, q);
         return lowestCommonAncestor;
     }
     
-    public int findLowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q){
+    public int findLCA(TreeNode node, TreeNode p, TreeNode q){
         if(node == null)
             return 0;
         
-        int findCurrent = ((node. val == p.val) || node.val == q.val) ? 1 : 0;
-        int findLeft = findLowestCommonAncestor(node.left, p, q);
-        int findRight = findLowestCommonAncestor(node.right, p, q);
+        int findCurrent = (node.val == p.val || node.val == q.val) ? 1 : 0;
+        int findLeft = findLCA(node.left, p, q);
+        int findRight = findLCA(node.right, p, q);
         
-        if(findCurrent + findLeft + findRight >= 2 && lowestCommonAncestor == null){
+        if(lowestCommonAncestor == null && findCurrent + findLeft + findRight >= 2){
             lowestCommonAncestor = node;
         }
         

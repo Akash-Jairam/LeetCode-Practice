becf48422123cc171f73dc9dd2639b44a2744cfc
Akash Jairam <Quasar2197@gmail.com>
2022-01-24 20:29:05 -0600

    Time: 1 ms, Memory: 52.1 MB - LeetHub

---
+++ number-of-provinces/number-of-provinces.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+       int count = 0;
+       int[] visited = new int[isConnected.length];
+       for(int i = 0; i < visited.length; ++i){
+           if(visited[i] == 0){
+               count++;
+               dfs(isConnected, visited, i);
+           }
+       }
+        
+       return count;
+    }
+    
+    public void dfs(int[][] array, int[] visited, int i){
+        for(int j = 0; j < array.length; ++j){
+            if(array[i][j] == 1 && visited[j] == 0){
+                visited[j] = 1;
+                dfs(array, visited, j);
+            }
+        }
+    }
+}

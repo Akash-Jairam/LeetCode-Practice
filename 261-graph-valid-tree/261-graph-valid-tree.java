e32f4de8ee43efacda4e30155ac20e9f0777fb74
Akash Jairam <Quasar2197@gmail.com>
2022-01-26 18:05:36 -0600

    Time: 10 ms (15.70%), Space: 45.2 MB (12.87%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,38 +1,39 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        List<List<Integer>> adjacencyList = new ArrayList<>();
+        // Create and initialize adjacency matrix
+        // Loop through edges and add them to the adjacency matrix
+        
+        // Initialize a stack and a map
+        // Loop through the stack and compare the item to the adjacency matrix
+        // Push the neighbor to the stack and add the relationship to the adjacency matrix
+        List<List<Integer>> adjacencyMatrix = new ArrayList<>();
         
         for(int i = 0; i < n; ++i){
-            adjacencyList.add(new ArrayList<Integer>());
+            adjacencyMatrix.add(new ArrayList<>());
         }
         
         for(int[] edge : edges){
-            adjacencyList.get(edge[0]).add(edge[1]);
-            adjacencyList.get(edge[1]).add(edge[0]);
+            adjacencyMatrix.get(edge[0]).add(edge[1]);
+            adjacencyMatrix.get(edge[1]).add(edge[0]);
         }
         
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(0, -1);
         Stack<Integer> stack = new Stack<Integer>();
+        HashMap<Integer,  Integer> map = new HashMap<Integer, Integer>();
         stack.push(0);
+        map.put(0,-1);
         
         while(!stack.isEmpty()){
             int node = stack.pop();
-            for(int neighbor : adjacencyList.get(node)){
-                if(map.get(node) == neighbor)
+            for(int neighbor : adjacencyMatrix.get(node)){
+                if(neighbor == map.get(node))
                     continue;
-                
-                if(map.containsKey(neighbor)){
+                if(map.containsKey(neighbor))
                     return false;
-                }
-                
-                map.put(neighbor, node);
                 stack.push(neighbor);
+                map.put(neighbor, node);
             }
         }
         
         return map.size() == n;
     }
-    
-    
 }

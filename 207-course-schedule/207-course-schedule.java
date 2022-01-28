c8588956c2acd6f6c89f32a10544a998072bf14e
Akash Jairam <Quasar2197@gmail.com>
2022-01-28 10:49:03 -0600

    Time: 42 ms (8.16%), Space: 46.9 MB (15.46%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // Create an array to track the number of prerequisite courses
-        // Loop through that array to create a hashset with each course that does not have prerequisites
-        // If no courses do not have prerequisites, return false
-        //Iterate through each element of the hashset and Loop through the first array you created and decrement the occurence of courses before adding them to the hash set when their occurence reaches zero 
-        // If the values of the first array are not equal to zero, we return false.
-        // Else we return true
+        // Create an array of numCourses size and iterate through prerequisites to keep track of the courses that have prerequisites/dependencies/edges
+        // Create a hashset and iterate through the created array and add the courses that have no prerequisistes or dependencies to the hashmap
+        // If the hashset is empty, all the courses have dependencies so we return false
+        // Else take each element from the hashset and iterate through the created array and remove the edges/completing the dependencies. Add any element which has no more dependencies
+        // to our set
+        // Iterate through the created array and if any element is not equal to zero, we return false
         int[] inDegree = new int[numCourses];
         Set<Integer> zeroDegree = new HashSet();
         
@@ -28,7 +28,7 @@ class Solution {
             int course = it.next();
             zeroDegree.remove(course);
             for(int[] pre : prerequisites){
-                if(pre[1] == course){
+                if(course == pre[1]){
                     inDegree[pre[0]]--;
                     if(inDegree[pre[0]] == 0)
                         zeroDegree.add(pre[0]);
@@ -36,8 +36,8 @@ class Solution {
             }
         }
         
-        for(int num : inDegree){
-            if(num != 0)
+        for(int count : inDegree){
+            if(count != 0)
                 return false;
         }
         

7c0fa3d0326388a2dc9de571182c24344d9f64a3
Akash Jairam <Quasar2197@gmail.com>
2022-01-28 08:42:04 -0600

    Time: 20 ms (17.24%), Space: 42.5 MB (51.05%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        for(int[] prereq: prerequisites){
-            indegree[prereq[0]]++;
+        // Create an array to track the number of prerequisite courses
+        // Loop through that array to create a hashset with each course that does not have prerequisites
+        // If no courses do not have prerequisites, return false
+        //Iterate through each element of the hashset and Loop through the first array you created and decrement the occurence of courses before adding them to the hash set when their occurence reaches zero 
+        // If the values of the first array are not equal to zero, we return false.
+        // Else we return true
+        int[] inDegree = new int[numCourses];
+        Set<Integer> zeroDegree = new HashSet();
+        
+        for(int[] pre : prerequisites){
+            inDegree[pre[0]]++;
         }
         
-        Set<Integer> zeroDegree = new HashSet();
-        for(int i = 0; i < indegree.length; i++){
-            if(indegree[i] == 0)
+        for(int i = 0; i < numCourses; ++i){
+            if(inDegree[i] == 0){
                 zeroDegree.add(i);
+            }
         }
         
         if(zeroDegree.isEmpty()){
@@ -16,19 +24,19 @@ class Solution {
         }
         
         while(!zeroDegree.isEmpty()){
-            Iterator<Integer> iter = zeroDegree.iterator();
-            int course = iter.next();
+            Iterator<Integer> it = zeroDegree.iterator();
+            int course = it.next();
             zeroDegree.remove(course);
             for(int[] pre : prerequisites){
-                if(course == pre[1]){
-                    indegree[pre[0]]--;
-                    if(indegree[pre[0]] == 0)
+                if(pre[1] == course){
+                    inDegree[pre[0]]--;
+                    if(inDegree[pre[0]] == 0)
                         zeroDegree.add(pre[0]);
                 }
             }
         }
         
-        for(int num : indegree){
+        for(int num : inDegree){
             if(num != 0)
                 return false;
         }

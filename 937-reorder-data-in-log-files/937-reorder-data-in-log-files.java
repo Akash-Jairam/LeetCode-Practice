a2171b6d53806fc371f96ca9f5142764d34314da
Akash Jairam <Quasar2197@gmail.com>
2022-01-30 17:08:09 -0600

    Time: 4 ms (89.03%), Space: 46.1 MB (34.02%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,30 +1,30 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        Comparator<String> comp = new Comparator<String>(){
-            @Override public int compare(String log1, String log2){
-                int log1SpaceIndex = log1.indexOf(" ");
-                int log2SpaceIndex = log2.indexOf(" ");
-                char log1Char = log1.charAt(log1SpaceIndex+1);
-                char log2Char = log2.charAt(log2SpaceIndex+1);
-                boolean isDigit1 = Character.isDigit(log1Char);
-                boolean isDigit2 = Character.isDigit(log2Char);
+        Comparator<String> comp = new Comparator<>(){
+            @Override
+            public int compare(String log1, String log2){
+                int spacePos1 = log1.indexOf(" ");
+                int spacePos2 = log2.indexOf(" ");
+                int char1 = log1.charAt(spacePos1 + 1);
+                int char2 = log2.charAt(spacePos2 + 1);
+                
+                boolean isDigit1 = Character.isDigit(char1);
+                boolean isDigit2 = Character.isDigit(char2);
                 
                 if(!isDigit1 && !isDigit2){
-                    int comparer = log1.substring(log1SpaceIndex+1).compareTo(log2.substring(log2SpaceIndex+1));
-                    if(comparer != 0)
-                        return comparer;
-                    return log1.substring(0, log1SpaceIndex).compareTo(log2.substring(0, log2SpaceIndex));
-                } else if(isDigit1 && !isDigit2){
-                    return 1;
+                    int res = log1.substring(spacePos1 + 1).compareTo(log2.substring(spacePos2 + 1));
+                    if(res != 0)
+                        return res;
+                    return log1.substring(0, spacePos1).compareTo(log2.substring(0, spacePos2));
                 } else if(!isDigit1 && isDigit2){
                     return -1;
+                } else if(isDigit1 && !isDigit2){
+                    return 1;
                 } else{
-                    // Both characters are digit logs and maintain relative ordering
                     return 0;
                 }
             }
         };
-        
         Arrays.sort(logs, comp);
         return logs;
     }

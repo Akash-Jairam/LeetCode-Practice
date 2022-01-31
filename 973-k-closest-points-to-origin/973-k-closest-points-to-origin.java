0d798e10f599577c5f0c67e6460d8117737e3e40
Akash Jairam <Quasar2197@gmail.com>
2022-01-31 14:23:04 -0600

    Time: 21 ms (78.92%), Space: 50.4 MB (45.22%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (x,y) -> squaredDistance(x) - squaredDistance(y));
+        Arrays.sort(points, (x,y) -> findLength(x) - findLength(y));
         return Arrays.copyOf(points, k);
     }
     
-    private int squaredDistance(int[] array){
+    public int findLength(int[] array){
         return array[0] * array[0] + array[1] * array[1];
     }
 }

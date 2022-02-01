e0079037ab15bb57a531de9c27fc261a72a0632d
Akash Jairam <Quasar2197@gmail.com>
2022-02-01 17:20:55 -0600

    Time: 12 ms (28.93%), Space: 47.8 MB (5.35%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,77 +1,36 @@
 class Solution {
+    static Map<String, Integer> values = new HashMap<>();
+
+        static {
+            values.put("I", 1);
+            values.put("V", 5);
+            values.put("X", 10);
+            values.put("L", 50);
+            values.put("C", 100);
+            values.put("D", 500);
+            values.put("M", 1000);
+            values.put("IV", 4);
+            values.put("IX", 9);
+            values.put("XL", 40);
+            values.put("XC", 90);
+            values.put("CD", 400);
+            values.put("CM", 900);
+        }
     public int romanToInt(String s) {
-        if(s.length() == 0)
-            return -1;
-        else if(s.length() < 1 || s.length() > 15)
-            return -1;
-        s = s.toUpperCase();
-        int runningTotal = 0;
-        if(s.contains("IV")){
-            int removeLocation = s.indexOf("IV");
-            String s1 = s.substring(0, removeLocation);
-            String s2 = s.substring(removeLocation+2);
-            s =  s1+ s2;
-            runningTotal += 4;
-        }
-        
-        if(s.contains("CM")){
-            int removeLocation = s.indexOf("CM");
-            s = s.substring(0, removeLocation) + s.substring(removeLocation+2);
-            runningTotal += 900;
-        }
-        
-        if(s.contains("XC")){
-            int removeLocation = s.indexOf("XC");
-            s = s.substring(0, removeLocation) + s.substring(removeLocation+2);
-            runningTotal += 90;
-        }
-        
-        if(s.contains("XL")){
-            int removeLocation = s.indexOf("XL");
-            s = s.substring(0, removeLocation) + s.substring(removeLocation+2);
-            runningTotal += 40;
-        }
-        
-        if(s.contains("IX")){
-            int removeLocation = s.indexOf("IX");
-            s = s.substring(0, removeLocation) + s.substring(removeLocation+2);
-            runningTotal += 9;
-        }
-        
-        if(s.contains("CD")){
-            int removeLocation = s.indexOf("CD");
-            s = s.substring(0, removeLocation) + s.substring(removeLocation+2);
-            runningTotal += 400;
-        }
-        
-        
-        
-        for(int i = 0; i < s.length(); i++){
-            int currentVal = romanNumeralConvert(s.charAt(i));
-            if(currentVal == -1)
-                return -1;
-            else{
-                runningTotal += currentVal;
+        int sum = 0;
+        int i = 0;
+        while(i < s.length()){
+            if(i < s.length()-1){
+                String doubleSymbol = s.substring(i, i+2);
+                if(values.containsKey(doubleSymbol)){
+                    sum += values.get(doubleSymbol);
+                    i += 2;
+                    continue;
+                }
             }
+            sum += values.get(s.substring(i, i+1));
+            ++i;
         }
-        return runningTotal;
-    }
-    
-    public int romanNumeralConvert(char c){
-        if(c == 'I')
-            return 1;
-        else if(c == 'V')
-            return 5;
-        else if(c == 'X')
-            return 10;
-        else if(c == 'L')
-            return 50;
-        else if(c == 'C')
-            return 100;
-        else if(c == 'D')
-            return 500;
-        else if(c == 'M')
-            return 1000;
-        else return -1;
+        return sum;
     }
 }

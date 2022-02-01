4871c0356555777c3dc42726c4e3225b247542f5
Akash Jairam <Quasar2197@gmail.com>
2022-02-01 14:43:00 -0600

    Time: 4 ms, Memory: 44.4 MB - LeetHub

---
+++ minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
+        
+        for(char c : t.toCharArray()){
+            map[c]++;
+        }
+        
+        int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, count = t.length();
+        
+        while(end < s.length()){
+            final char c1 = s.charAt(end);
+            if(map[c1] > 0)
+                count--;
+            map[c1]--;
+            end++;
+            
+            while(count == 0){
+                if(minLen > end - start){
+                    minLen = end - start;
+                    minStart = start;
+                }
+                final char c2 = s.charAt(start);
+                if(map[c2] >= 0){
+                    count++;
+                }
+                map[c2]++;
+                start++;
+            }
+        }
+        
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+    }
+}

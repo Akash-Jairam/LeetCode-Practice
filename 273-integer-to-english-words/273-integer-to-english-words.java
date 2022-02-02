74220c7deb28fafbfa45567657a58b5f26384f50
Akash Jairam <Quasar2197@gmail.com>
2022-02-02 12:09:10 -0600

    Time: 15 ms (34.87%), Space: 42.6 MB (6.44%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.java
+++ 273-integer-to-english-words/273-integer-to-english-words.java
@@ -1,33 +1,26 @@
 class Solution {
     private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 
 public String numberToWords(int num) {
-    if (num == 0) return "Zero";
+    if (num == 0) return  "Zero";
+    return help(num);
+}
 
-    int i = 0;
-    String words = "";
-    
-    while (num > 0) {
-        if (num % 1000 != 0)
-    	    words = helper(num % 1000) +THOUSANDS[i] + " " + words;
-    	num /= 1000;
-    	i++;
+String help(int num) {
+        String ret = "";
+        if(num < 20)
+            ret = LESS_THAN_20[num];
+        else if(num < 100)
+            ret =TENS[num / 10] + " " + help(num % 10);
+        else if(num < 1000)
+            ret = help(num / 100) + " Hundred " + help(num % 100);
+        else if(num < 1000000)
+            ret= help(num / 1000) + " Thousand " + help(num % 1000);
+        else if(num < 1000000000)
+            ret =help(num / 1000000)+ " Million " + help(num % 1000000);
+        else 
+            ret = help(num / 1000000000)+ " Billion " + help(num % 1000000000);
+        return ret.trim();
     }
-    
-    return words.trim();
-}
-
-    
-    private String helper(int num) {
-    if(num == 0)
-        return "";
-    else if(num < 20)
-        return LESS_THAN_20[num] + " ";
-    else if(num < 100)
-        return TENS[num/10] + " " + helper(num%10);
-    else
-        return LESS_THAN_20[num / 100] + " Hundred " + helper(num%100); 
-}
 }

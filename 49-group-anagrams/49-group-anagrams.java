713e2915aee8ed5fc48fceaf67a8e3a911fbb449
Akash Jairam <Quasar2197@gmail.com>
2022-02-03 21:19:18 -0600

    Time: 8 ms (72.56%), Space: 45.8 MB (59.76%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,18 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        HashMap<String, List<String>> map = new HashMap<>();
+        // Create a map of String keys and list values and assign keys of the sorted characters of the string
+        // If a character is equal to that sorted character, we add it to that list
+        // Return our list
+        Map<String, List<String>> map = new HashMap<>();
         for(String str : strs){
             char[] array = str.toCharArray();
             Arrays.sort(array);
-            String s = new String(array);
-            if(!map.containsKey(s)){
-                map.put(s, new ArrayList<>());
-            }
-            map.get(s).add(str);
+            map.put(new String(array), new ArrayList<>());
         }
         
-       
+        for(String str: strs){
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String sortedString = new String(array);
+            if(map.containsKey(sortedString)){
+                map.get(sortedString).add(str);
+            }
+        }
+        
         return new ArrayList<>(map.values());
     }
 }

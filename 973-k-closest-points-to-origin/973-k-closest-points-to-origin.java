065214b5cab5f57b8c28ee7d4ba8e0b27d9d1b71
Akash Jairam <Quasar2197@gmail.com>
2022-02-03 21:28:24 -0600

    Time: 40 ms (42.17%), Space: 118.1 MB (5.18%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,10 +1,21 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (x,y) -> findLength(x) - findLength(y));
-        return Arrays.copyOf(points, k);
+        // Create a function to find the distance from k to the origin
+        // Priority queue take the distance as a lambda function and it will keep the closest at the top
+        int[][] result = new int[k][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> Integer.compare(findSquaredDistance(x[0], x[1]), findSquaredDistance(y[0],y[1])));
+        for(int[] point : points){
+            pq.offer(point);
+        }
+        
+        for(int i = 0; i < k; ++i){
+            result[i] = pq.remove();
+        }
+        
+        return result;
     }
     
-    public int findLength(int[] array){
-        return array[0] * array[0] + array[1] * array[1];
+    protected int findSquaredDistance(int x, int y){
+        return x * x + y *y;
     }
 }

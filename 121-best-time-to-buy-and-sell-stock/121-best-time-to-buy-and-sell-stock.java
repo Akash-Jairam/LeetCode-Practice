87ca61d72a7b0a489133f198088558cd247aed19
Akash Jairam <Quasar2197@gmail.com>
2022-02-09 09:35:39 -0600

    Time: 4 ms (32.09%), Space: 84.6 MB (41.69%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x,y) -> Integer.compare(x,y));
+        int min = prices[0];
         int profit = 0;
         
         for(int i = 0; i < prices.length; ++i){
-            if(!pq.isEmpty())
-                profit = Math.max(profit, prices[i] - pq.peek());
-            
-            pq.add(prices[i]);
+            profit = Math.max(profit, prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
         
        return profit;

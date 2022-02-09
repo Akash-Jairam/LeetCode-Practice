74f518b470356f53770cdf008f3ff56eaa314272
Akash Jairam <Quasar2197@gmail.com>
2022-02-08 20:10:28 -0600

    Time: 12 ms (16.67%), Space: 41.9 MB (12.12%) - LeetHub

--- 800-similar-rgb-color/800-similar-rgb-color.java
+++ 800-similar-rgb-color/800-similar-rgb-color.java
@@ -1,26 +1,23 @@
 class Solution {
     public String similarRGB(String color) {
-        // Valid shorthands are 0x00, 0x11, 0x22, 0x33... 0xee, 0xff
-        // First we will create our stringbuilder to return
-        // We will assign the # to that stringbuilder since the first character is always a #
-        // Iterate through our given string starting from position 1 and incrementing our iterator by 2 at the end of every loop
-        // Construct our hex character using the value at the current index plus the value at the next index and convert it to an int
-        // Find the upper and lower value by adding and subtracting a shorthand respectively
-        // To subtract by a shorthand, we just substract the int value by the int value mod 17
-        // To add by a shorthand, we add the int value minus the int value mod 17 plus 17
-        // We determine the closest shorthand by finding the difference between the int value and the upper value as well as the difference between the int value and the lower value and going with the lower or higher depending on which difference was lower
-        // We convert the closestshorthand to an int and we append it to our string
+        // Shorthand distances are 0x00, 0x11 ... 0xee, 0xff
+        // Take two elements at a time from the string and convert them to int
+        // Subtract by a shorthand distance and we add by a shorthand distance and store both of these in variables
+        // Find the difference between the converted value and our subtracted distance and do the same for our converted value and our added distance
+        // We compare the mod of these differences and go with the shorthand distance with the smallest difference
+        // We then convert that shorthand distance back to hex and append it to our string
+        // Return our string
         StringBuilder result = new StringBuilder(color.length());
         result.append("#");
         
         for(int i = 1; i < color.length()-1; i+=2){
             int hexToInt = Integer.parseInt(color.charAt(i) + "" + color.charAt(i+1), 16);
             
-            int lower = hexToInt - (hexToInt % 17);
-            int upper = (hexToInt - (hexToInt % 17)) + 17;
-            int closestShortHand = Math.abs(hexToInt - lower) < Math.abs(hexToInt - upper) ? lower : upper;
+            int lowerCandidate = hexToInt - (hexToInt % 17);
+            int higherCandidate = (hexToInt - (hexToInt % 17)) + 17;
+            int lowerShortHand = Math.abs(hexToInt - lowerCandidate) < Math.abs(hexToInt - higherCandidate) ? lowerCandidate : higherCandidate;
             
-            result.append(closestShortHand == 0 ? "00" : Integer.toHexString(closestShortHand));
+            result.append(lowerShortHand == 0 ? "00" : Integer.toHexString(lowerShortHand));
         }
         
         return result.toString();

94ea85de334d30c29c3cf60695ac78634a7e1b86
Akash Jairam <Quasar2197@gmail.com>
2022-02-08 18:05:18 -0600

    Time: 8 ms (22.73%), Space: 42.8 MB (7.57%) - LeetHub

--- 800-similar-rgb-color/800-similar-rgb-color.java
+++ 800-similar-rgb-color/800-similar-rgb-color.java
@@ -1,25 +1,28 @@
 class Solution {
     public String similarRGB(String color) {
-    StringBuilder sb = new StringBuilder(color.length());
-    sb.append("#");
-    for (int i = 1; i < color.length(); i += 2){
-        sb.append(getHexDigits(color.charAt(i), color.charAt(i + 1)));
+        // Valid shorthands are 0x00, 0x11, 0x22, 0x33... 0xee, 0xff
+        // First we will create our stringbuilder to return
+        // We will assign the # to that stringbuilder since the first character is always a #
+        // Iterate through our given string starting from position 1 and incrementing our iterator by 2 at the end of every loop
+        // Construct our hex character using the value at the current index plus the value at the next index and convert it to an int
+        // Find the upper and lower value by adding and subtracting a shorthand respectively
+        // To subtract by a shorthand, we just substract the int value by the int value mod 17
+        // To add by a shorthand, we add the int value minus the int value mod 17 plus 17
+        // We determine the closest shorthand by finding the difference between the int value and the upper value as well as the difference between the int value and the lower value and going with the lower or higher depending on which difference was lower
+        // We convert the closestshorthand to an int and we append it to our string
+        StringBuilder result = new StringBuilder(color.length());
+        result.append("#");
+        
+        for(int i = 1; i < color.length()-1; i+=2){
+            int hexToInt = Integer.parseInt(color.charAt(i) + "" + color.charAt(i+1), 16);
+            
+            int lower = hexToInt - (hexToInt % 17);
+            int upper = (hexToInt - (hexToInt % 17)) + 17;
+            int closestShortHand = Math.abs(hexToInt - lower) < Math.abs(hexToInt - upper) ? lower : upper;
+            
+            result.append(closestShortHand == 0 ? "00" : Integer.toHexString(closestShortHand));
+        }
+        
+        return result.toString();
     }
-    return sb.toString();
-}
-
-private String getHexDigits(char c1, char c2){
-    int d1 = Character.isDigit(c1)? c1 - '0': 10 + c1 - 'a';
-    int d2 = Character.isDigit(c2)? c2 - '0': 10 + c2 - 'a';
-    
-    int sum       = d1 * 16 + d2;
-    int index     = sum / 17; // [ 0x00(0) , 0x11(17), 0x22(34),  0x33(51), ........., 0xff(255) ]
-    int remainder = sum % 17;
-    if (remainder > 17 / 2){
-        index++;
-    }
-    
-    char c = 0 <= index && index <= 9? (char)('0' + index): (char)('a' + index - 10);
-    return String.valueOf(c) + String.valueOf(c);
-}
 }

252efff6f0ed4c949076d61212ae0821bafc701a
Akash Jairam <Quasar2197@gmail.com>
2022-02-11 08:28:05 -0600

    Time: 43 ms, Memory: 81 MB - LeetHub

--- fruit-into-baskets/fruit-into-baskets.java
+++ fruit-into-baskets/fruit-into-baskets.java
@@ -1,45 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        // Create a variable to track the current total number of fruits and the max
-        // Create a linked map
-        // Iterate through array
-        // If a tree is not in the map and the size of the map is equal to 2, remove the first item in the map and subtract its count from your current total
-        // Return the max total
-        // Create a variable to track the current total number of fruits and the max
-        // Create a linked map
-        // Iterate through array
-        // If a tree is not in the map and the size of the map is equal to 2, remove the first item in the map and subtract its count from your current total
-        // Return the max total
-        int left = 0;
-        int[] map = new int[fruits.length];
-        int runningTotal = 0;
-        int max = 0;
-        int basketCount = 0;
-
-        for (int right = 0; right < fruits.length; ++right) {
-            if (map[fruits[right]] == 0 && basketCount == 2) {
-                max = Math.max(runningTotal, max);
-                int lastIndexOfCharToRemove = -1;
-                int count = 0;
-                for(int i = left; i < right-1; ++i){
-                    if(fruits[i] != fruits[right-1]) {
-                        map[fruits[i]]--;
-                        lastIndexOfCharToRemove = i;
-                        map[fruits[right-1]] -= count;
-                        count = 0;
-                    } else
-                        count++;
-                }
-                left = lastIndexOfCharToRemove +1;
-                runningTotal = right-left;
-
-            } else if (map[fruits[right]] == 0) {
-                basketCount++;
+       // Create our max variable
+       // Create a map to store the last index of each type of fruit tree encountered
+       // Create two pointers and set them both to the first index of the array
+       // If the size of our map is less than or equal to 2, we put the current type of fruit tree and its index into our map and we increment our right pointer
+        // If the size of our map is greater than 2, we iterate through all the values in our map to get the lowest one and we remove that type of tree from our map
+        // Before our loop ends, we subtract the right and left pointers to get our max variable
+        int max = 0, left = 0, right = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while(right < fruits.length){
+            if(map.size() <= 2){
+                map.put(fruits[right], right);
+                right++;
             }
-            map[fruits[right]]++;
-            runningTotal++;
+            
+            if(map.size() > 2){
+                int min = fruits.length;
+                for(int tree : map.values()){
+                    min = Math.min(min, tree);
+                }
+                left = min + 1;
+                map.remove(fruits[min]);
+            }
+            max = Math.max(max, right - left);
         }
-
-        return Math.max(max, runningTotal);
+        return max;
     }
 }

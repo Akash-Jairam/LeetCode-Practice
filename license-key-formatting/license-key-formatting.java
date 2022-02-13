d4fce3019190e7a3cdb429fe7ac211bdbe825acc
Akash Jairam <Quasar2197@gmail.com>
2022-02-13 09:24:56 -0600

    Time: 1049 ms, Memory: 412.5 MB - LeetHub

--- license-key-formatting/license-key-formatting.java
+++ license-key-formatting/license-key-formatting.java
@@ -1,21 +1,28 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
-        // Divide string length by k to see if the size of the parts are uneven
-        // Iterate through the string from right to left. If the char at the current position 
+        // Create a stringbuilder that is of size s.length()
+        // Iterate through the string from right to left and use a variable to keep track of k
+        // If the character is a '-', we skip it
+        // Else we append the character to our resultstring
+        // We then decrement our variable and check to see if it is 0
+        // If so, we reset it to k and append a dash to our resultstring
+        // Return the string result
         String key = "";
+        char[] sChars = s.toCharArray();
         int count = k;
         
-        for(int i = s.length()-1; i >=0; --i){
-            if(s.charAt(i) != '-'){
-                if(count > 0)
-                    key = s.charAt(i) + "" + key;
-                else{
-                    count = k;
-                    key = s.charAt(i) + "-" + key;
-                }
+        for(int i = sChars.length-1; i >=0; --i){
+            if(sChars[i] != '-'){
+                key = sChars[i] + "" + key;
                 --count;
+                if(count == 0){
+                    count = k;
+                    key =  '-' + key;
+                }
             }
         }
+        if(key.length() > 1 && key.charAt(0) == '-' )
+            key = key.substring(1);
         
         return key.toUpperCase();
     }

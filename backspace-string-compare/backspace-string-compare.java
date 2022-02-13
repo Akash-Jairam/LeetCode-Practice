7107a77f6e407b2ebdf6b1b7f5f4ba3acf1a612b
Akash Jairam <Quasar2197@gmail.com>
2022-02-13 12:39:39 -0600

    Time: 10 ms, Memory: 42.5 MB - LeetHub

--- backspace-string-compare/backspace-string-compare.java
+++ backspace-string-compare/backspace-string-compare.java
@@ -1,17 +1,25 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return cleanString(s).equals(cleanString(t));
+        // Create a function that iterates through the characters in each string
+        // Create a stack which will hold our string
+        // We will iterate through each character in the given string
+        // If the character is a # we will pop the stack
+        // Else we will push the character to the stack
+        // We will convert the stack to a string and return it
+        // Compare the two returned strings and return the result
+        return backspace(s).equals(backspace(t));
     }
     
-    public String cleanString(String s){
+    public String backspace(String s){
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); ++i){
-            if(s.charAt(i) != '#')
-                stack.push(s.charAt(i));
-            else if(!stack.isEmpty())
-                stack.pop();
-        }
         
+        for(char c : s.toCharArray()){
+            if(c == '#' && !stack.isEmpty())
+                stack.pop();
+            else if(c != '#')
+                stack.push(c);
+        }
+        System.out.println(String.valueOf(stack));
         return String.valueOf(stack);
     }
 }

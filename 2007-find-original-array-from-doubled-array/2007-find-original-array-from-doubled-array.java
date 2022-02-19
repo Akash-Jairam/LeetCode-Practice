25a2e5839ba39a81be3beb94b4b5fe191dfe19bb
Akash Jairam <Quasar2197@gmail.com>
2022-02-19 16:48:29 -0600

    Time: 607 ms (8.09%), Space: 131.5 MB (39.79%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,29 +1,33 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
+        // Create a treeMap to track each number and its count
+        // put the elements of the array into the treemap while making sure to update the count if needed
+        // Iterate through the treemap's keys and check to see if the double of the key is in the map. If it is, decrement the occurrence of the double according to the occurence of the key. And add the key to the list for each decrement
+        // If not, return a blank array
+        // Convert the list to an array and return it.
         if(changed.length % 2 == 1)
             return new int[0];
-        
-        int index = 0;
-        int[] result = new int[changed.length /2];
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        
+        List<Integer> list = new ArrayList<>();
+        TreeMap<Integer, Integer> tree = new TreeMap<>();
         for(int num : changed){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            tree.put(num, tree.getOrDefault(num, 0) + 1);
         }
         
-        for(int key : map.keySet()){
-            if(map.get(key) > map.getOrDefault(key + key, 0))
+        for(int key : tree.keySet()){
+            if(tree.get(key) > tree.getOrDefault(key + key, 0))
                 return new int[0];
             
-            for(int i = 0; i < map.get(key); ++i){
-                result[index] = key;
-                index++;
-                map.put(key + key, map.get(key + key) - 1);
-                
+            for(int i = 0; i < tree.get(key); ++i){
+                tree.put(key + key, tree.get(key + key) -1);
+                list.add(key);
             }
         }
-            return result;
-
+        
+        int[] arr = new int[list.size()];
+        for(int i = 0; i < arr.length; ++i){
+            arr[i] = list.get(i);
+        }
+        
+        return arr;
     }
-    
 }

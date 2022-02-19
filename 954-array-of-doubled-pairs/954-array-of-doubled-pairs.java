bd8d26e8aacfe0290f941111bd9a1079346188d4
Akash Jairam <Quasar2197@gmail.com>
2022-02-19 11:30:17 -0600

    Time: 80 ms (42.95%), Space: 71.9 MB (50.34%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean canReorderDoubled(int[] arr) {
-        if(arr.length % 2 ==1)
-            return false;
-            
+        // Create a tree map and put all the numbers in there along with their count
+        // Iterate through the lowest keys in the tree map and decrement their counts
+        // If the number is less than zero, we should assume that it is doubled and we need to get the key of the number that is closer to zero
+        // If it is greater than zero, we check for the key os its doubled and reduce that count
+        // If the key's count is greater than the double's count we return false;
         TreeMap<Integer, Integer> tree = new TreeMap<>();
-        
         for(int num : arr){
             tree.put(num, tree.getOrDefault(num, 0) + 1);
         }
         
         for(int key : tree.keySet()){
-            if(tree.get(key) == 0)
-                continue;
-            int val = key < 0 ? key / 2 : key * 2;
-
-            if(key < 0 && key % 2 != 0 || tree.get(key) > tree.getOrDefault(val, 0))
+            if(key < 0){
+                if(tree.getOrDefault(key/2,0) < tree.get(key))
+                    return false;
+                else if(key % 2 == -1 && tree.get(key) != 0)
+                        return false;
+                else{
+                    
+                    for(int i = 0; i < tree.get(key); ++i){
+                        tree.put(key/2, tree.get(key/2) -1);
+                    }
+                }
+            }
+         else{
+            if(tree.get(key) > tree.getOrDefault(key + key, 0))
                 return false;
             
-            
-            tree.put(val, tree.get(val) - tree.get(key));
+            for(int i = 0; i < tree.get(key); ++i ){
+                tree.put(key + key, tree.get(key+key) -1);
+            }
+        }
         }
         
         return true;

09faec1c2fb9952023511b6fda75dca5853dbfd8
Akash Jairam <Quasar2197@gmail.com>
2022-02-21 16:42:19 -0600

    Time: 83 ms, Memory: 100.3 MB - LeetHub

--- fruit-into-baskets/fruit-into-baskets.java
+++ fruit-into-baskets/fruit-into-baskets.java
@@ -1,35 +1,36 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        // Create a map to keep track of each fruit and it's last index
-        // Create a variable to keep track of the max number of fruits
-        // Find some way to access each type of fruit in the map
-        // If we encounter a new fruit that is not in the map and that size of the map is 2
-        // We remove the type of fruit that was not at the tree before the new fruit and insert the new fruit and its index/
-        // Keep track of the last index of each fruit by updating the map
-        // At the end of each loop check to see if we can increase the max variable
-        // Return the max at the end of the program
+        // Create a map to keep track of each type of fruit in our baskets and their most recent index
+        // Iterate through our fruits array, while making sure to update the number of fruits we currently have 
+        // If we encounter a new type of tree that is not in our baskets, we iterate through our baskets and remove the type of tree  with the lowest index
+        // We then update our max value before subtracting the lowest index from our current fruit count
+        // We put the new type of fruit into our basket along with its index
         Map<Integer, Integer> map = new HashMap<>();
-        int max = -1;
-        int startIndex = 0;
+        int max = 0;
+        int count = 0;
         
         for(int i = 0; i < fruits.length; ++i){
-            if(!map.containsKey(fruits[i]) && map.size() == 2){
+            if(map.size() == 2 && !map.containsKey(fruits[i])){
                 int min = Integer.MAX_VALUE;
-                int key = -1;
-                for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-                    if(entry.getValue() < min){
-                        min = entry.getValue();
-                        key = entry.getKey();
+                int minKey = -1;
+                for(int key : map.keySet()){
+                    if(map.get(key) < min){
+                        min = map.get(key);
+                        minKey = key;
                     }
+                    
                 }
-                startIndex = min + 1;
-                map.remove(key);
+                max = Math.max(count, max);
+                if(map.get(minKey) == 0)
+                    count--;
+                count = i - map.get(minKey);
+                map.remove(minKey);
+                map.put(fruits[i], i);
+                continue;
             }
-            max = Math.max(max, i - startIndex + 1);
             map.put(fruits[i], i);
+            count++;
         }
-        
-        return max;
+        return Math.max(max, count);
     }
-    
 }

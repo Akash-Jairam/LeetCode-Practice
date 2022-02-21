ae5416e6f27584f6892ebb92d24683c5abc6920f
Akash Jairam <Quasar2197@gmail.com>
2022-02-21 15:55:06 -0600

    Time: 18 ms, Memory: 44.9 MB - LeetHub

--- license-key-formatting/license-key-formatting.java
+++ license-key-formatting/license-key-formatting.java
@@ -1,29 +1,29 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
-        // Create a stringbuilder that is of size s.length()
-        // Iterate through the string from right to left and use a variable to keep track of k
-        // If the character is a '-', we skip it
-        // Else we append the character to our resultstring
-        // We then decrement our variable and check to see if it is 0
-        // If so, we reset it to k and append a dash to our resultstring
-        // Return the string result
-        String key = "";
-        char[] sChars = s.toCharArray();
-        int count = k;
+        // Create a stringbuilder with s.length size to save space
+        // Starting from the right,iterate through each character in our string
+        // If it's not a '-' we append it
+        // If the stringbuilder length is divisible by k, we add a -
+        // Check to see if the first character in the sb is a dash, if so remove
+        // Return the sb converted to a string
+        char[] chars = s.toCharArray();
+        int numChars = 0;
+        StringBuilder sb = new StringBuilder(s.length());
         
-        for(int i = sChars.length-1; i >=0; --i){
-            if(sChars[i] != '-'){
-                key = sChars[i] + "" + key;
-                --count;
-                if(count == 0){
-                    count = k;
-                    key =  '-' + key;
-                }
+        for(int i = chars.length-1; i >= 0; --i){
+            if(chars[i] != '-'){
+                sb.append(chars[i]);
+                ++numChars;
+                if(numChars % k == 0)
+                    sb.append('-');
             }
         }
-        if(key.length() > 1 && key.charAt(0) == '-' )
-            key = key.substring(1);
         
-        return key.toUpperCase();
+        if(!sb.isEmpty() && sb.charAt(sb.length()-1) == '-')
+            sb.deleteCharAt(sb.length()-1);
+        
+
+        
+        return sb.reverse().toString().toUpperCase();
     }
 }

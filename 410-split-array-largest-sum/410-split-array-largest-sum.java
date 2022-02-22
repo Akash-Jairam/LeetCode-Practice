3c9a9fc17e0e73ab6c5bbeb4808e0f59e4a1fa03
Akash Jairam <Quasar2197@gmail.com>
2022-02-22 16:23:04 -0600

    Time: 0 ms (100.00%), Space: 39.8 MB (42.22%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,46 +1,41 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-        // The answer will be in the range of two cases
-        // The first is m = 1 where each element will be a subarray and the largest sum would be the largest element in the array
-        // The other case is where m == nums.length where the subarray will be the entire array and the total sum would be the largest element in the array
-        // Once we get these values, we can do a binary search where we find the midpoint between our two ranges and check to see if our array can be split up into m groups where the largest sum is equal to that midpoint
-        // If this is true, we bring the lowest point up to the point after the midpoint
-        // Else we bring the highest point down to the point before the midpoint
-        // We can return either the final lowest or highest point which should be equal
-        int min = -1;
-        int sum = 0;
-        
+        // Given that m can be the a single element in one edge and the entire length of the array in the other, we can get these values and perform a binary search
+        // We do this by cont
+        int low = -1;
+        int high = 0;
         for(int num : nums){
-            min = Math.max(min, num);
-            sum += num;
+            low = Math.max(low, num);
+            high += num;
         }
         
-        int low = min;
-        int high = sum;
+        int largestSum = 0;
         
         while(low < high){
-            int mid = low + (high - low) / 2;
-            if(canSplit(nums, mid, m)){
-                high = mid;
-            } else{
-                low = mid+1;
-            }
+            int midPoint = low + (high-low) /2;
+            boolean canReach = check(nums, m, midPoint);
+            if(canReach)
+                high = midPoint;
+            else
+                low = midPoint+1;
+            
+            
         }
         
         return low;
     }
     
-    public boolean canSplit(int[] nums, int largestSumCandidate, int m){
+    public boolean check(int[] nums, int m, int midpoint){
+        int count = 1;
         int sum = 0;
-        int numSubArrays = 1;
         
         for(int num : nums){
             sum += num;
-            if(sum > largestSumCandidate){
-                numSubArrays++;
-                if(numSubArrays > m)
-                    return false;
+            if(sum > midpoint){
+                ++count;
                 sum = num;
+                if(count > m)
+                    return false;
             }
         }
         

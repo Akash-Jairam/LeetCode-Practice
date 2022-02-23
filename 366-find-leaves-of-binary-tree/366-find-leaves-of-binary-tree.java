5eed7d01e72cbd49e71ce8bf6d2d25ac635644d0
Akash Jairam <Quasar2197@gmail.com>
2022-02-22 20:33:54 -0600

    Time: 0 ms (100.00%), Space: 41.3 MB (34.34%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -15,27 +15,29 @@
  */
 class Solution {
     public List<List<Integer>> findLeaves(TreeNode root) {
-        // Create a list of lists that will hold our results
-        // Call our recursive function and give the root and list of lists as parameters
-        // Return the result
         List<List<Integer>> result = new ArrayList<>();
-        find(root, result);
+        findLeaf(root, result);
+        
         return result;
     }
     
-    // If the root is null, return -1
-    // Level will be the greater of the recursive call to the left subnode and the right subnode + 1
-    // If the size of our result list is less than the level, add a new blank arraylist to our result list
-    // We get the current level and add the current node to it
-    public int find(TreeNode node, List<List<Integer>> result){
+    public int findLeaf(TreeNode node, List<List<Integer>> result){
         if(node == null)
             return -1;
         
-        int level = Math.max(find(node.left, result), find(node.right, result)) + 1;
+        int leftLevel = findLeaf(node.left, result);
+        int rightLevel = findLeaf(node.right, result);
+        
+        int level = Math.max(leftLevel, rightLevel) + 1;
+        
         if(result.size() <= level)
             result.add(new ArrayList<>());
         
+        
+        
         result.get(level).add(node.val);
-        return level;
+        
+        
+        return Math.max(leftLevel, rightLevel) + 1;
     }
 }

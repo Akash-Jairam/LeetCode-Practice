7bee5b2c08ade6bbbf7c8bc102d6fb3591a9693b
Akash Jairam <Quasar2197@gmail.com>
2022-02-28 22:06:35 -0600

    Time: 0 ms (100.00%), Space: 43.1 MB (7.12%) - LeetHub

--- 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
@@ -14,21 +14,24 @@
  * }
  */
 class Solution {
-    
     public int distributeCoins(TreeNode root) {
-        int[] steps = new int[]{0};
+        // Create an array to store our steps
+        // Call our dfs function
+        int[] steps = new int[] {0};
         distribute(root, steps);
         return steps[0];
     }
     
     public int distribute(TreeNode node, int[] steps){
+        // If the node is null, return 0
         if(node == null)
             return 0;
-        
-        int leftSteps = distribute(node.left, steps);
-        int rightSteps = distribute(node.right, steps);
-        
-        steps[0] += (Math.abs(leftSteps) + Math.abs(rightSteps));
-        return node.val + leftSteps + rightSteps -1;
+        // Find the number of excess coins / coins needed from both the left side and the right side
+        // Add to our array, the sum of the absolute values obtained from the left side and the right side
+        int findLeft = distribute(node.left, steps);
+        int findRight = distribute(node.right, steps);
+        // Return the count at the current node + the left side count + the right side count - 1 (indicates that the current node either has to keep 1 coin or needs 1 coin)
+        steps[0] += Math.abs(findLeft) + Math.abs(findRight);
+        return node.val + findLeft + findRight - 1;
     }
 }

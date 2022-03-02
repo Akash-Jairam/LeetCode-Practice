b5583ff17cfd5ecf4b009dcd8f01f557f785dd58
Akash Jairam <Quasar2197@gmail.com>
2022-03-01 22:29:20 -0600

    Time: 26 ms (60.82%), Space: 45.6 MB (23.95%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,24 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        // Bottom up dp approach
-        // Create dp array starting from 0 up to the given amt
-        // Fill array with amount + 1 as the value of its indices
-        // Iterate through dp array and coins array to set the min number of coins
-        // Check if the value at the amount index is not equal to amount plus one
-        // if true, return value
-        // Return -1
-        Arrays.sort(coins);
-        int[] dp = new int[amount + 1];
+        // Create an array to store our optimal number of coins from 0 to amount+1
+        // Fill our array with values of amount plus 1
+        // Assign 0 to the first index of our array since the optimal solution is 0
+        // Create a for loop that goes from 1 to amount 
+        // Create an inner loop that loops through the coins in the array
+        // If the coin is <= to the ammount, choose the lesser of array[amount - coin] + 1 and amount
+        int[] dp = new int[amount+1];
         Arrays.fill(dp, amount+1);
         dp[0] = 0;
+        
         for(int i = 1; i <= amount; ++i){
-            for(int j = 0; j < coins.length; ++j){
-                if(coins[j] <=  i){
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }else
-                    break;
+            for(int coin : coins){
+                if(coin <= i){
+                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                }
             }
         }
-        return dp[amount] == amount +1 ? -1 : dp[amount];
+        
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
     }
 }

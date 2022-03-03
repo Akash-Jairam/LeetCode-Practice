cdc16d6ba13ca6641f53706ea8750f797ce3314f
Akash Jairam <Quasar2197@gmail.com>
2022-03-03 13:46:45 -0600

    Time: 68 ms (47.21%), Space: 54.6 MB (29.31%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,36 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int length = s.length();
-        boolean isEven = length % 2 == 0;
+        int max = s.length();
+        String result = "";
         int resultLength = 0;
-        String res = "";
-        
-        for(int i = 0; i < length; ++i){
+        for(int i = 0; i < max; ++i){
             int left = i;
             int right = i;
-            
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                if((right - left + 1) > resultLength){
-                    res = s.substring(left, right+1);
+            while(left >= 0 && right < max && s.charAt(left) == s.charAt(right)){
+                if( (right - left + 1) > resultLength){
+                    result = s.substring(left, right+1);
                     resultLength = right - left + 1;
                 }
-                --left;
-                ++right;
+                left--;
+                right++;
             }
             
-            left = i;
-            right = i+1;
             
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                if((right - left + 1) > resultLength){
-                    res = s.substring(left, right+1);
+            left = i;
+            right = i + 1;
+            while(left >= 0 && right < max && s.charAt(left) == s.charAt(right)){
+                if( (right - left + 1) > resultLength){
+                    result = s.substring(left, right+1);
                     resultLength = right - left + 1;
                 }
-                --left;
-                ++right;
+                left--;
+                right++;
             }
         }
         
-        return res;
+        return result;
     }
 }

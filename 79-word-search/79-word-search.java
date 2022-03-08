d7abbdaa2877e53cd1a343de43e120a420683ec9
Akash Jairam <Quasar2197@gmail.com>
2022-03-08 09:21:46 -0600

    Time: 149 ms (60.42%), Space: 42.3 MB (39.11%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        
         for(int i = 0; i < board.length; ++i){
-            for(int j = 0; j < board[0].length; ++j){
-                if((board[i][j] == word.charAt(0)) && dfs(board, i, j, 0, word)){
+            for(int j = 0; j < board[i].length; ++j){
+                if(board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word))
                     return true;
-                }
             }
         }
         
         return false;
     }
     
-    public boolean dfs(char[][] board, int row, int column, int count, String word){
+    public boolean dfs(char[][] board, int row, int col, int count, String word){
         if(count == word.length())
             return true;
         
-        if(row < 0 || row >= board.length || column < 0 || column >= board[0].length || board[row][column] != word.charAt(count))
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != word.charAt(count))
             return false;
         
-        char temp = board[row][column];
-        board[row][column] = ' ';
-        boolean found = dfs(board, row -1, column, count + 1, word) 
-                        || dfs(board, row + 1, column, count+1, word)
-                        || dfs(board, row, column-1, count+1, word)
-                        || dfs(board, row, column+1, count + 1, word);
-        board[row][column] = temp;
+        char temp = board[row][col];
+        board[row][col] = ' ';
+        
+        boolean found = dfs(board, row - 1, col, count+1, word)
+                        || dfs(board, row + 1, col, count+1, word)
+                        || dfs(board, row, col - 1, count+1, word)
+                        || dfs(board, row, col + 1, count+1, word);
+        board[row][col] = temp;
+        
         return found;
     }
 }

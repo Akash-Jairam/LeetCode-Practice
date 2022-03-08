ce06dc2e99fd45e3df0e49b3550c6f4e2456bf42
Akash Jairam <Quasar2197@gmail.com>
2022-03-08 11:48:23 -0600

    Time: 2 ms (64.62%), Space: 44.6 MB (9.71%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,30 +1,35 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        // Create a list of strings which will hold our results
-        // If n is less than or equal to 0, return the list immediately
-        // Call the backtrack function 
-        // Return the result
-        List<String> result = new ArrayList<>();
+        // Create our list that will hold our parenthesis
+        // If n is less than or equal to zero, return our list
+        // Call our backtrack function passing in our list, an empty string, 0, 0 and n (the two zeros are for the number of open brackets and the number of closing brackets)
+        // Return our list
+        List<String> res = new ArrayList<>();
         if(n <= 0)
-            return result;
+            return res;
         
-        backtrack(result, n, "", 0, 0);
-        return result;
+        backtrack(res, "", 0, 0, n);
+        return res;
     }
     
-    public void backtrack(List<String> result, int max, String current, int openCount, int closeCount){
-        // Exit clause
-        if(current.length() == max * 2){
-            result.add(current.toString());
+    public void backtrack(List<String> result, String curr, int numOpen, int numClose, int n){
+        // If the length of the current string is equal to n times 2
+        // Add the current string to the list and add it to our result before returning
+        
+        // If the number of open braces is less than n, we append an open brace to our current string and make a recursive call, making sure to increment the number of open braces we are sending in the parameter
+        
+        // If the number of closed braces is less than the number of open braces, we do the same as before but with a closed brace and incremementing the closed braces count
+        if(curr.length() == n * 2){
+            result.add(curr);
             return;
         }
-        // Conditionals
-        if(openCount < max){
-            backtrack(result, max, current + "(", openCount+1, closeCount);
+        
+        if(numOpen < n){
+            backtrack(result, curr+"(", numOpen+1, numClose, n);
         }
         
-        if(closeCount < openCount){
-            backtrack(result, max, current + ")", openCount, closeCount+1);
+        if(numClose < numOpen){
+            backtrack(result, curr+")", numOpen, numClose+1, n);
         }
     }
 }

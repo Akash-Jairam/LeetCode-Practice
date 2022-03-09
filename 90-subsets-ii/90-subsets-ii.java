3ae5e2008b29f4a5aac1c7bd8658b529a877bb77
Akash Jairam <Quasar2197@gmail.com>
2022-03-09 09:14:37 -0600

    Time: 3 ms (47.83%), Space: 44.7 MB (9.49%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,26 +1,34 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-    List<List<Integer>> subsets = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         if(nums.length == 0)
-            return subsets;
-                 Arrays.sort(nums);
-
-    subsets.add(new ArrayList<>());
-    int startIndex = 0, endIndex = 0;
-    for (int i = 0; i < nums.length; i++) {
-      startIndex = 0;
-      // if current and the previous elements are same, create new subsets only from the subsets 
-      // added in the previous step
-      if (i > 0 && nums[i] == nums[i - 1])
-        startIndex = endIndex + 1;
-      endIndex = subsets.size() - 1;
-      for (int j = startIndex; j <= endIndex; j++) {
-        // create a new subset from the existing subset and add the current element to it
-        List<Integer> set = new ArrayList<>(subsets.get(j));
-        set.add(nums[i]);
-        subsets.add(set);
-      }
-    }
-    return subsets;
+            return res;
+        
+        //  Sort the array
+        // Intialize start and end variables to 0
+        //  Iterate through array
+        //  If index > 0 && current val at index == val at prev index
+        // Set start to end + 1
+        // Set end to the last index of our res
+        // Iterate through array from start to end
+        // Get a new arraylist from the index at start from res
+        // Add current num to new arraylist
+        // add new arraylist to res
+        // Return the result
+        Arrays.sort(nums);
+        res.add(new ArrayList<>());
+        int start = 0, end = 0;
+        for(int i = 0; i < nums.length; ++i){
+            start = 0;
+            if(i > 0 && nums[i] == nums[i-1])
+                start = end + 1;
+            end = res.size()-1;
+            for(int j = start; j <= end; ++j){
+                List<Integer> curr = new ArrayList<>(res.get(j));
+                curr.add(nums[i]);
+                res.add(curr);
+            }
+        }
+        return res;
     }
 }

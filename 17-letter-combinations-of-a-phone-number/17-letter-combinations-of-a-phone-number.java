d55ccb56a2564a0e42825dc9e50c74fa778b5436
Akash Jairam <Quasar2197@gmail.com>
2022-03-09 19:53:16 -0600

    Time: 3 ms (49.95%), Space: 42.9 MB (29.55%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         // Create a list to hold our results
-        // If the string is empty, return the list
-        // Create a map to hold the corresponding letters for each number
-        // Call our backtrack function passing the list, the map and digits
+        // iF the string is empty, return our list
+        // Call our backtrack function with our string, our list and an empty string
+        // Create an initialize a map to hold the letters for each digit
+        // Return list
         List<String> res = new ArrayList<>();
         if(digits.isEmpty())
             return res;
         Map<Character, String> map = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
-        backtrack(res, map, digits, new StringBuilder(digits.length()));
+        backtrack(res, digits, new StringBuilder(digits.length()), map);
         return res;
     }
     
-    public void backtrack(List<String> res, Map<Character, String> map , String digits, StringBuilder current){
-        if(current.length() == digits.length()){
-            res.add(current.toString());
+    public void backtrack(List<String> res, String digits, StringBuilder curr, Map<Character, String> map){
+        if(curr.length() == digits.length()){
+            res.add(curr.toString());
             return;
         }
         
-        String curr = map.get(digits.charAt(current.length()));
-        for(char c : curr.toCharArray()){
-            current.append(c);
-            backtrack(res, map, digits, current);
-            current.deleteCharAt(current.length()-1);
+        for(char c : map.get(digits.charAt(curr.length())).toCharArray()){
+            curr.append(String.valueOf(c));
+            backtrack(res, digits, curr, map);
+            curr.deleteCharAt(curr.length()-1);
         }
     }
 }

0dcb6a02c495aa7edb517df2a79b16d5067faef6
Akash Jairam <Quasar2197@gmail.com>
2022-03-09 20:12:28 -0600

    Time: 2 ms (64.52%), Space: 43.8 MB (29.62%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,35 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        // Create our list that will hold our parenthesis
-        // If n is less than or equal to zero, return our list
-        // Call our backtrack function passing in our list, an empty string, 0, 0 and n (the two zeros are for the number of open brackets and the number of closing brackets)
-        // Return our list
+        // Create a list to hold our results
+        // If n is less than or equal to zero, just return the list
+        // Call our backtrack function while passing our list, n, an empty string, 0, and 0 (the two zeroes represent our open and closed bracket counts, the empty string represents our current string)
         List<String> res = new ArrayList<>();
         if(n <= 0)
             return res;
-        
-        backtrack(res, "", 0, 0, n);
+        backtrack(res, n, "", 0, 0);
         return res;
     }
     
-    public void backtrack(List<String> result, String curr, int numOpen, int numClose, int n){
-        // If the length of the current string is equal to n times 2
-        // Add the current string to the list and add it to our result before returning
-        
-        // If the number of open braces is less than n, we append an open brace to our current string and make a recursive call, making sure to increment the number of open braces we are sending in the parameter
-        
-        // If the number of closed braces is less than the number of open braces, we do the same as before but with a closed brace and incremementing the closed braces count
-        if(curr.length() == n * 2){
-            result.add(curr);
+    public void backtrack(List<String> res, int n, String curr, int numOpen, int numClosed){
+        // Create exit statement
+        if(curr.length() == n*2){
+            res.add(curr);
             return;
         }
         
+        // If the number of open braces are less than n, recurse by adding a brace to the current string and incrementing numOpen
+        // If the numberof closed braces is less than num open( we only add closing braces to close open ones), do the same but with a closing brace and incrementing numClosed
         if(numOpen < n){
-            backtrack(result, curr+"(", numOpen+1, numClose, n);
-        }
-        
-        if(numClose < numOpen){
-            backtrack(result, curr+")", numOpen, numClose+1, n);
-        }
+            backtrack(res, n, curr+"(", numOpen+1, numClosed);}
+        if(numClosed < numOpen){
+            backtrack(res, n, curr+")", numOpen, numClosed+1);}
     }
 }

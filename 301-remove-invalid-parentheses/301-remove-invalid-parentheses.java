04b3f199459af9f0d6e72ac17bdc10ec50110553
Akash Jairam <Quasar2197@gmail.com>
2022-03-13 12:31:50 -0500

    Time: 1 ms (100.00%), Space: 42.4 MB (74.01%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
@@ -1,31 +1,34 @@
 class Solution {
     public List<String> removeInvalidParentheses(String s) {
+        // Create a list of strings to hold our result
+        // Call our method
+        // Return result
         List<String> res = new ArrayList<>();
         removeHelper(s, res, 0, 0, '(', ')');
         return res;
     }
     
-    public void removeHelper(String s, List<String> res, int iStart, int jStart, char openParen, char closedParen ){
-        int numOpenParen = 0, numClosedParen = 0;
+    public void removeHelper(String s, List<String> res, int iStart, int jStart, char open, char closed){
+        int numOpen = 0,  numClosed = 0;
+        
         for(int i = iStart; i < s.length(); ++i){
-            if(s.charAt(i) == openParen) ++numOpenParen;
-            if(s.charAt(i) == closedParen) ++numClosedParen;
+            if(s.charAt(i) == open) ++numOpen;
+            if(s.charAt(i) == closed) ++numClosed;
             
-            if(numClosedParen > numOpenParen){
+            if(numClosed > numOpen){
                 for(int j = jStart; j <= i; ++j){
-                    if(s.charAt(j) == closedParen && (j == jStart || s.charAt(j-1) != closedParen)){
-                        removeHelper(s.substring(0, j) + s.substring(j+1), res, i, j, openParen, closedParen);
-                    } 
-                    
+                    if(s.charAt(j) == closed && (j == jStart || s.charAt(j-1) != closed)){
+                        removeHelper(s.substring(0, j) + s.substring(j+1), res, i, j, open, closed);
+                    }
                 }
                 return;
             }
         }
         
         String reversed = new StringBuilder(s).reverse().toString();
-        if(openParen == '(')
+        if(open == '('){
             removeHelper(reversed, res, 0, 0, ')', '(');
-        else
+        } else
             res.add(reversed);
     }
 }

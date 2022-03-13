c37d03a478149d94bfb21dce7e0be013ce711b8c
Akash Jairam <Quasar2197@gmail.com>
2022-03-12 22:33:03 -0600

    Time: 6 ms (25.96%), Space: 42.5 MB (51.33%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,32 +1,38 @@
 class Solution {
     public String addBinary(String a, String b) {
-        String res = "";
         int aIndex = a.length()-1;
         int bIndex = b.length()-1;
+        String sum = "";
+        int curr = 0;
         int carry = 0;
-        int sum = 0;
-        
+        // Create vars to track the indices of a and b as well as the sum and carry
+        // Do a while loop that does not end until both indexes are below zero
+        // If either index is > 0, add that number to the sum
+        // Add sum to carry
+        // Add mod to current string
+        // Set carry to dividend
+        // If carry is greater than zero, prepend carry to string
         while(aIndex >= 0 || bIndex >= 0){
             if(aIndex >= 0){
-                sum += a.charAt(aIndex) - '0';
+                curr += (a.charAt(aIndex) - '0');
                 --aIndex;
             }
             
             if(bIndex >= 0){
-                sum += b.charAt(bIndex) - '0';
+                curr += (b.charAt(bIndex) - '0');
                 --bIndex;
             }
             
-            sum += carry;
-            res = ((sum) % 2) + "" + res;
-            carry = sum / 2;
-            sum = 0;
+            curr += carry;
+            sum = (curr % 2) + "" + sum;
+            carry = curr / 2;
+            curr = 0;
         }
         
         if(carry > 0){
-            res = "1" + "" + res;
+            sum = carry + "" + sum;
         }
         
-        return res;
+        return sum;
     }
 }

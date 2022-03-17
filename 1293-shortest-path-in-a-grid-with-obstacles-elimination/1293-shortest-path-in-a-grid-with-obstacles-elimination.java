64f520f09ffff44cd57b9a79ae6508d36025b01f
Akash Jairam <Quasar2197@gmail.com>
2022-03-16 21:00:42 -0500

    Time: 73 ms (38.60%), Space: 55.9 MB (28.24%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,41 +1,51 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        int[][] move = new int[4][2];
-        move[0] = new int[]{0, -1};
-        move[1] = new int[]{0, 1};
-        move[2] = new int[]{-1, 0};
-        move[3] = new int[]{1, 0};
-        boolean[][][]  visited= new boolean[grid.length][grid[0].length][k+1];
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0,k});
-        int rowLimit = grid.length;
-        int columnLimit = grid[0].length;
-        int count = 0;
+        if(grid.length == 0)
+            return -1;
         
+        
+        int[][] directions = new int[][]{{-1, 0}, {1,0}, {0, -1}, {0, 1}};
+        boolean[][][] visited = new boolean[grid.length][grid[0].length][k+1];
+        int rowLimit = grid.length;
+        int colLimit = grid[0].length;
+        // Create an array of directions to track up, down, left or right
+        // Create a queue to iterate through our graph
+        // Add 0, 0, k as an array to our queue
+        // While our queue is not empty
+        // Get the current size of the queue
+        // Iterate through the queue according to its current size
+        // Take the number and update it based on the directions
+        // Check to see if it the direction is valid and if the queue has not been visited before
+        // Also check to see if we reach the end, if so, return our path, 
+        // Check to see if the current node is an obstacle and remove k if k > 0 before adding to the queue
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, k});
+        int steps = 0;
+        visited[0][0][k] = true;
         while(!queue.isEmpty()){
             int size = queue.size();
-            for(int w = 0; w < size; ++w){
-                int[] current = queue.poll();
-                if(current[0] == rowLimit -1  && current[1] == columnLimit - 1)
-                    return count;
-                for(int[] dir : move){
-                    int row = current[0] + dir[0];
-                    int column = current[1] + dir[1];
-                    int limit = current[2];
-                    if(row >= 0 && row < rowLimit && column >= 0 && column < columnLimit){
-                        if(grid[row][column] == 0 && visited[row][column][limit] == false){
-                            queue.offer(new int[] {row, column, limit});
-                            visited[row][column][limit] = true;
-                        } else if(grid[row][column] == 1 && limit > 0 && visited[row][column][limit-1] == false){
-                            queue.offer(new int[]{row, column, limit-1});
-                            visited[row][column][limit-1] = true; 
+            for(int i = 0; i < size; ++i){
+                int[] curr = queue.poll();
+                if(curr[0] == rowLimit -1 && curr[1] == colLimit -1)
+                    return steps;
+                for(int[] dir : directions){
+                    int row = curr[0] + dir[0];
+                    int col = curr[1] + dir[1];
+                    int kCount = curr[2];
+                    if(row >= 0 && row < rowLimit && col >= 0 && col < colLimit){
+                        if(grid[row][col] == 0 && visited[row][col][kCount] == false){
+                            queue.offer(new int[]{row, col, kCount});
+                            visited[row][col][kCount] = true;
+                        } else if(grid[row][col] == 1 && kCount > 0 && visited[row][col][kCount-1] == false){
+                            queue.offer(new int[]{row, col, kCount-1});
+                            visited[row][col][kCount-1] = true;
                         }
                     }
                 }
             }
-            count++;
+            ++steps;
+
         }
-        
         return -1;
     }
 }

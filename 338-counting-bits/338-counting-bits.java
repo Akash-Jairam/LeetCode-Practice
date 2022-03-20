e94110f4da33c149d4cdfdaf68743bc97e5c2725
Akash Jairam <Quasar2197@gmail.com>
2022-03-20 10:10:38 -0500

    Time: 2 ms (81.01%), Space: 48.2 MB (48.29%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] res = new int[n+1];
-        res[0] = 0;
+        // Use a dynamic programming bottom up approach
+        // We'll set 0 to 0
+        // We will use a for loop that goes from 0 to n 
+        // We will set the count at the index of 'i' to the count at the index of 'i/2'
+        // If i is an odd number, we will increment the count stored at index 'i' by 1
+        int[] count = new int[n+1];
+        count[0] = 0;
+        
         for(int i = 1; i <= n; ++i){
-            int index = i >> 1;
-            res[i] = res[index];
+            count[i] = count[i/2];
             if(i % 2 == 1)
-                res[i] += 1;
+                count[i]++;
         }
         
-        return res;
+        return count;
     }
-    
-   
 }

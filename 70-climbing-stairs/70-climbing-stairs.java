a3764336085c51f1cee056d37a23183bc2af68ce
Akash Jairam <Quasar2197@gmail.com>
2022-03-22 20:29:20 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (45.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,23 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
         // Use a bottom up approach
-        // Create our array with n+1 size
-        // Set index 0 to 0 and index 1 to 1 since if the staircase has no steps or 1 step, you can just take a single step to reach the top
-        // Iterate through the array from 2 to n
-        // Assign the value at the current index to the sum of the values at the current index -1 and the value at the current index - 2;
-        // Return array[n]
-        
-        int prevStep = 1;
-        int prevStep2 = 1;
-        int currStep = 1;
+        // If n is 0, there are no stairs so it will only take one step to get to the top
+        // We will find all the differnent ways from 1 to n
+        // dp[n] = dp[n-2] + dp[n-1]
+        // Then we'll just return n
+        int[] dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1;
         
         for(int i = 2; i <= n; ++i){
-            currStep= prevStep + prevStep2;
-            prevStep2 = prevStep;
-            prevStep = currStep;
-            
+            dp[i] = dp[i-1] + dp[i-2];
         }
         
-        return currStep;
+        return dp[n];
     }
 }

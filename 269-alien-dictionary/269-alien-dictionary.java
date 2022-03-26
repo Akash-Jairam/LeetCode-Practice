0a24906ee1dad253b2e58233ad6838fb4fae71cf
Akash Jairam <Quasar2197@gmail.com>
2022-03-25 20:05:17 -0500

    Time: 7 ms (50.84%), Space: 43.1 MB (29.76%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.java
+++ 269-alien-dictionary/269-alien-dictionary.java
@@ -1,66 +1,80 @@
 class Solution {
     public String alienOrder(String[] words) {
-        int[] inDegree = new int[26];
+        // Create a hashmap with character as the key and a hashset of characters as the value
+        // Crease a hashmap to track the occurrences of each character
+        // Make a call to our buildgraph function which will create our graph
+        // Make a call to our dfs function which will perform the topological sorting
         HashMap<Character, HashSet<Character>> graph = new HashMap<>();
-        buildGraph(graph, words, inDegree);
-        return dfs(graph, words, inDegree);
+        HashMap<Character, Integer> inDegree = new HashMap<>();
+        buildGraph(graph, inDegree, words);
+        return dfs(graph, inDegree, words);
     }
     
-    public String dfs(HashMap<Character, HashSet<Character>> graph, String[] words, int[] inDegree){
-        StringBuilder sb = new StringBuilder(graph.size());
-        Queue<Character> queue = new LinkedList<>();
-        int size = graph.size();
-        
-        for(char c : graph.keySet()){
-            if(inDegree[c - 'a'] == 0){
-                queue.offer(c);
-                sb.append(String.valueOf(c));
-            }
-        }
-        
-        while(!queue.isEmpty()){
-            char curr = queue.poll();
-            if(graph.get(curr) == null || graph.get(curr).size() == 0) continue;
-            for(char inner : graph.get(curr)){
-                inDegree[inner - 'a']--;
-                if(inDegree[inner - 'a'] == 0){
-                    sb.append(String.valueOf(inner));
-                    queue.offer(inner);
-                }
-            }
-        }
-        
-        return sb.length() == size ? sb.toString() : "";
-    }
-    
-    public void buildGraph(HashMap<Character, HashSet<Character>> graph, String[] words, int[] inDegree){
+    public void buildGraph(HashMap<Character, HashSet<Character>> graph, HashMap<Character, Integer> inDegree, String[] words){
+        // First we will initialize our graph. For each unique character in words, we will add that as a key and an empty hashset as the value
+        // We will compare two words at a time from the array and find the first unique characters
+        // We will take the unique character from the first word (whic is the lexigraph.. smaller and add the character from the second word to the hashset corresponding to the unique character from the first word
+        // We will take the larger character and add it to our indegree map
         for(String word : words){
-            for(char c : word.toCharArray()){
+            for(Character c : word.toCharArray()){
                 graph.putIfAbsent(c, new HashSet());
             }
         }
         
         for(int i = 1; i < words.length; ++i){
-            String first = words[i-1];
-            String second = words[i];
-            int len = Math.min(first.length(), second.length());
-            
+            String prev = words[i-1];
+            String curr = words[i];
+            int len = Math.min(prev.length(), curr.length());
             for(int j = 0; j < len; ++j){
-                if(first.charAt(j) != second.charAt(j)){
-                    char outer = first.charAt(j);
-                    char inner = second.charAt(j);
-                    if(!graph.get(outer).contains(inner)){
-                        graph.get(outer).add(inner);
-                        inDegree[inner - 'a']++;
+                if(prev.charAt(j) != curr.charAt(j)){
+                    char outer = prev.charAt(j);
+                    char inner = curr.charAt(j);
+                    if(graph.get(outer) != null && !graph.get(outer).contains(inner))                     {
+                    graph.get(outer).add(inner);
+                    inDegree.put(inner, inDegree.getOrDefault(inner, 0) + 1);
                     }
-                    
                     break;
+                
                 }
-                if (j + 1 == len && first.length() > second.length()) {
+                if (j + 1 == len && prev.length() > curr.length()) {
                     graph.clear();
                     return;
                 }
             }
+            
         }
+    }
+    
+    public String dfs(HashMap<Character, HashSet<Character>> graph, HashMap<Character, Integer> inDegree, String[] words){
+        // Get the size of our graph
+        // Create a string builder to hold our result
+        // Iterate through each key in the graph
+        // Add that key to our set. if the corresponding hashset is not null and append the character to our stringbuilder
+        // Iterate through each character in the hashset
+        // Use that character to iterate through the hashset value that corresponds to that key
+        // Decrement the count of each of those characters in our indegree. If the count reaches 0, append that character to our string  and add it to our hashset
+        // Check to see if the stringbuilder size == the size of graph, if true, convert the sb to a string and return it, else return false
+        int size = graph.size();
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> queue = new LinkedList<>();
+        
+        for(Character c : graph.keySet()){
+            if(inDegree.getOrDefault(c, 0) == 0)
+                queue.offer(c);
+        }
+        
+        while(!queue.isEmpty()){
+            Character curr = queue.poll();
+            sb.append(curr);
+            if(graph.containsKey(curr)){
+                for(Character c : graph.get(curr)){
+                    inDegree.put(c, inDegree.getOrDefault(c, 0) -1);
+                    if(inDegree.getOrDefault(c, 0) == 0)
+                        queue.offer(c);
+                }
+            }
+        }
+        
+        return sb.length() == size ? sb.toString() : "";
     }
 }

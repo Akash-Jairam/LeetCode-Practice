997ee39c05e6c167b1519c06a2fc748017ecbe08
Akash Jairam <Quasar2197@gmail.com>
2022-03-26 14:32:27 -0500

    Time: 18 ms (19.35%), Space: 54.3 MB (75.19%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Sort the array
-        // Create a pointer to the first array
-        // Iterate through the array
-        // If the last element of the first array is greater than the first element of the current array, update the last element of the "first array" to the greater last element of the two arrays
-        // Else add the first array to the map and assign the second one as the first array
-        if(intervals.length <= 1)
+        if(intervals == null || intervals.length < 2)
             return intervals;
-        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
-        List<int[]> list = new ArrayList<>();
-        int[] current = intervals[0];
-        list.add(current);
         
-        for(int[] interval : intervals){
-            int[] next = interval;
-            if(current[1] >= next[0]){
-                current[1] = Math.max(current[1], interval[1]);
-            } else {
-                list.add(next);
-                current = next;
+        Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
+        int[] interval = intervals[0];
+        List<int[]> list = new LinkedList<>();
+        for(int i = 1; i < intervals.length; ++i){
+            if(interval[1] >= intervals[i][0]){
+                interval[0] = Math.min(interval[0], intervals[i][0]);
+                interval[1] = Math.max(interval[1], intervals[i][1]);
+            } else{
+                list.add(interval);
+                interval = intervals[i];
             }
-            
         }
+    
+        if(!list.contains(interval)) list.add(interval);
         
         return list.toArray(new int[list.size()][]);
     }

188986456902159afb0a255a312d86bc8d91ade4
Akash Jairam <Quasar2197@gmail.com>
2022-03-27 19:38:04 -0500

    Time: 4 ms (70.34%), Space: 46.5 MB (12.64%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,12 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a hashmap where the key and value with both be of integer type
-        // The key will represent the number at an index while the value will be the corresponding index
-        // Iterate through the array and check to see if the difference between the target and the current number is in the array
-        // If it is, return the current index and the index of the stored number
-        // Else, put the current number and the current index into the map
         HashMap<Integer, Integer> map = new HashMap<>();
-        if(nums.length == 0)
+        if(nums == null || nums.length == 0)
             return new int[]{};
         
         for(int i = 0; i < nums.length; ++i){

7aa25bb23e86531fdfdf25520e6b426489cc7c77
Akash Jairam <Quasar2197@gmail.com>
2022-03-27 19:58:41 -0500

    Time: 3 ms (45.25%), Space: 57.7 MB (53.31%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int product = 1;
-        int zeroCount = 0;
+        if(nums == null || nums.length == 0)
+            return nums;
         
-        if(nums.length == 0)
-            return new int[]{0};
+        int[] leftProduct = new int[nums.length];
+        int[] rightProduct = new int[nums.length];
+        leftProduct[0] = nums[0];
+        rightProduct[nums.length-1] = nums[nums.length-1];
         
-        for(int num : nums){
-            if(num != 0){
-                product *= num;
-            } else {
-                ++zeroCount;
-            }
+        for(int i = 1; i < nums.length; ++i){
+            leftProduct[i] = leftProduct[i-1] * nums[i];
         }
         
-        for(int i = 0; i < nums.length; ++i){
-            if(zeroCount == 1 && nums[i] == 0 ){
-                nums[i] = product;
-            } else if(zeroCount > 1){
-                nums[i] = 0;
-            } else if(zeroCount == 0){
-                nums[i] = product / nums[i];
-            } else
-                nums[i] = 0;
+        for(int i = nums.length-2; i >= 0; --i){
+            rightProduct[i] = rightProduct[i+1] * nums[i];
+        }
+        
+        if(nums.length > 1){
+            nums[0] = rightProduct[1];
+            nums[nums.length-1] = leftProduct[nums.length-2];
+        }
+        
+        for(int i = 1; i < nums.length-1; ++i){
+            nums[i] = leftProduct[i-1] * rightProduct[i+1];
         }
         
         return nums;

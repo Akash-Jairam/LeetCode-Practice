e06be2c818de7efce328bae428320d54002ef06f
Akash Jairam <Quasar2197@gmail.com>
2022-03-29 09:06:57 -0500

    Time: 1 ms (92.12%), Space: 43.1 MB (29.62%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+        // Create a hashmap to map each digit to it's corresponding string
+        Map<Character, String> map = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
+        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
         // Create a list to hold our results
-        // iF the string is empty, return our list
-        // Call our backtrack function with our string, our list and an empty string
-        // Create an initialize a map to hold the letters for each digit
-        // Return list
         List<String> res = new ArrayList<>();
-        if(digits.isEmpty())
+        // Do obligaotry null check
+        if(digits.isBlank() || digits.isEmpty())
             return res;
-        Map<Character, String> map = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
-        backtrack(res, digits, new StringBuilder(digits.length()), map);
+        // Make a call to our backtracking function and pass in the map, the given string and our list     
+        backtrack(digits, map, res, new StringBuilder(), 0);
         return res;
     }
     
-    public void backtrack(List<String> res, String digits, StringBuilder curr, Map<Character, String> map){
-        if(curr.length() == digits.length()){
-            res.add(curr.toString());
+    public void backtrack(String digits, Map<Character, String> map, List<String> res, StringBuilder sb, int index){
+        // Do base case to define return conditions. If sb.length = digits.length, add a copy of it to our result and return or return if index > digits.legnth -1
+        if(sb.length() == digits.length() || index > digits.length()-1){
+            res.add(sb.toString());
             return;
         }
+        // Iterate throguh the characters from the given index, make a recursive cal and remove the character afterwards
+        String curr = map.get(digits.charAt(index));
         
-        for(char c : map.get(digits.charAt(curr.length())).toCharArray()){
-            curr.append(String.valueOf(c));
-            backtrack(res, digits, curr, map);
-            curr.deleteCharAt(curr.length()-1);
+        for(char c : curr.toCharArray()){
+            sb.append(c);
+            backtrack(digits, map, res, sb, index+1);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }

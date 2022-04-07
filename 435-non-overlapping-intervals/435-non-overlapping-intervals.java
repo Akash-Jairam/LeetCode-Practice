7bea3326280556973858c862b4a8689f3b9795c2
Akash Jairam <Quasar2197@gmail.com>
2022-04-06 20:47:15 -0500

    Time: 45 ms (99.73%), Space: 99.8 MB (80.97%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,30 +1,19 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if(intervals == null || intervals.length == 0 || intervals[0].length == 0 || intervals.length == 1)
+        if(intervals == null || intervals.length <= 1)
             return 0;
         
-        // Sort the intervals
-        Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
-        // Create a variable to track our count
-        int count = 0;
-        // Create an array that will track the previous interval and initialize it ot the first arrayin interval
-        int[] previous = intervals[0];
+        Arrays.sort(intervals, (arr1, arr2) -> (arr1[1] - arr2[1]));
         
-        // Iterate through the other elements in interval
+        int[] curr = intervals[0];
+        int res = 0;
         for(int i = 1; i < intervals.length; ++i){
-        // If the last element in previous is greater than the first element in interval, check to see if the last element in previous is greater than the last element in interval, if that's the case, we increment our count and set previous to the current interval
-            if(previous[1] > intervals[i][0]){
-                ++count;
-                if(previous[1] > intervals[i][1]){
-                    
-                    previous = intervals[i];
-                }
+            if(curr[1] > intervals[i][0]){
+                ++res;
             } else{
-                previous = intervals[i];
+                curr = intervals[i];
             }
-        // Else we just set previous to the current interval
         }
-        
-        return count;
+        return res;
     }
 }

dca80dfa0a7fd58440d8ef46015dcf6146531d59
Akash Jairam <Quasar2197@gmail.com>
2022-04-06 20:18:13 -0500

    Time: 2 ms (76.06%), Space: 47.5 MB (63.36%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,25 +1,37 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new LinkedList<>();
+        // Do obligatory edge case checks
+        // Sort the intervals array
+        // Create a list to hold our intervals
+        // Iterate through the intervals array and add all the intervals that occur before our new interval into the list
+        // Iterate through the intervals array while the current interval starts before the end of our "newInterval", making sure to merge the current interval into the new interval as needed
+        // Add the remainind intervals to our list
+        // Convert our list to an array and return it
+        if(intervals == null || intervals.length == 0)
+            return new int[][]{newInterval};
         
-        int i = 0;
-        while(i < intervals.length && intervals[i][1] < newInterval[0]){
-            list.add(intervals[i]);
-            ++i;
+        if(newInterval == null || newInterval.length == 0)
+            return intervals;
+        
+        int index = 0;
+        List<int[]> res = new ArrayList<>();
+        while(index < intervals.length && intervals[index][1] < newInterval[0]){
+            res.add(intervals[index]);
+            ++index;
         }
         
-        while(i < intervals.length && intervals[i][0] <= newInterval[1] ){
-            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-            ++i;
+        while(index < intervals.length && intervals[index][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
+            ++index;
         }
-        list.add(newInterval);
+        res.add(newInterval);
         
-        while(i < intervals.length){
-            list.add(intervals[i]);
-            ++i;
+        while(index < intervals.length){
+            res.add(intervals[index]);
+            ++index;
         }
         
-        return list.toArray(new int[list.size()][2]);
+        return res.toArray(new int[res.size()][2]);
     }
 }

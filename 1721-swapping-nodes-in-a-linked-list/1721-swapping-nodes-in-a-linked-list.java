e3e84e559e990a52447d0885a0db046d0388615b
Akash Jairam <Quasar2197@gmail.com>
2022-04-06 19:45:51 -0500

    Time: 2 ms (100.00%), Space: 179.2 MB (58.53%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,7 +10,15 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
+        // Use two pointer method
+        // Set both pointers to the head
+        // For the first pointer
+        // Traverse through the list until we reach the kth element from the front
+        // Create a pointer and assign it to the first pointer
+        // While the newest pointer is not equal to null, traverse the list while making sure to moveour second pointer in tandem
+        // Since there is k distance between our 
         ListNode first = head;
+        ListNode second = head;
         int count = 1;
         
         while(count < k){
@@ -18,31 +26,15 @@ class Solution {
             ++count;
         }
         
-        head = reverse(head);
-        ListNode second = head;
-        count = 1;
-        
-        while(count < k){
+        ListNode it = first;
+        while(it.next != null){
+            it = it.next;
             second = second.next;
-            ++count;
         }
         
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
-        return reverse(head);
-    }
-    
-    public ListNode reverse(ListNode head){
-        ListNode prev = null;
-        
-        while(head != null){
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        
-        return prev;
+        return head;
     }
 }

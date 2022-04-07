571c0ac2c452a0267974e62a5e8a1fd5826453ac
Akash Jairam <Quasar2197@gmail.com>
2022-04-06 20:31:32 -0500

    Time: 7 ms (89.17%), Space: 47 MB (93.26%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,23 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals == null || intervals.length < 2)
+        if(intervals == null || intervals.length < 1)
             return intervals;
         
-        Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
-        int[] interval = intervals[0];
-        List<int[]> list = new LinkedList<>();
+        // Create a list to hold our result
+        // Sort intervals
+        // Create a variable to point to the current interval
+        // Iterate through the array and compare the current interval to the next interval
+        // If they overlap, merge them else add the current interval to your list and set the pointer to the current next interval 
+        // Finally, check to see if the final current interval is in our list. If it is not, add it
+        // Conver the list to an array and return it
+        Arrays.sort(intervals, ((arr1, arr2) -> arr1[0] - arr2[0]));
+        List<int[]> res = new ArrayList<>();
+        int[] curr = intervals[0];
+        
         for(int i = 1; i < intervals.length; ++i){
-            if(interval[1] >= intervals[i][0]){
-                interval[0] = Math.min(interval[0], intervals[i][0]);
-                interval[1] = Math.max(interval[1], intervals[i][1]);
+            if(curr[1] >= intervals[i][0]){
+                curr[0] = Math.min(curr[0], intervals[i][0]);
+                curr[1] = Math.max(curr[1], intervals[i][1]);
             } else{
-                list.add(interval);
-                interval = intervals[i];
+                res.add(curr);
+                curr = intervals[i];
             }
         }
-    
-        if(!list.contains(interval)) list.add(interval);
         
-        return list.toArray(new int[list.size()][]);
+        if(!res.contains(curr))
+            res.add(curr);
+        
+        return res.toArray(new int[res.size()][2]);
     }
 }

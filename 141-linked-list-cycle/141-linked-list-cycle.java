c34626141d819e0011d7f91e24cea592d9b88166
Akash Jairam <Quasar2197@gmail.com>
2022-04-07 20:59:22 -0500

    Time: 0 ms (100.00%), Space: 46.1 MB (35.95%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,15 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode copy = head;
-        HashSet<ListNode> set = new HashSet();
+        ListNode slow = head;
+        ListNode fast = head;
         
-        while(copy != null){
-            if(set.contains(copy))
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
                 return true;
-            
-            set.add(copy);
-            copy = copy.next;
         }
         
         return false;

1fca6b87716a4f53e9153a9eba70b44de8e44144
Akash Jairam <Quasar2197@gmail.com>
2022-04-16 10:43:36 -0500

    Time: 6 ms (79.71%), Space: 42.2 MB (86.96%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,17 +1,11 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        // Create a priority queue to hold which of our meeting rooms are booked with the one with the earliest ending at the front 
-        // Iterate through our intervals array
-        // If our queue is not empty and there is an element in our priority queue that ends before the start of our interval, we "use that meeting room" for our current meeting by kicking out the people who are in that room lol
-        // We will add the current interval into our queue
-        
-        // Finally, we return the size of the priorityqueue as our result
-        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> (arr1[1] - arr2[1]));
         Arrays.sort(intervals, (arr1, arr2) -> (arr1[0] - arr2[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> (arr1[1] - arr2[1]));
         for(int[] interval : intervals){
-            if(!pq.isEmpty() && pq.peek()[1] <= interval[0])
+            if(!pq.isEmpty() && pq.peek()[1] <= interval[0]){
                 pq.poll();
-            
+            }
             pq.offer(interval);
         }
         

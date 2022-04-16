8694a14a30ffef10277a79d281865813460f6023
Akash Jairam <Quasar2197@gmail.com>
2022-04-16 09:49:23 -0500

    Time: 3 ms (90.83%), Space: 54.6 MB (82.20%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -4,25 +4,17 @@ class Solution {
     }
     
     public boolean traverse(int[] arr, int start, boolean[] visited){
+        if(start >= arr.length || start < 0 || visited[start])
+            return false;
+        
         if(arr[start] == 0)
             return true;
         
-        if(visited[start])
-            return false;
+        
         
         visited[start] = true;
         
-        int forwardJump = start + arr[start];
-        int backwardJump = start - arr[start];
-        boolean canForward = false;
-        boolean canBackward = false;
+        return traverse(arr, start + arr[start], visited) || traverse(arr, start - arr[start], visited);
         
-        if(forwardJump >= 0 && forwardJump < arr.length)
-            canForward = traverse(arr, forwardJump, visited);
-        
-        if(backwardJump >= 0 && backwardJump < arr.length)
-            canBackward = traverse(arr, backwardJump, visited);
-        
-        return canForward || canBackward;
     }
 }

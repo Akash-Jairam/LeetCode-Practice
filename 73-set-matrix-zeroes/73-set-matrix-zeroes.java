dccf00b225986bfd7933f5239ea4549f771b7107
Akash Jairam <Quasar2197@gmail.com>
2022-04-17 18:54:54 -0500

    Time: 1 ms (88.25%), Space: 44.3 MB (76.17%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        // Create a variable to serve as the flag for whether to set column 0 to 0s
+        // Iterate through our matrix, starting from row 0 and col 1
+        // If an element is 0, update the beginning of that row and the beginning of that col to 0
+        // Iterate through the matrix from the bottom up and if the row head or col head of a given element is 0, set that element to 0
+        // If col 0 is enabled, set the current row and column 0 / beginning of current row to 0
+        boolean column0Flag = false;
         int row = matrix.length;
         int col = matrix[0].length;
-        
-        // Create a flag to check to see if the first element in a given row is set to 0
-        // If we encounter that, we set the flag to true
-        // If we encounter a zero, set the first element in the corresponding row, and the ocrresponding col to 0
-        // If
-        boolean flag = false;
-        
         for(int i = 0; i < row; ++i){
             if(matrix[i][0] == 0)
-                flag = true;
+                column0Flag = true;
             for(int j = 1; j < col; ++j){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
@@ -20,19 +19,13 @@ class Solution {
             }
         }
         
-        
-        // Iterate through our matrix again
-        // if the first element for a given row or column is equal to zero, we set the element at the current row and col to 0
-        
-        // If the  flag is set to true, set the first element of the column to 0
         for(int i = row - 1; i >= 0; --i){
             for(int j = col - 1; j >= 1; --j){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
-            
-            if(flag)
+            if(column0Flag)
                 matrix[i][0] = 0;
         }
     }

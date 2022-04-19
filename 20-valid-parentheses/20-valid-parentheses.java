ffa4d3ea6b74a4a07a41c3daf2557bdc98b06720
Akash Jairam <Quasar2197@gmail.com>
2022-04-19 08:26:08 -0500

    Time: 2 ms (81.17%), Space: 41.8 MB (64.73%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,22 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        // Create a stack that will hold our open braces
-        // Iterate through s to char array
-        // If we encounter an opening brace, we push to the stack
-        // Else, if the closing brace is equal to the element at the top of the stack, we pop it
-        // Else, push it to the stack
+        if(s == null || s.length() == 0)
+            return true;
+        
+        if(s.length() % 2 == 1)
+            return false;
+        
+        
         Stack<Character> stack = new Stack<>();
+        
         for(Character c : s.toCharArray()){
-            if(c == '(' || c == '{' || c == '[')
+            if(c == '(' || c == '(' || c == '[')
                 stack.push(c);
-            else if( c == ')' && !stack.isEmpty() && stack.peek() == '(')
-                stack.pop();
-            else if( c == ']' && !stack.isEmpty() && stack.peek() == '[')
-                stack.pop();
-            else if( c == '}' && !stack.isEmpty() && stack.peek() == '{')
-                stack.pop();
-            else
-                stack.push(c);
+            else {
+                if(c == ')' && !stack.isEmpty() && stack.peek() == '(')
+                    stack.pop();
+                else if(c == ']' && !stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else if(c == '}' && !stack.isEmpty() && stack.peek() == '{')
+                    stack.pop();
+                else
+                    stack.push(c);
+            }
         }
         
         return stack.isEmpty();

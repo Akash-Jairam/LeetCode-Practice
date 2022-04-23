23a5aa407ac28d194979db8a9ee25b10aea3c46a
Akash Jairam <Quasar2197@gmail.com>
2022-04-23 07:42:20 -0500

    Time: 1 ms (90.52%), Space: 40.9 MB (91.70%) - LeetHub

---
+++ 887-super-egg-drop/887-super-egg-drop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int[] dp = new int[k+1];
+        int steps = 0;
+        while(dp[k] < n){
+            steps++;
+            for(int i = k; i > 0; --i){
+                dp[i] +=  dp[i-1] + 1;
+            }
+            
+        }
+        
+        return steps;
+    }
+}
+

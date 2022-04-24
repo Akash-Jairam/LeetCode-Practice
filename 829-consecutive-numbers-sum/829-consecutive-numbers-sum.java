fa359e2328e239a242f1eb60fedfac8360b53fc3
Akash Jairam <Quasar2197@gmail.com>
2022-04-24 10:10:56 -0500

    Time: 7 ms (40.34%), Space: 41.2 MB (27.49%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
@@ -3,10 +3,13 @@ class Solution {
         int count = 0;
         
         for(int n = 2; n*(n+1)/2 <= N; ++n){
-            if((N - n * (n+1)/2) % n == 0)
+            int val = (N - n * (n+1)/2);
+            if( val % n == 0)
                 ++count;
         }
         
         return count+1;
     }
+    
+    // https://www.youtube.com/watch?v=oKOCYZd4m7E
 }

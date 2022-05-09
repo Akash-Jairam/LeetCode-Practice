954d740dae73139c1db95bd76b7539521bf3779e
Akash Jairam <Quasar2197@gmail.com>
2022-05-08 19:13:44 -0500

    Time: 30 ms (47.98%), Space: 54.7 MB (17.84%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.java
+++ 887-super-egg-drop/887-super-egg-drop.java
@@ -1,12 +1,12 @@
 class Solution {
     public int superEggDrop(int k, int n) {
-        int[][] dp = new int[n+1][k+1];
+        int[][] dp = new int[n + 1][k+1];
         int steps = 0;
         
         while(dp[steps][k] < n){
             ++steps;
             for(int i = 1; i <= k; ++i){
-                dp[steps][i] = dp[steps-1][i-1] + dp[steps-1][i] + 1;
+                dp[steps][i] = dp[steps-1][i] + dp[steps-1][i-1] + 1;
             }
         }
         

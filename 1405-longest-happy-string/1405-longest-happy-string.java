9ae33bf991a74f28acb26c02589ceda64530e573
Akash Jairam <Quasar2197@gmail.com>
2022-05-17 20:34:42 -0500

    Time: 4 ms (26.54%), Space: 41.6 MB (37.06%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.java
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -1,40 +1,38 @@
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
-        // Create a queue that holds pairs of characters and integers
-        // Put the characters a, b and c paired with their given counts into the queue
-        // While the queue is not empty, check the last element of our stringbuilder and see if it is the element at the top of the queue, if it is, take the second element and add it to our string builder before putting both back while making sure to update the count
-        // Else find the minimum of the count and 2, add that element to the queue  count timesand decrement it by the count before adding it back to the queue
-        PriorityQueue<Pair<Character, Integer>> pq = new PriorityQueue<>((p1, p2) -> Integer.compare(p2.getValue(), p1.getValue()));
-        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Pair<Character, Integer>> pq = new PriorityQueue<>((p1, p2) -> p2.getValue() - p1.getValue());
         
         if(a > 0)
-            pq.add(new Pair<Character, Integer>('a', a));
+            pq.offer(new Pair<Character, Integer>('a', a));
+        
         if(b > 0)
-            pq.add(new Pair<Character, Integer>('b', b));
+            pq.offer(new Pair<Character, Integer>('b', b));
+        
         if(c > 0)
-            pq.add(new Pair<Character, Integer> ('c', c));
+            pq.offer(new Pair<Character, Integer>('c', c));
+        
+        StringBuilder sb = new StringBuilder(a + b + c);
         
         while(!pq.isEmpty()){
-            Pair<Character, Integer> firstPair = pq.poll();
-            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == firstPair.getKey()){
+            Pair<Character, Integer> first = pq.poll();
+            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == first.getKey()){
                 if(pq.isEmpty())
                     return sb.toString();
                 
-                Pair<Character, Integer> secondPair = pq.poll();
-                sb.append(secondPair.getKey());
-                if(secondPair.getValue() - 1 > 0)
-                    pq.add(new Pair<Character, Integer> (secondPair.getKey(), secondPair.getValue() -1));
-                pq.add(firstPair);
-            }else{
-                int count = Math.min(firstPair.getValue(), 2);
-                for(int i = 0; i < count; ++i){
-                    sb.append(firstPair.getKey());
-                }
-                if(firstPair.getValue() - count > 0)
-                    pq.add(new Pair<Character, Integer> (firstPair.getKey(), firstPair.getValue() - count));
+                Pair<Character, Integer> second = pq.poll();
+                sb.append(second.getKey());
+                if(second.getValue() - 1 > 0)
+                    pq.offer(new Pair<Character, Integer>(second.getKey(), second.getValue() - 1));
+                pq.offer(first);
+            } else{
+                int val = Math.min(first.getValue(), 2);
+                for(int i = 0; i < val; ++i)
+                    sb.append(first.getKey());
+                if(first.getValue() - val > 0)
+                    pq.offer(new Pair<Character, Integer>(first.getKey(), first.getValue() - val));
             }
+                
         }
-        
         return sb.toString();
     }
 }

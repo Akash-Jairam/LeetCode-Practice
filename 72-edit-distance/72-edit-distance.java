30516a5ab0cb10742635389554b88b4c068f9887
Akash Jairam <Quasar2197@gmail.com>
2022-05-21 18:47:05 -0500

    Time: 5 ms (89.66%), Space: 42.3 MB (85.94%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -3,38 +3,34 @@ class Solution {
         if(word1 == null || word2 == null)
             return -1;
         
-        if(word1.length() == 0)
+        if(word1 == null && word2 != null)
             return word2.length();
         
-        if(word2.length() == 0)
+        if(word1 != null && word2 == null)
             return word1.length();
         
-        char[] c1 = word1.toCharArray();
-        char[] c2 = word2.toCharArray();
+        int[][] dp = new int[word1.length()+1][word2.length() + 1];
         
-        int[][] matched = new int[c1.length+1][c2.length+1];
-        
-        for(int i = 0; i <= c1.length; ++i){
-            matched[i][0] =i;
+        for(int i = 0; i < dp[0].length; ++i){
+            dp[0][i] = i;
         }
         
-        for(int i = 0; i <= c2.length; ++i){
-            matched[0][i] = i;
+        for(int i = 0; i < dp.length; ++i){
+            dp[i][0] = i;
         }
         
-        
-        for(int i = 0; i < c1.length; ++i){
-            for(int j = 0; j < c2.length; ++j){
-                if(c1[i] == c2[j])
-                    matched[i+1][j+1] = matched[i][j];
-                else{
-                    matched[i+1][j+1] = Math.min(Math.min(matched[i][j + 1], matched[i + 1][j]), matched[i][j]) + 1;
+        for(int i = 1; i < dp.length; ++i){
+            char w1 = word1.charAt(i - 1);
+            for(int j = 1; j < dp[0].length; ++j){
+                char w2 = word2.charAt(j - 1);
+                if(w1 != w2){
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j-1])) + 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1];
                 }
             }
         }
         
-        return matched[c1.length][c2.length];
+        return dp[word1.length()][word2.length()];
     }
-    
-    
 }

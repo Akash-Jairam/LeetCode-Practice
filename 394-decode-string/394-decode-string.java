6a3fc48bc3653a9f4b376904cfcf9091415d2614
Akash Jairam <Quasar2197@gmail.com>
2022-05-22 19:13:24 -0500

    Time: 8 ms (15.68%), Space: 42.5 MB (24.46%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,37 +1,38 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> repeat = new Stack();
-        Stack<String> pieces = new Stack();
-        int index = 0;
-        String res = "";
-        while(index < s.length()){
-            char curr = s.charAt(index);
-            if(Character.isDigit(curr)){
-                int number = 0;
-                while(Character.isDigit(s.charAt(index))){
-                    number = number * 10 + (s.charAt(index) - '0');
-                    ++index;
-                }
-                repeat.push(number);
-            } else if(curr == '['){
-                pieces.push(res);
-                res = "";
-                ++index;
-            } else if(curr == ']'){
-                int count = repeat.pop();
-                StringBuilder temp = new StringBuilder(pieces.pop());
-                for(int i = 0; i < count; ++i){
-                    temp.append(res);
-                }
-                res = temp.toString();
-                ++index;
-            } else{
-                res += curr;
-                ++index;
-            }
+        if(s == null || s.isEmpty())
+            return s;
+        
+        Stack<Integer> nums = new Stack<>();
+        Stack<String> pieces = new Stack<>();
+        String curr = "";
+        
+        for(int i = 0; i < s.length(); ++i){
+            char c = s.charAt(i);
             
+            if(Character.isDigit(c)){
+                int num = 0;
+                while(Character.isDigit(s.charAt(i))){
+                    num = num * 10 + (s.charAt(i) - '0');
+                    ++i;
+                }
+                nums.push(num);
+                --i;
+            } else if(c == '['){
+                pieces.push(curr);
+                curr = "";
+            } else if(c == ']'){
+                int count = nums.pop();
+                StringBuilder temp = new StringBuilder(pieces.pop());
+                for(int j = 0; j < count; ++j){
+                    temp.append(curr);
+                }
+                curr = temp.toString();
+            } else {
+                curr += c;
+            }
         }
         
-        return res;
+        return curr;
     }
 }

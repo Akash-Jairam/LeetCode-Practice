4a2192b0603f1e3245eb63f8dab66bc1b3aeece0
Akash Jairam <Quasar2197@gmail.com>
2022-06-08 18:24:21 -0500

    Time: 24 ms (67.24%), Space: 52.2 MB (45.20%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.java
+++ 811-subdomain-visit-count/811-subdomain-visit-count.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<String> subdomainVisits(String[] cpdomains) {
+        HashMap<String, Integer> map = new HashMap<>();
         List<String> res = new ArrayList<>();
         if(cpdomains == null || cpdomains.length == 0)
             return res;
         
-        HashMap<String, Integer> map = new HashMap<>();
         for(String domain : cpdomains){
-            int spacePos = domain.indexOf(" ");
-            int visits = Integer.parseInt(domain.substring(0, spacePos));
-            String d = domain.substring(spacePos+1);
+            String[] separated = domain.split(" ");
+            String[] split = separated[1].split("\\.");
+            String prev = "";
             
-            for(int i = 0; i < d.length(); ++i){
-                if(d.charAt(i) == '.'){
-                    String name = d.substring(i+1);
-                    map.put(name, map.getOrDefault(name, 0) + visits);
+            for(int i = split.length - 1; i >= 0; --i){
+                if(prev.equals("")){
+                    prev = split[i];
+                } else {
+                    prev = split[i] + "." + prev;
                 }
-               
+                map.put(prev, map.getOrDefault(prev, 0) + Integer.parseInt(separated[0]));                
             }
-             map.put(d, map.getOrDefault(d,0) + visits);
         }
         
-        for(String d : map.keySet()){
-            res.add(map.get(d) + " " + d);
+        for(String key : map.keySet()){
+            res.add(map.get(key) + " " + key);
         }
         
         return res;

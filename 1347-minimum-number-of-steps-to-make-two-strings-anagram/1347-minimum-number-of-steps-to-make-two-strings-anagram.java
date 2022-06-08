a9bb9467586d25eac434dfb54d1a85df8e57b07d
Akash Jairam <Quasar2197@gmail.com>
2022-06-08 18:30:56 -0500

    Time: 15 ms (56.68%), Space: 54.2 MB (56.45%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -3,27 +3,21 @@ class Solution {
         int[] sMap = new int[26];
         int[] tMap = new int[26];
         
-        
-        fill(s, sMap);
-        fill(t, tMap);
-        int sSteps = 0;
-        int tSteps = 0;
+        map(s, sMap);
+        map(t, tMap);
+        int count = 0;
         for(int i = 0; i < 26; ++i){
-            if(sMap[i] > tMap[i])
-                sSteps += sMap[i] - tMap[i];
-            
-            if(tMap[i] > sMap[i])
-                tSteps += tMap[i] - sMap[i];
+            if(tMap[i] != sMap[i]){
+                count += Math.abs(tMap[i] - sMap[i]);
+            }
         }
         
-        return Math.min(sSteps, tSteps);
+        return count/2;
     }
     
-    public void fill(String s, int[] map){
-        
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            map[c - 'a']++;
+    public void map(String s, int[] map){
+        for(int i = 0; i < s.length(); ++i){
+            map[s.charAt(i) - 'a']++;
         }
     }
 }

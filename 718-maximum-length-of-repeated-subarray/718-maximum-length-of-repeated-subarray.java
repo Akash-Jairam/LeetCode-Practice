c3e3190e64f68e0da5def0046690d35ae9aaee2b
Akash Jairam <Quasar2197@gmail.com>
2022-06-09 18:09:15 -0500

    Time: 55 ms (77.51%), Space: 78.4 MB (36.39%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -3,19 +3,11 @@ class Solution {
         int[][] dp = new int[nums1.length+1][nums2.length+1];
         int max = 0;
         
-        for(int i = 0; i < dp.length; ++i){
-            dp[i][0] = 0;
-        }
-        
-        for(int i = 0; i < dp[0].length; ++i){
-            dp[0][i] = 0;
-        }
-        
         for(int i = 1; i < dp.length; ++i){
             for(int j = 1; j < dp[0].length; ++j){
-                if(nums1[i - 1] == nums2[j - 1]){
-                    dp[i][j] = dp[i-1][j - 1] + 1;
-                    max = Math.max(max, dp[i][j]);
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    max = Math.max(dp[i][j], max);
                 }
             }
         }

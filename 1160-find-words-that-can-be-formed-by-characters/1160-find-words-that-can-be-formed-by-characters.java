e4e434d5bbac80ef7a4c5890e6df64ff2bedcaa4
Akash Jairam <Quasar2197@gmail.com>
2022-06-09 19:20:13 -0500

    Time: 18 ms (52.36%), Space: 53.2 MB (65.08%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,34 +1,26 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int sum = 0;
-        int[] charsMap = map(chars);
-        
+        int[] charsMap = new int[26];
+        mapWord(charsMap, chars);
+        int res = 0;
         for(String word : words){
-            int[] wordMap = map(word);
-            if(compare(charsMap, wordMap, word.length()))
-                sum += word.length();
-        }
-        
-        return sum;
-    }
-    
-    public int[] map(String s){
-        int[] arr = new int[26];
-        for(int i = 0; i < s.length(); ++i){
-            char c = s.charAt(i);
-            arr[c - 'a']++;
-        }
-        
-        return arr;
-    }
-    
-    public boolean compare(int[] charsMap, int[] wordMap, int wordLength){
-        for(int i = 0; i < 26; ++i){
-            if(wordMap[i] <= charsMap[i]){
-                wordLength -= wordMap[i];
+            int[] wordMap = new int[26];
+            mapWord(wordMap, word);
+            int count = 0;
+            for(int i = 0; i < 26; ++i){
+                if(wordMap[i] <= charsMap[i])
+                    count += wordMap[i];
             }
+            if(count == word.length())
+                res += count;
         }
         
-        return wordLength == 0;
+        return res;
+    }
+    
+    public void mapWord(int[] map, String word){
+        for(int i = 0; i < word.length(); ++i){
+            map[word.charAt(i) - 'a']++;
+        }
     }
 }

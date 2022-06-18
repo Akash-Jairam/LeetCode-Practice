bb8f8404b4a2bb96eb94eade4865067380a38e02
Akash Jairam <Quasar2197@gmail.com>
2022-06-18 16:29:16 -0500

    Time: 18 ms (12.44%), Space: 42.5 MB (99.53%) - LeetHub

--- 490-the-maze/490-the-maze.java
+++ 490-the-maze/490-the-maze.java
@@ -1,26 +1,27 @@
 class Solution {
+    int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
-        int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        
+        int rowLimit = maze.length;
+        int colLimit = maze[0].length;
+        boolean[][] visited = new boolean[rowLimit+1][colLimit+1];
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(start);
         
         while(!queue.isEmpty()){
             int[] curr = queue.poll();
             visited[curr[0]][curr[1]] = true;
-            
-            if(destination[0] == curr[0] && destination[1] == curr[1])
+            if(curr[0] == destination[0] && curr[1] == destination[1])
                 return true;
             
             for(int[] dir : directions){
                 int row = curr[0];
                 int col = curr[1];
                 
-                while(isValid(maze, row+dir[0], col+dir[1])){
+                while(isValid(maze, row + dir[0], col + dir[1])){
                     row += dir[0];
                     col += dir[1];
                 }
+                
                 if(!visited[row][col])
                     queue.offer(new int[]{row, col});
             }

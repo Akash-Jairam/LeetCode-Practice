d71edcf9eb931815a094a1489fdc8fe8cd3258d1
Akash Jairam <Quasar2197@gmail.com>
2022-06-19 10:57:39 -0500

    Time: 145 ms (20.53%), Space: 88 MB (60.82%) - LeetHub

--- 353-design-snake-game/353-design-snake-game.java
+++ 353-design-snake-game/353-design-snake-game.java
@@ -1,66 +1,69 @@
 class SnakeGame {
-    int height;
-    int width;
-    int foodEaten;
-    
-    HashSet<Pair<Integer, Integer>> body;
     Deque<Pair<Integer, Integer>> bodyQueue;
+    HashSet<Pair<Integer, Integer>> body;
     Queue<Pair<Integer, Integer>> foodQueue;
+    int foodEaten;
+    int width;
+    int height;
+    
     public SnakeGame(int width, int height, int[][] food) {
-        this.height = height-1;
         this.width = width - 1;
+        this.height = height - 1;
         foodEaten = 0;
         
-        body = new HashSet();
-        body.add(new Pair<>(0, 0));
         bodyQueue = new LinkedList<>();
         bodyQueue.offer(new Pair<>(0,0));
+        body = new HashSet();
+        body.add(new Pair<>(0,0));
         
         foodQueue = new LinkedList<>();
-        for(int[] f : food){
+        for(int[] f : food)
             foodQueue.offer(new Pair<>(f[0], f[1]));
-        }
     }
     
     public int move(String direction) {
-        Pair<Integer, Integer> nextDirection = move(bodyQueue.peekFirst(), direction);
-        if(snakeDie(nextDirection))
+        // Find where the head would be if moved in that direction
+        // Check to see if the snake would die
+        // Check to see if there is food at that part
+        // Return food eaten 
+        Pair<Integer, Integer> nextDirection = move(direction, bodyQueue.peekFirst());
+        if(snakeDies(nextDirection))
             return -1;
         
         if(!foodQueue.isEmpty() && !body.contains(foodQueue.peek()) && nextDirection.equals(foodQueue.peek())){
-            ++foodEaten;
             body.add(nextDirection);
             bodyQueue.addFirst(nextDirection);
             foodQueue.poll();
+            ++foodEaten;
             return foodEaten;
         }
-         body.remove(bodyQueue.pollLast());
+        
+        body.remove(bodyQueue.pollLast());
         body.add(nextDirection);
         bodyQueue.addFirst(nextDirection);
-       
         return foodEaten;
     }
     
-    public Pair<Integer, Integer> move(Pair<Integer, Integer> currentPos, String direction){
+    public Pair<Integer, Integer> move(String direction, Pair<Integer, Integer> curr){
         if(direction.equals("L")){
-            return new Pair<>(currentPos.getKey(), currentPos.getValue() - 1);
+            return new Pair<>(curr.getKey(), curr.getValue() - 1);
         } else if(direction.equals("R")){
-            return new Pair<>(currentPos.getKey(), currentPos.getValue() + 1);
+            return new Pair<>(curr.getKey(), curr.getValue() + 1);
         } else if(direction.equals("U")){
-            return new Pair<>(currentPos.getKey() - 1,currentPos.getValue());
-        } else {
-            return new Pair<>(currentPos.getKey() + 1, currentPos.getValue());
+            return new Pair<>(curr.getKey() - 1 , curr.getValue());
+        } else{
+            return new Pair<>(curr.getKey() + 1, curr.getValue());
         }
     }
     
-    public boolean snakeDie(Pair<Integer, Integer> curr){
-        int row = curr.getKey();
-        int col = curr.getValue();
+    public boolean snakeDies(Pair<Integer, Integer> nextDirection){
+        int row = nextDirection.getKey();
+        int col = nextDirection.getValue();
         
         if(row < 0 || row > height || col < 0 || col > width)
             return true;
         
-        return !curr.equals(bodyQueue.peekLast()) && body.contains(curr);
+        return !nextDirection.equals(bodyQueue.peekLast()) && body.contains(nextDirection);
     }
 }
 

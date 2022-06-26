9b608e63008dab527729ddf7a319fa00d06ad207
Akash Jairam <Quasar2197@gmail.com>
2022-06-26 10:44:09 -0500

    Time: 7 ms (60.86%), Space: 75.9 MB (41.38%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -4,11 +4,13 @@ class Solution {
         int slow = nums[0];
         int fast = nums[nums[0]];
         
+        // Let tortoise meet hare
         while(slow != fast){
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
         
+        // Hare wins. Give tortoise a second chance from the start to catch hare
         slow = 0;
         
         while(slow != fast){

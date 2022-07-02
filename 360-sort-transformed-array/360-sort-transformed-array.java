c2e61b4dc0bca59c282acbdbcf2128940cc0a8f8
Akash Jairam <Quasar2197@gmail.com>
2022-07-02 08:00:03 -0500

    Time: 7 ms (5.34%), Space: 44.1 MB (10.02%) - LeetHub

--- 360-sort-transformed-array/360-sort-transformed-array.java
+++ 360-sort-transformed-array/360-sort-transformed-array.java
@@ -1,40 +1,15 @@
 class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-        int[] res = new int[nums.length];
-        int start = 0, end = nums.length - 1;
-        int i = a >= 0 ? nums.length - 1 : 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> (x - y));
         
-        while(start <= end){
-            int startNum = quad(nums[start], a, b, c);
-            int endNum = quad(nums[end], a, b, c);
-            
-            if(a >= 0){
-                if(startNum >= endNum){
-                    res[i] = startNum;
-                    --i;
-                    ++start;
-                } else{
-                    res[i] = endNum;
-                    --i;
-                    --end;
-                }
-            } else{
-                if(startNum <= endNum){
-                    res[i] = startNum;
-                    ++i;
-                    ++start;
-                } else{
-                    res[i] = endNum;
-                    --end;
-                    ++i;
-                }
-            }
+        for(int num : nums){
+            pq.offer((a * num * num) + (b * num) + c);
         }
         
-        return res;
-    }
-    
-    public int quad(int x, int a, int b, int c){
-        return (a * x * x) + (b * x) + c;
+        for(int i = 0; i < nums.length; ++i){
+            nums[i] = pq.poll();
+        }
+        
+        return nums;
     }
 }

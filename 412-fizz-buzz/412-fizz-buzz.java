9b3b3a659a7ae1b25426f9193baff84486037818
Akash Jairam <Quasar2197@gmail.com>
2022-07-07 08:09:33 -0500

    Time: 6 ms (6.61%), Space: 48.5 MB (41.01%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int idx = 0;
+        String fizz = "Fizz";
+        String buzz = "Buzz";
+        List<String> res = new ArrayList<>();
+        for(int i = 1; i <= n; ++i){
+            boolean divideBy3 = i % 3 == 0;
+            boolean divideBy5 = i % 5 == 0;
+            if(divideBy3 && divideBy5)
+                res.add(fizz + buzz);
+            else if(divideBy3)
+                res.add(fizz);
+            else if(divideBy5)
+                res.add(buzz);
+            else
+                res.add(String.valueOf(i));
+        }
+        
+        return res;
+    }
+}

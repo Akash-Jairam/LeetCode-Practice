1f6cbf78aeda0a6976ecff7b4934795dbeb4b7fb
Akash Jairam <Quasar2197@gmail.com>
2022-07-06 19:29:04 -0500

    Time: 28 ms (12.06%), Space: 48.3 MB (76.05%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,33 +1,32 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] inDegree = new int[numCourses];
-        List<Integer> order = new ArrayList<>();
         for(int[] pre : prerequisites){
             inDegree[pre[0]]++;
         }
-        HashSet<Integer> zeroDegree = new HashSet<>();
         
+        HashSet<Integer> zeroDegree = new HashSet();
         for(int i = 0; i < inDegree.length; ++i){
             if(inDegree[i] == 0){
                 zeroDegree.add(i);
-                order.add(i);
             }
         }
         
         if(zeroDegree.isEmpty())
             return new int[]{};
         
+        List<Integer> list = new ArrayList<>();
+        
         while(!zeroDegree.isEmpty()){
-            Iterator<Integer> it = zeroDegree.iterator();
-            int curr = it.next();
+            int curr = zeroDegree.iterator().next();
             zeroDegree.remove(curr);
+            list.add(curr);
             for(int[] pre : prerequisites){
                 if(pre[1] == curr){
                     inDegree[pre[0]]--;
                     if(inDegree[pre[0]] == 0){
                         zeroDegree.add(pre[0]);
-                        order.add(pre[0]);
-                    }
+                    } 
                 }
             }
         }
@@ -36,7 +35,7 @@ class Solution {
             if(inDegree[i] != 0)
                 return new int[]{};
             
-            inDegree[i] = order.get(i);
+            inDegree[i] =  list.get(i);
         }
         
         return inDegree;

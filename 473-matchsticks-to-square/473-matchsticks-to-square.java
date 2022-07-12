b6ebc67e01ae411f01927707d6fd2bf2f1bc6354
Akash Jairam <Quasar2197@gmail.com>
2022-07-12 07:58:57 -0500

    Time: 213 ms (39.90%), Space: 40.3 MB (89.51%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -12,21 +12,28 @@ class Solution {
         return canPartition(matchsticks ,new boolean[matchsticks.length], 0, 4, 0, sum / 4);
     }
     
-    public boolean canPartition(int[] nums, boolean[] used, int start, int k, int curSum, int targetSum){
+    public boolean canPartition(int[] matches, boolean[] used, int start, int k, int curSum, int targetSum){
         if(k == 1)
             return true;
         
-        if(curSum > targetSum)
+        if(curSum > targetSum){
             return false;
+        }
         
-        if(curSum == targetSum)
-            return canPartition(nums, used, 0, k-1, 0, targetSum);
         
-        for(int i = start; i < nums.length; ++i){
-            if(used[i]) continue;
+        
+        if(curSum == targetSum){
+            return canPartition(matches, used, 0, k-1, 0, targetSum);
+        }
+        
+        for(int i = start; i < matches.length; ++i){
+            if(used[i])
+                continue;
+            
             used[i] = true;
-            if(canPartition(nums, used, i+1, k, curSum + nums[i], targetSum))
+            if(canPartition(matches, used, i + 1, k, curSum + matches[i], targetSum))
                 return true;
+            
             used[i] = false;
         }
         

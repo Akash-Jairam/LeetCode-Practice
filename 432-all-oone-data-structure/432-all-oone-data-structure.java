7dd4306f3b4cc4299446900ae0a2297a460028dc
Akash Jairam <Quasar2197@gmail.com>
2022-07-16 18:47:37 -0500

    Time: 110 ms (43.39%), Space: 107.1 MB (16.03%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.java
+++ 432-all-oone-data-structure/432-all-oone-data-structure.java
@@ -1,70 +1,61 @@
 class AllOne {
-    ValueNode head,  tail;
-    Map<String, ValueNode> keys;
-    
+    ValueNode head, tail;
+    LinkedHashMap<String, ValueNode> map;
     public AllOne() {
         head = new ValueNode(0);
         tail = new ValueNode(0);
         head.next = tail;
         tail.prev = head;
-        keys = new HashMap<>();
+        map = new LinkedHashMap<>();
     }
     
     public void inc(String key) {
-        ValueNode node = keys.getOrDefault(key, head);
-        ValueNode vn = node.next;
-        
-        if(vn.val != node.val + 1){
-            vn = new ValueNode(node.val + 1);
-            vn.insertAt(node.next);
+        ValueNode curr = map.getOrDefault(key, head);
+        ValueNode vn = curr.next;
+        if(vn.val != curr.val + 1){
+            vn = new ValueNode(curr.val + 1);
+            vn.insertAt(curr.next);
         }
-        
         vn.keys.add(key);
-        keys.put(key, vn);
-        if(node != head) node.remove(key);
+        map.put(key, vn);
+        if(curr != head)
+            curr.remove(key);
     }
     
     public void dec(String key) {
-        ValueNode node = keys.get(key);
+        ValueNode node = map.get(key);
         if(node == null)
             return;
         
         if(node.val == 1){
             node.remove(key);
-            keys.remove(key);
+            map.remove(key);
+            return;
         }
         
         ValueNode prev = node.prev;
-        if(prev.val != node.val - 1){
-            prev = new ValueNode(node.val - 1);
+        if(prev.val != node.val -1){
+            prev = new ValueNode(node.val -1);
             prev.insertAt(node);
         }
         prev.keys.add(key);
-        keys.put(key, prev);
+        map.put(key, prev);
         node.remove(key);
     }
     
     public String getMaxKey() {
-        if(tail.prev != head){
-            return tail.prev.keys.iterator().next();
-        }
-        
-        return "";
+        return tail.prev != head ? tail.prev.keys.iterator().next() : ""; 
     }
     
     public String getMinKey() {
-        if(head.next != tail){
-            return head.next.keys.iterator().next();
-        }
-        
-        return "";
+        return head.next != tail ? head.next.keys.iterator().next() : "";
     }
 }
 
 class ValueNode{
-    ValueNode next, prev;
+    ValueNode prev, next;
+    LinkedHashSet<String> keys;
     int val;
-    Set<String> keys;
     
     public ValueNode(int val){
         this.val = val;
@@ -72,10 +63,12 @@ class ValueNode{
     }
     
     public void insertAt(ValueNode node){
-        next = node;
-        prev = node.prev;
+        ValueNode prev = node.prev;
+        ValueNode next = node.next;
         prev.next = this;
-        next.prev = this;
+        this.prev = prev;
+        this.next = node;
+        node.prev = this;
     }
     
     public void remove(String key){
@@ -83,10 +76,7 @@ class ValueNode{
         if(keys.isEmpty()){
             prev.next = next;
             next.prev = prev;
-            return;
         }
-        
-        
     }
 }
 

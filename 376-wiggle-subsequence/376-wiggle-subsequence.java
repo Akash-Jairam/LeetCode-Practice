ce5480358afe8ca1573bb23daa99dbf9b92f82f9
Akash Jairam <Quasar2197@gmail.com>
2022-07-15 20:01:06 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (12.98%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,30 +1,29 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        if(nums.length <= 2)
+        if(nums.length < 2)
             return 1;
         
-        int max = 1;
-        int count = 1;
         Boolean wasPositive = null;
-        for(int i = nums.length -1; i >= 1; --i){
+        int count = 1, max = 1;
+        
+        for(int i = 1; i < nums.length; ++i){
             int diff = nums[i] - nums[i-1];
-            if(wasPositive == null){
-                if(diff > 0){
-                    wasPositive = true;
-                     ++count;
-                } else if(diff < 0){
+            if(wasPositive != null){
+                if(wasPositive && diff < 0){
+                    ++count;
                     wasPositive = false;
-                     ++count;
+                } else if(!wasPositive && diff > 0){
+                    ++count;
+                    wasPositive = true;
                 }
-               
-            } else if(wasPositive && diff < 0){
-                ++count;
-                wasPositive = false;
-            } else if(!wasPositive && diff > 0){
-                ++count;
-                wasPositive = true;
-            } else {
-                max = Math.max(max, count);
+            } else{
+               if(diff > 0){
+                   wasPositive = true;
+                   ++count;
+               } else if(diff < 0){
+                   wasPositive = false;
+                   ++count;
+               }
             }
         }
         

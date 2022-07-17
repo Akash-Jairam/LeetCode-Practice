70651a917d3dce1a45e7cf463bf3ad4002815f4b
Akash Jairam <Quasar2197@gmail.com>
2022-07-17 09:30:09 -0500

    Time: 128 ms (24.69%), Space: 105.3 MB (37.98%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.java
+++ 432-all-oone-data-structure/432-all-oone-data-structure.java
@@ -1,74 +1,94 @@
 class AllOne {
+    // Create nodes for the head and tail
     ValueNode head, tail;
+    // Create a linkedhashmap to store each string and the node that they are located on
     LinkedHashMap<String, ValueNode> map;
+    
     public AllOne() {
-        head = new ValueNode(0);
-        tail = new ValueNode(0);
+        // Create head and tail as blank nodes and point them to each other
+        // Create the linked hashmap
+        this.head = new ValueNode(0);
+        this.tail = new ValueNode(0);
         head.next = tail;
         tail.prev = head;
         map = new LinkedHashMap<>();
     }
     
     public void inc(String key) {
+        // Try to get the node from the map
+        // If the node is not in the map, get the head
+        // If the value of the node next to the current node is not 1 more than current
+        // Remove the key from the curr node if the current node is not the head
+        // Add the key to the next node and put the key into the map with the next node
         ValueNode curr = map.getOrDefault(key, head);
-        ValueNode vn = curr.next;
-        if(vn.val != curr.val + 1){
-            vn = new ValueNode(curr.val + 1);
+        if(curr.next.val != curr.val + 1){
+            ValueNode vn = new ValueNode(curr.val + 1);
             vn.insertAt(curr.next);
         }
-        vn.keys.add(key);
-        map.put(key, vn);
+        ValueNode next = curr.next;
         if(curr != head)
             curr.remove(key);
+        next.keys.add(key);
+        map.put(key, next);
+        
     }
     
     public void dec(String key) {
-        ValueNode node = map.get(key);
-        if(node == null)
+        ValueNode curr = map.get(key);
+        if(curr == null){
             return;
-        
-        if(node.val == 1){
-            node.remove(key);
+        }
+        if(curr.val == 1){
+            curr.remove(key);
             map.remove(key);
             return;
         }
         
-        ValueNode prev = node.prev;
-        if(prev.val != node.val -1){
-            prev = new ValueNode(node.val -1);
-            prev.insertAt(node);
+        if(curr.prev.val != curr.val -1){
+            ValueNode vn = new ValueNode(curr.val - 1);
+            vn.insertAt(curr);
         }
+        
+        ValueNode prev = curr.prev;
+        curr.remove(key);
         prev.keys.add(key);
         map.put(key, prev);
-        node.remove(key);
     }
     
     public String getMaxKey() {
-        return tail.prev != head ? tail.prev.keys.iterator().next() : ""; 
+        if(tail.prev != head){
+            return tail.prev.keys.iterator().next();
+        }
+        
+        return "";
     }
     
     public String getMinKey() {
-        return head.next != tail ? head.next.keys.iterator().next() : "";
+        if(head.next != tail){
+            return head.next.keys.iterator().next();
+        }
+        
+        return "";
     }
 }
 
-class ValueNode{
+public class ValueNode{
     ValueNode prev, next;
-    LinkedHashSet<String> keys;
     int val;
+    LinkedHashSet<String> keys;
+    
     
     public ValueNode(int val){
         this.val = val;
-        this.keys = new LinkedHashSet<>();
+        keys = new LinkedHashSet<>();
     }
     
-    public void insertAt(ValueNode node){
-        ValueNode prev = node.prev;
-        ValueNode next = node.next;
+    public void insertAt(ValueNode next){
+        ValueNode prev = next.prev;
         prev.next = this;
         this.prev = prev;
-        this.next = node;
-        node.prev = this;
+        this.next = next;
+        next.prev = this;
     }
     
     public void remove(String key){
@@ -79,6 +99,16 @@ class ValueNode{
         }
     }
 }
+
+/*
+- Create a doubly linked list that will represent each count and store strings that have that count in a LinkedHashSet
+
+- This class will have a two methods:
+    -insertAt - which will accept a node and insert the current node between the given node and it's predecessor
+    
+    -remove - this will accept a given key and remove it from the LinkedHashSet. If the set is empty, we will remove the node itself from the list
+
+*/
 
 /**
  * Your AllOne object will be instantiated and called as such:

cfe0499aa72f43cde84d23c4320a3d79bc291929
Akash Jairam <Quasar2197@gmail.com>
2022-07-27 18:24:27 -0500

    Time: 29 ms (94.60%), Space: 49.3 MB (85.64%) - LeetHub

--- 244-shortest-word-distance-ii/244-shortest-word-distance-ii.java
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.java
@@ -1,44 +1,23 @@
 class WordDistance {
+    
     HashMap<String, List<Integer>> map;
-    HashMap<String, Integer> cache;
     public WordDistance(String[] wordsDict) {
         map = new HashMap<>();
-        cache = new HashMap<>();
-        
         for(int i = 0; i < wordsDict.length; ++i){
-            map.putIfAbsent(wordsDict[i], new ArrayList<>());
-            map.get(wordsDict[i]).add(i);
+           map.putIfAbsent(wordsDict[i], new ArrayList<>());
+           map.get(wordsDict[i]).add(i);
         }
     }
     
     public int shortest(String word1, String word2) {
-        String cacheKey = word1 + "::" + word2;
-        if(cache.containsKey(cacheKey))
-            return cache.get(cacheKey);
+       int min = Integer.MAX_VALUE;
+       for(int i : map.get(word1)){
+           for(int j : map.get(word2)){
+               min = Math.min(min, Math.abs(i-j));
+           }
+       }
         
-        List<Integer> w1 = map.get(word1);
-        List<Integer> w2 = map.get(word2);
-        int i = 0, j = 0, min = Integer.MAX_VALUE;
-        
-        while(i < w1.size() && j < w2.size()){
-            int w1Idx = w1.get(i);
-            int w2Idx = w2.get(j);
-            if(w1Idx > w2Idx){
-                min = Math.min(min, w1Idx - w2Idx);
-                ++j;
-            } else{
-                min = Math.min(min, w2Idx - w1Idx);
-                ++i;
-            }
-            
-            if(min == 1){
-                cache.put(cacheKey, min);
-                return min;
-            }
-        }
-        
-        cache.put(cacheKey, min);
-        return min;
+       return min;
     }
 }
 

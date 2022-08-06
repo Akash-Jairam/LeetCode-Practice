dc9032b2a13559a7e76f6d96100024568049bd96
Akash Jairam <Quasar2197@gmail.com>
2022-08-06 15:08:09 -0500

    Time: 214 ms (32.70%), Space: 112.8 MB (38.51%) - LeetHub

--- 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
@@ -1,33 +1,25 @@
 class Solution {
     public List<String> alertNames(String[] keyName, String[] keyTime) {
-        List<String> res = new ArrayList<>();
         HashMap<String, List<Integer>> map = new HashMap<>();
-        
+        List<String> res = new ArrayList<>();
         for(int i = 0; i < keyName.length; ++i){
+            String[] currTime = keyTime[i].split(":");
             map.putIfAbsent(keyName[i], new ArrayList<>());
-            map.get(keyName[i]).add(convertToMins(keyTime[i]));
+            map.get(keyName[i]).add((Integer.parseInt(currTime[0]) * 60) + Integer.parseInt(currTime[1]));
         }
         
-        for(String name: map.keySet()){
+        for(String name : map.keySet()){
             List<Integer> times = map.get(name);
             Collections.sort(times);
-            int count = 1;
             for(int i = 0; i < times.size() - 2; ++i){
                 if(Math.abs(times.get(i) - times.get(i+2)) <= 60){
                     res.add(name);
                     break;
                 }
             }
-            
         }
         
         Collections.sort(res);
         return res;
-    }
-    
-    public int convertToMins(String keyTime){
-        int colon = keyTime.indexOf(":");
-        
-        return Integer.parseInt(keyTime.substring(0, colon)) * 60 + Integer.parseInt(keyTime.substring(colon + 1));
     }
 }

2b9bbf6d00f60213435cc105b7046217215daa16
Akash Jairam <Quasar2197@gmail.com>
2022-08-30 21:29:18 -0500

    Time: 5 ms (92.97%), Space: 55 MB (44.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,27 +1,23 @@
 class Solution {
-    int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0,1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        // We will use a dfs approach
-        // First, we will create a list of lists to hold our results
-        // We will perform null checks on our given array to ensure that it is not empty and return the empty list if it is
-        // Create variables to track the row and the column limits
-        // Create an array to track which nodes we have visited for both the pacific and atlantic ocean
-        // Create an array that will hold all the possible directions we can move in
-        // Loop through our array's rows and call dfs passing in the current row and column and the pacific or atlantic  array, -1 (the lowest possible height), and the height array
-        // The start column for pacific ocean will be 0 and the start row for atlantic will be 1
-        // Do the same for the columns where the start column for pacific is 0 and the oen for atlantic is the last one
-        // Loop through the elements of our height array again and if both pacific visted and atlantic visited are true, we will add the list of that coordinate to our result
+        // Create a list of lists to hold our results
+        // Do a null check and return list of lists if needed
+        //  Get the row and column limits of the given graph
+        // Create  boolean arrays using the limits to track whether we can reach both oceans
+        // Iterating through each row, We'll call our bfs function and pass in the graph, the curr row, 0 or column limit depending on which ocean we are tracking and the specific ocean array
+        // Do the same but for each column
+        // Iterate through each individual element of our graph and check to see if the binary flags for both arrays are true. If they are, add them to our result
         List<List<Integer>> res = new ArrayList<>();
-        if(heights == null || heights.length == 0 || heights[0].length == 0)
+        if(heights == null || heights.length == 0)
             return res;
         
-        int row = heights.length;
-        int col = heights[0].length;
+        int row = heights.length, col = heights[0].length;
         boolean[][] pacific = new boolean[row][col];
         boolean[][] atlantic = new boolean[row][col];
         
         for(int i = 0; i < row; ++i){
-            dfs(heights, i, 0, -1,  pacific);
+            dfs(heights, i, 0, -1, pacific);
             dfs(heights, i, col-1, -1, atlantic);
         }
         
@@ -33,18 +29,18 @@ class Solution {
         for(int i = 0; i < row; ++i){
             for(int j = 0; j < col; ++j){
                 if(pacific[i][j] && atlantic[i][j])
-                    res.add(List.of(i,j));
+                    res.add(List.of(i, j));
             }
         }
         
         return res;
     }
     
-    public void dfs(int[][] heights, int row, int col, int currHeight, boolean[][] ocean ){
+    public void dfs(int[][] heights, int row, int col, int prevHeight, boolean[][] ocean){
         if(row < 0 || row >= heights.length || col < 0 || col >= heights[0].length)
             return;
-        // Check to see if the given height is lower than the height of the given index
-        if(currHeight > heights[row][col] || ocean[row][col])
+        
+        if(prevHeight > heights[row][col] || ocean[row][col])
             return;
         
         ocean[row][col] = true;

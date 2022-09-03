2dea04929ed57d987e8fee9a89ddbcef221aeeac
Akash Jairam <Quasar2197@gmail.com>
2022-09-02 19:36:29 -0500

    Time: 5 ms (55.31%), Space: 43.1 MB (45.00%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] numsSameConsecDiff(int n, int k) {
+        List<Integer> curr = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        
+        for(int i = 2; i <= n; ++i){
+            List<Integer> curr2 = new ArrayList<>();
+            for(int x : curr){
+                int y = x % 10;
+                if(y + k < 10){
+                    curr2.add((x * 10) + y + k);
+                }
+                if(k > 0 && y - k >= 0){
+                    curr2.add((x * 10) + y - k);
+                }
+            }
+            curr = curr2;
+        }
+        int[] res = new int[curr.size()];
+        for(int i = 0; i < res.length; ++i){
+            res[i] = curr.get(i);
+        }
+        
+        return res;
+    }
+}

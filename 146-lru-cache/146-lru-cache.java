ff0e212fc539e1923c95561bb31118f1665a0998
Akash Jairam <Quasar2197@gmail.com>
2022-09-11 14:28:55 -0500

    Time: 107 ms (39.26%), Space: 124 MB (80.89%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,65 +1,71 @@
 class LRUCache {
-    final ListNode head = new ListNode(0,0);
-    final ListNode tail = new ListNode(0,0);
-    HashMap<Integer, ListNode> cache;
+
+    LinkedHashMap<Integer, Node> map = new LinkedHashMap<>();
+    Node head, tail;
     int capacity;
-    // Create pointers to the head and tail of our doubly linked list
-    // Create a hashmap that will store the key as the key and the ListNode as the value
     public LRUCache(int capacity) {
-        // The constructor will initialize our map and capacity
-        // It will also connect the head to the tail
         this.capacity = capacity;
-        this.cache = new HashMap(capacity);
-        this.head.next = this.tail;
-        this.tail.prev = this.head;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        if(this.cache.containsKey(key)){
-            ListNode node = this.cache.get(key);
-            remove(node);
-            insert(node);
-            return node.val;
+        Node curr = map.getOrDefault(key, null);
+        int val = -1;
+        if(curr != null){
+            curr.remove();
+            head.insertAt(curr);
+            val = curr.val;
         }
-        return -1;
+        return val;
     }
     
     public void put(int key, int value) {
-        if(this.cache.containsKey(key)){
-            remove(this.cache.get(key));
-        } else {
-            if(this.cache.size() == this.capacity)
-                remove(this.tail.prev);
+        Node curr = map.getOrDefault(key, null);
+        if(curr != null){
+            curr.val = value;
+            curr.remove();
+        } else{
+            curr = new Node(key, value);
+            map.put(key, curr);
         }
-        
-        insert(new ListNode(key, value));
+        head.insertAt(curr);
+        if(map.size() > capacity) {
+            map.remove(tail.prev.key);
+            tail.prev.remove();
+        }
+    }
+}
+
+class Node{
+    int key;
+    int val;
+    Node next;
+    Node prev;
+    
+    public Node(int key, int val){
+        this.key = key;
+        this.val = val;
     }
     
-    public void insert(ListNode node){
-        // First, we will inset the entry from our map using the key value stored in the node and the node itself
-        // Next, we will get the reference to the next element after the head
-        // We will assign the head's next to the given node and the node's prev to the head
-        // Similarly, we will assign the node's next to the reference and the reference's prev to the node
-        this.cache.put(node.key, node);
-        ListNode next = head.next;
-        head.next = node;
-        node.prev = head;
+    public void remove(){
+        this.prev.next = this.next;
+        this.next.prev = this.prev;
+    }
+    
+    public void insertAt(Node node){
+        Node next = this.next;
+        
+        this.next = node;
+        node.prev = this;
+        
         node.next = next;
         next.prev = node;
     }
-    
-    public void remove(ListNode node){
-        // First, we will remove the node from the cache using the node's key
-        // Second, we will find the reference to the node's next node and the node's previous node
-        // We'll set the node's next node's prev to point to the previous node
-        // We'll set the previous node's next to point to the next node
-        this.cache.remove(node.key);
-        ListNode next = node.next;
-        ListNode previous = node.prev;
-        previous.next = next;
-        next.prev = previous;
-    }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
@@ -67,15 +73,3 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
-// Create a ListNode (LinkedList class) that will hold a key, value of int type and pointers to the next and previous nodes
-public class ListNode{
-    int key;
-    int val;
-    ListNode next;
-    ListNode prev;
-    
-    public ListNode(int key, int val){
-        this.key = key;
-        this.val = val;
-    }
-}

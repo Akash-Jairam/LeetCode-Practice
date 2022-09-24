3c1f4673ec00b1ad326a8cd40f352a43ded7e8fb
Akash Jairam <Quasar2197@gmail.com>
2022-09-24 09:59:03 -0500

    Time: 60 ms (43.53%), Space: 55.3 MB (33.66%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,51 +1,32 @@
 class Solution {
+    int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
     public int shortestPath(int[][] grid, int k) {
-        if(grid.length == 0)
-            return -1;
-        
-        
-        int[][] directions = new int[][]{{-1, 0}, {1,0}, {0, -1}, {0, 1}};
-        boolean[][][] visited = new boolean[grid.length][grid[0].length][k+1];
-        int rowLimit = grid.length;
-        int colLimit = grid[0].length;
-        // Create an array of directions to track up, down, left or right
-        // Create a queue to iterate through our graph
-        // Add 0, 0, k as an array to our queue
-        // While our queue is not empty
-        // Get the current size of the queue
-        // Iterate through the queue according to its current size
-        // Take the number and update it based on the directions
-        // Check to see if it the direction is valid and if the queue has not been visited before
-        // Also check to see if we reach the end, if so, return our path, 
-        // Check to see if the current node is an obstacle and remove k if k > 0 before adding to the queue
+        int m = grid.length, n = grid[0].length;
+        boolean[][][] visited = new boolean[m][n][k+1];
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, k});
         int steps = 0;
-        visited[0][0][k] = true;
         while(!queue.isEmpty()){
             int size = queue.size();
             for(int i = 0; i < size; ++i){
-                int[] curr = queue.poll();
-                if(curr[0] == rowLimit -1 && curr[1] == colLimit -1)
-                    return steps;
+                int[]curr = queue.poll();
+                if(curr[0] == m - 1 && curr[1] == n - 1) return steps;
+                
                 for(int[] dir : directions){
-                    int row = curr[0] + dir[0];
-                    int col = curr[1] + dir[1];
-                    int kCount = curr[2];
-                    if(row >= 0 && row < rowLimit && col >= 0 && col < colLimit){
-                        if(grid[row][col] == 0 && visited[row][col][kCount] == false){
-                            queue.offer(new int[]{row, col, kCount});
-                            visited[row][col][kCount] = true;
-                        } else if(grid[row][col] == 1 && kCount > 0 && visited[row][col][kCount-1] == false){
-                            queue.offer(new int[]{row, col, kCount-1});
-                            visited[row][col][kCount-1] = true;
+                    int row = curr[0] + dir[0], col = curr[1] + dir[1], elims = curr[2];
+                    if(row >= 0 && row < m && col >= 0 && col < n){
+                        elims -= grid[row][col];
+                        if(elims >= 0 && !visited[row][col][elims]){
+                            visited[row][col][elims] = true;
+                            queue.offer(new int[]{row, col, elims});
                         }
                     }
                 }
             }
             ++steps;
-
         }
+        
         return -1;
     }
 }

402a10f276c4a7d4698c85bf8538fae790acbf28
Akash Jairam <Quasar2197@gmail.com>
2022-09-27 08:38:33 -0500

    Time: 113 ms (46.44%), Space: 82.4 MB (34.88%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        HashMap<String, List<Integer>> map = new HashMap<>();
         List<String> res = new ArrayList<>();
         int[] inDegree = new int[recipes.length];
+        HashMap<String, List<Integer>> map = new HashMap<>();
         
-        for(int i = 0; i < ingredients.size(); ++i){
-            for(String ing : ingredients.get(i)){
-                if(!map.containsKey(ing)) map.put(ing, new ArrayList<>());
+        for(int i = 0; i < recipes.length; ++i){
+            for(String in : ingredients.get(i)){
+                if(!map.containsKey(in)) map.put(in, new ArrayList<>());
                 
-                map.get(ing).add(i);
+                map.get(in).add(i);
             }
             inDegree[i] = ingredients.get(i).size();
         }
@@ -18,13 +18,12 @@ class Solution {
         
         while(!queue.isEmpty()){
             String curr = queue.poll();
-            
             if(map.containsKey(curr)){
                 for(int i : map.get(curr)){
                     inDegree[i]--;
                     if(inDegree[i] == 0){
                         res.add(recipes[i]);
-                        queue.add(recipes[i]);
+                        queue.offer(recipes[i]);
                     }
                 }
             }

c42565cc9e6d5da047b832e4e02f889856f6b8c9
Akash Jairam <Quasar2197@gmail.com>
2022-09-26 20:00:43 -0500

    Time: 43 ms (25.48%), Space: 54.6 MB (31.28%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,28 +1,38 @@
 class Solution {
-   public String pushDominoes(String d) {
-        d = 'L' + d + 'R';
+    public String pushDominoes(String dominoes) {
+        dominoes = 'L' + dominoes + 'R';
         StringBuilder res = new StringBuilder();
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d.charAt(j) == '.') continue;
+        
+        for(int i = 0, j = 1; j < dominoes.length(); ++j){
+            if(dominoes.charAt(j) == '.') continue;
+            
             int middle = j - i - 1;
-            if (i > 0)
-                res.append(d.charAt(i));
-            if (d.charAt(i) == d.charAt(j))
-                for (int k = 0; k < middle; k++)
-                    res.append(d.charAt(i));
-            else if (d.charAt(i) == 'L' && d.charAt(j) == 'R')
-                for (int k = 0; k < middle; k++)
+            if(i > 0)
+                res.append(dominoes.charAt(i));
+            if(dominoes.charAt(i) == dominoes.charAt(j)){
+                for(int k = 0; k < middle; ++k){
+                    res.append(dominoes.charAt(i));
+                }
+            } else if(dominoes.charAt(i) == 'L' && dominoes.charAt(j) == 'R'){
+                for(int k = 0; k < middle; ++k){
                     res.append('.');
-            else {
-                for (int k = 0; k < middle / 2; k++)
+                }
+            } else{
+                for(int k = 0; k < middle / 2 ; ++k){
                     res.append('R');
-                if (middle % 2 == 1)
+                }
+                
+                if(middle % 2 == 1)
                     res.append('.');
-                for (int k = 0; k < middle / 2; k++)
+                
+                for(int k = 0; k < middle / 2; ++k){
                     res.append('L');
+                }
             }
+            
             i = j;
         }
+        
         return res.toString();
     }
 }

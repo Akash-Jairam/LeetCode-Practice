caed8e34f1825bb61999e282e190921816a729b2
Akash Jairam <Quasar2197@gmail.com>
2022-11-01 19:59:14 -0500

    Time: 31 ms (94.23%), Space: 13.9 MB (86.98%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,34 @@
+from collections import deque
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        steps = 0
+        queue = deque()
+        queue.append(start)
+        
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                curr = queue.popleft()
+                if curr in bank:
+                    bank.remove(curr)
+                if curr == end:
+                    return steps
+                for cand in bank:
+                    if self.isNext(curr, cand):
+                        queue.append(cand)
+            
+            steps += 1
+        
+        return -1
+    
+    def isNext(self, curr, cand):
+        i, j, numDiff = 0, 0, 0
+        while i < len(curr) and j < len(cand) and numDiff <= 1:
+            if curr[i] != cand[j]:
+                numDiff += 1
+            i += 1
+            j += 1
+        
+        return numDiff == 1
+        
+        

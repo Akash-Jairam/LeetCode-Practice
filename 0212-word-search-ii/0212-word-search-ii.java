03395dc9ed313f4e5115dacadfc93f87feb37252
Akash Jairam <Quasar2197@gmail.com>
2022-11-05 18:06:16 -0500

    Time: 264 ms (61.50%), Space: 45.4 MB (40.01%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,62 @@
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        TrieNode root = new TrieNode();
+        for(String word : words){
+            buildTrie(root, word);
+        }
+        
+        for(int i = 0; i < board.length; ++i){
+            for(int j = 0; j < board[0].length; ++j){
+                char c = board[i][j];
+                if(root.children[c - 'a'] != null){
+                    dfs(board, i, j, res, root, visited);
+                }
+            }
+        }
+        return res;
+    }
+    
+    
+    public void dfs(char[][] board, int i, int j, List<String> res, TrieNode it, boolean[][] visited){
+        if(it.word != ""){
+           res.add(it.word);
+           it.word = "";
+        }
+        
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j])
+            return;
+        
+        char c = board[i][j];
+        if(it.children[c - 'a'] == null)
+            return;
+        
+        it = it.children[c - 'a'];
+        visited[i][j] = true;
+        dfs(board, i + 1, j, res, it, visited);
+        dfs(board, i - 1, j, res, it, visited);
+        dfs(board, i, j + 1, res, it, visited);
+        dfs(board, i, j - 1, res, it, visited);
+        
+        visited[i][j] = false;
+    }
+    
+    public void buildTrie(TrieNode root, String s){
+        TrieNode it = root;
+        
+        for(int i = 0; i < s.length(); ++i){
+            char c = s.charAt(i);
+            if(it.children[c - 'a'] == null)
+                it.children[c - 'a'] = new TrieNode();
+            
+            it = it.children[c - 'a'];
+        }
+        it.word = s;
+    }
+    
+    class TrieNode{
+        String word = "";
+        TrieNode[] children = new TrieNode[26];
+    }
+}

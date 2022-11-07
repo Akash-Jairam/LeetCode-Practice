4d7981da1f8c0938bd1523b35ddb4bb8f12649c7
Akash Jairam <Quasar2197@gmail.com>
2022-11-06 19:12:15 -0600

    Time: 63 ms (21.46%), Space: 13.8 MB (95.47%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -11,7 +11,7 @@ class Solution:
         
         if index_first_six > -1:
             to_add = int (3 * (math.pow(10, index_first_six)))
-            return num + to_add
+            num += to_add
         
         return num
         

dbf8eed12e79bd7ff08cbc421cbcb12fc16e26fc
Akash Jairam <Quasar2197@gmail.com>
2022-11-12 19:31:51 -0600

    Time: 31 ms (96.93%), Space: 13.9 MB (97.78%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -2,26 +2,21 @@ from collections import deque
 
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = []
+        words = deque()
         curr_word = []
         res = []
         for c in s:
             if c == ' ':
                 if curr_word:
-                    words.append(''.join(curr_word))
+                    words.appendleft(''.join(curr_word))
                     curr_word = []
             else:
                 curr_word.append(c)
         
         if curr_word:
-            words.append(''.join(curr_word))
+            words.appendleft(''.join(curr_word))
             
-        for i in range(len(words) - 1, -1, -1):
-            res.append(words[i])
-
-        
-        word = ' '.join(res)
-        return word
+        return ' '.join(words)
                 
         
         

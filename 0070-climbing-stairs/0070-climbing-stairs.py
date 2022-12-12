98d7201a8b3a769f9eeae2bc2f2d13ed8d6ebfc3
Akash Jairam <Quasar2197@gmail.com>
2022-12-11 21:03:38 -0600

    Time: 30 ms (93.74%), Space: 13.8 MB (96.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -8,7 +8,7 @@ class Solution:
             return 2
         
         dp = [0] * (n + 1)
-        dp[0], dp[1], dp[2] = 0, 1, 2
+        dp[1], dp[2] = 1, 2
         
         for i in range(3, n + 1):
             dp[i] = dp[i-2] + dp[i-1]

86d422dae147b039acef671a8eedf0b44838bf41
Akash Jairam <Quasar2197@gmail.com>
2023-01-01 21:16:24 -0600

    Time: 26 ms (97.99%), Space: 13.8 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,10 +1,10 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        
         num_caps = 0
         for c in word:
-            if str(c) in capitals:
+            if c.isupper():
                 num_caps += 1
         
-        return (word[0] in capitals and num_caps == 1) or num_caps == len(word) or num_caps == 0
+        return (word[0].isupper() and num_caps == 1) or num_caps == len(word) or num_caps == 0
         

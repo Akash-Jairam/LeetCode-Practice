d7302b67a695f0f65c04ec474aac65c14da35a00
Akash Jairam <Quasar2197@gmail.com>
2023-01-04 21:09:32 -0600

    Time: 3548 ms (16.92%), Space: 59.9 MB (70.29%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,10 +1,15 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        # Merge interval
+        # But instead we only take the common interval
+        # If we can't merge intervals, we increase our count
+        # Else we merge
+        # Return count plus the remaining interval to be merged
         points.sort()
        
-        i, curr, res = 1, points[0], 0
+        i, curr, res, n = 1, points[0], 0, len(points)
         
-        while i < len(points):
+        while i < n:
             if points[i][0] <= curr[1]:
                 curr[0], curr[1] = max(curr[0], points[i][0]), min(curr[1], points[i][1])
             else:

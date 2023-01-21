f573eade56be90e860592b74b22dfac7f0ee21ac
Akash Jairam <Quasar2197@gmail.com>
2023-01-20 19:11:50 -0600

    Time: 37 ms (80.65%), Space: 13.9 MB (79.81%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,23 +1,20 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        def backtrack(s, count, curr, rst):
-            if count == 4:
-                if not s: rst.append(curr[:-1])
+        def backtrack(s, ct, path, rst):
+            if ct == 4:
+                #have 4 chunk and use up all digits
+                if not s: rst.append(path[:-1]) 
                 return
-            
-            
             for i in range(1, 4):
-                if i > len(s):
-                    continue
-                
-                if i > 1 and s[0] == '0':
-                    continue
-                
+                #prevent index overflow
+                if i > len(s): continue
+                #take 1 digit is always good
+                #take 2 or 3 digits, first digit cannot be '0'
+                if i > 1 and s[0] == '0': continue
+                #take 3 digits, cannot greater than 255
                 if i > 2 and int(s[:3]) > 255: continue
-                
-                backtrack(s[i:], count + 1, curr + s[:i] +'.', rst)
+                backtrack(s[i:], ct+1, path + s[:i] + '.', rst)
             
-        
-        res = []
-        backtrack(s, 0, '', res)
-        return res
+        rst = []
+        backtrack(s, 0, '', rst)
+        return rst

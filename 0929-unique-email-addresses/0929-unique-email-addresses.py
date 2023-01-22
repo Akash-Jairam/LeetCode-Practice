51bb2a2231c71c885f0329fa9e11e16da15a5027
Akash Jairam <Quasar2197@gmail.com>
2023-01-22 11:12:01 -0600

    Time: 74 ms (49.42%), Space: 13.8 MB (97.06%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -1,5 +1,11 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
+        # Use a set to store emails and keep track of unique ones
+        # Iterate through each email by character
+        # If the curr char is a + or @ break
+        # else if the curr char is not a ., append to the list that is accumulating the locale
+        # Append the combination of the locale and the substring of emails from the '@' to the end to the set
+        # Return the size of the set at the end
         email_set = set()
         
         for email in emails:

e69751ca7dbee7940a317ddc551d54f63e466e0d
Akash Jairam <Quasar2197@gmail.com>
2023-01-22 22:34:18 -0600

    Time: 198 ms (39.30%), Space: 14.4 MB (69.28%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,12 +1,23 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        length, count = len(flowerbed), 0
-        for i in range(length):
-            if not flowerbed[i]: 
-                left_flower = True if i > 0 and flowerbed[i-1] else False
-                right_flower = True if i < length - 1 and flowerbed[i+1] else False
-                if not left_flower and not right_flower:
-                    flowerbed[i] = 1
-                    count += 1
+        # Dynamic Programming solution
+        length = len(flowerbed)
+        dp = [0] * length
         
-        return count >= n
+        for i in range(length):
+            left_flower_av = i == 0 or flowerbed[i-1] == 0
+            right_flower_av = i == length - 1 or flowerbed[i+1] == 0
+            
+            if left_flower_av and not flowerbed[i] and right_flower_av:
+                if i == 1:
+                    dp[i] = 1
+                else:
+                    prev_2 = 0 if i - 2 < 0 else dp[i-2]
+                    prev_1 = 1 if i - 1 < 0 else dp[i-1]
+                    dp[i] = max(prev_2 + 1, prev_1)
+            
+            else:
+                dp[i] = dp[i-1]
+        
+        return dp[length-1] >= n
+        

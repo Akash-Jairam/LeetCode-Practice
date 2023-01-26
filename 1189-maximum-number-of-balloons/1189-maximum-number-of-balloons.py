b0ce6be1f0a44f0473c9d6e80ac89d8c73d1611f
Akash Jairam <Quasar2197@gmail.com>
2023-01-26 17:42:35 -0600

    Time: 44 ms (41.35%), Space: 13.9 MB (86.64%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,12 +1,16 @@
 from collections import defaultdict
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        s, c_map = 'ban', defaultdict(int)
+        s, c_map, s_map = 'balloon', defaultdict(int), defaultdict(int)
+        min_val = sys.maxsize
         for c in text:
             c_map[c] += 1
         
         min_val = sys.maxsize
         for c in s:
-            min_val = min(min_val, c_map[c])
+            s_map[c] += 1
         
-        return min(min_val, min(c_map['l'] // 2, c_map['o'] // 2))
+        for c in s_map.keys():
+                min_val = min(min_val, c_map[c] // s_map[c])
+        
+        return min_val

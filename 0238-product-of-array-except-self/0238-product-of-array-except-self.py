748de7eddbe399b230a12eddb30fb81409491e2b
Akash Jairam <Quasar2197@gmail.com>
2023-01-28 10:04:27 -0600

    Time: 295 ms (37.29%), Space: 21.4 MB (49.60%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        #Use arrays to track the left product and right product
+        # Copy nums to our result array
+        # Create a variable to track our right product
+        # Iterate from right to left
+        # Update res[i] to the product of res[i] and right product
+        # Multiply right product by nums[i]
         n = len(nums)
-        left_prod, right_prod = [1] * n, [1] * n
-        res = []
-        start, end = 1, n - 2
-        left_prod[0] = nums[0]
-        right_prod[-1] = nums[-1]
+        res = [1] * n
+        res[0] = 1
         
-        while start < n and end >= 0:
-            left_prod[start] = nums[start] * left_prod[start - 1]
-            right_prod[end] = nums[end] * right_prod[end + 1]
-            start += 1
-            end -= 1
-            
-        for i in range(n):
-            curr_prod = 1
-            if i - 1 >= 0:
-                curr_prod *= left_prod[i-1]
-            
-            if i + 1 < n:
-                curr_prod *= right_prod[i+1]
-            
-            res.append(curr_prod)
+        for i in range(1, n):
+            res[i] = res[i-1] * nums[i-1]
+        
+        r_prod = 1
+        
+        for i in range(n - 1, -1, -1):
+            res[i] *= r_prod
+            r_prod *= nums[i]
         
         return res

ce2e0d45ecc0b2f75c3fef8c03f29aaaad18bdfe
Akash Jairam <Quasar2197@gmail.com>
2023-01-28 09:15:51 -0600

    Time: 55 ms (8.70%), Space: 14 MB (13.20%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,24 +1,23 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        word_map = {}
-        strings = s.split(' ')
-        if len(pattern) != len(strings):
-            return False;
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
         
-        for i in range(len(strings)):
+        c_map = {}
+        w_map = {}
+        
+        for i in range(len(pattern)):
             c = pattern[i]
-            w = strings[i]
-
-            char_key = 'char_{}'.format(c)
-            char_word = 'word_{}'.format(w)
+            word = words[i]
             
-            if char_key not in word_map:
-                word_map[char_key] = i
-            
-            if char_word not in word_map:
-                word_map[char_word] = i 
-            
-            if word_map[char_key] != word_map[char_word]:
+            if c in c_map and c_map[c] != word:
                 return False
+            
+            if word in w_map and w_map[word] != c:
+                return False
+            
+            w_map[word] = c
+            c_map[c] = word
         
         return True

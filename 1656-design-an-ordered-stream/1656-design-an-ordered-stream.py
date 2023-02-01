95a3ccf8d039a2e03921402a66634d1bc4032e43
Akash Jairam <Quasar2197@gmail.com>
2023-01-31 21:06:47 -0600

    Time: 209 ms (93.62%), Space: 14.8 MB (9.24%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,19 +1,17 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.key_map = {}
-        self.max_key = n
-        self.curr_key = 1
+        self.stream = [""] * (n+1)
+        self.idx = 1
 
     def insert(self, idKey: int, value: str) -> List[str]:
-        self.key_map[idKey] = value
+        self.stream[idKey] = value
         curr = []
-        while self.curr_key <= self.max_key and self.curr_key in self.key_map:
-            curr.append(self.key_map[self.curr_key])
-            self.curr_key += 1
+        while self.idx < len(self.stream) and self.stream[self.idx]:
+            curr.append(self.stream[self.idx])
+            self.idx += 1
         
         return curr
-            
 
 
 # Your OrderedStream object will be instantiated and called as such:

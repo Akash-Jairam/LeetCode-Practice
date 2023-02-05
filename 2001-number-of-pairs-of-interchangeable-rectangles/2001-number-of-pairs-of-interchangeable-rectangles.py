ff71d78f10c328d06d2ee784926d00256a62aaa6
Akash Jairam <Quasar2197@gmail.com>
2023-02-05 10:14:42 -0600

    Time: 2033 ms (31.66%), Space: 70.1 MB (47.49%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -1,15 +1,19 @@
 from collections import defaultdict
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        # Use a map to keep track of all the rectangles with the same ratio
+        # Use the combination formula to get the number of combinations for a given ratio
+        # (n!) / ((n - k!) * k!) 
+        # where n is the count for a given ratio and k is the size of an individual combo (2 in this case)
         ratio_map = defaultdict(int)
         num_pairs = 0
-        for x, y in rectangles:
-            ratio_map[x/y] += 1
+        for w, h in rectangles:
+            ratio_map[w/h] += 1
             
             
         
         for val in ratio_map.values():
-            for i in range(val):
-                num_pairs += (val - i - 1)
+            if val > 1:
+                num_pairs += math.factorial(val) // (math.factorial(val - 2) * math.factorial(2))
         
         return num_pairs

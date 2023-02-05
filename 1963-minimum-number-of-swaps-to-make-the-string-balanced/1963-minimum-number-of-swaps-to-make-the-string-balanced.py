acf48e89d0379c12e64b951fd82774f0db405845
Akash Jairam <Quasar2197@gmail.com>
2023-02-05 09:51:26 -0600

    Time: 847 ms (37.02%), Space: 25.5 MB (43.18%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,27 +1,13 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        num_left, num_right = 0, 0
-        left, right = 0, len(s) - 1
-        swaps = 0
+        num_close, max_close = 0, 0
         
-        while left < right:
-            if s[left] == '[':
-                num_left += 1
-            elif num_left:
-                num_left -= 1
+        for c in s:
+            if c == '[':
+                num_close -= 1
             else:
-                while right > left:
-                    if s[right] == ']':
-                        num_right += 1
-                    elif num_right:
-                        num_right -= 1
-                    else:
-                        num_left += 1
-                        num_right += 1
-                        right -= 1
-                        swaps += 1
-                        break
-                    right -= 1
-            left += 1
+                num_close += 1
+            
+            max_close = max(max_close, num_close)
         
-        return swaps
+        return (max_close + 1) // 2

9276e68651f253c293fbf2dffea45e46cebb7193
Akash Jairam <Quasar2197@gmail.com>
2023-02-07 09:22:59 -0600

    Time: 1072 ms (49.83%), Space: 33.2 MB (69.06%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,5 +1,10 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # Use a map to keep track of remainders and the index where they were found
+        # Also use a variable to track the running sum
+        # If you found a remainder that is already in the map and there is at least a two index gap between the map's idx and the curr idx, return true
+        # This is because a multiple of k was found between the gap
+        # Return false at the end of the func
         remainder = {0: -1}
         total = 0
         

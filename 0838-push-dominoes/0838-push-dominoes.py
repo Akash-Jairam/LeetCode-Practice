3b23bf684d3bb65e74929d7d88fe3b2c45a28f97
Akash Jairam <Quasar2197@gmail.com>
2023-02-07 13:49:44 -0600

    Time: 325 ms (59.81%), Space: 22.4 MB (25.54%) - LeetHub

---
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        queue = deque()
+        doms = list(dominoes)
+        
+        for i, v in enumerate(doms):
+            if v != '.':
+                queue.append((i, v))
+        
+        while queue:
+            i, v = queue.popleft()
+            
+            if v == 'L' and i > 0 and doms[i-1] == '.':
+                queue.append((i-1, 'L'))
+                doms[i-1] = 'L'
+            elif v == 'R':
+                if i + 1 < len(doms) and doms[i+1] == '.':
+                    if i + 2 < len(doms) and doms[i+2] == 'L':
+                        queue.popleft()
+                    else:
+                        queue.append((i+1, 'R'))
+                        doms[i+1] = 'R'
+        return ''.join(doms)
+                    

9566c04d481f6f3fd4e74b40ad905f9857edeaf6
Akash Jairam <Quasar2197@gmail.com>
2023-02-07 15:43:12 -0600

    Time: 82 ms (48.17%), Space: 27.6 MB (36.36%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        # Get every 10 char seq and update its count in a hashmap
+        # Return a list of the ones with a count greater than 1
         s_map = defaultdict(int)
         for i in range(len(s) - 9):
             s_map[s[i:i+10]] += 1

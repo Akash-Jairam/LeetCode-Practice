499bf9c8f68d2feee6480a89e7fc53837b2fa18e
Akash Jairam <Quasar2197@gmail.com>
2023-02-07 15:24:23 -0600

    Time: 313 ms (66.27%), Space: 22.2 MB (37.90%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.py
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -4,23 +4,22 @@ class Solution:
         queue = deque()
         doms = list(dominoes)
         
-        for i, v in enumerate(doms):
+        for i, v in enumerate(dominoes):
             if v != '.':
                 queue.append((i, v))
         
         while queue:
             i, v = queue.popleft()
-            
             if v == 'L' and i > 0 and doms[i-1] == '.':
-                queue.append((i-1, 'L'))
                 doms[i-1] = 'L'
+                queue.append((i - 1, 'L'))
             elif v == 'R':
                 if i + 1 < len(doms) and doms[i+1] == '.':
-                    # If standing dom is in between left and right like 'R.L', don't add to queue
                     if i + 2 < len(doms) and doms[i+2] == 'L':
                         queue.popleft()
                     else:
-                        queue.append((i+1, 'R'))
                         doms[i+1] = 'R'
+                        queue.append((i+1, 'R'))
+            
+        
         return ''.join(doms)
-                    

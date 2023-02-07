ad012494643f9316a91235089ab68441b13a1ff6
Akash Jairam <Quasar2197@gmail.com>
2023-02-06 21:10:55 -0600

    Time: 896 ms (84.34%), Space: 20.2 MB (51.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,24 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        f_map = {}
-        curr, res = 0, 0
+        counter, i = {}, 0
+        n = len(fruits)
         
-        for i in range(len(fruits)):
-            f = fruits[i]
-            if f not in f_map and len(f_map) == 2:
-                max_key = -1
-                max_val = float('inf')
-                min_key = -1
-                for k, v in f_map.items():
-                    if v < max_val:
-                        max_val = v
-                        max_key = k
-                del f_map[max_key]
-                curr = i - max_val
-                f_map[f] = i
-            else:
-                f_map[f] = i
-                curr += 1
-            res = max(res, curr)
+        for j in range(n):
+            f = fruits[j]
+            counter[f] = counter.get(f, 0) + 1
+            
+            if len(counter) > 2:
+                counter[fruits[i]] -= 1
+                if counter[fruits[i]] == 0:
+                    del counter[fruits[i]]
+                i += 1
         
-        return res
+        return n - 1 - i + 1

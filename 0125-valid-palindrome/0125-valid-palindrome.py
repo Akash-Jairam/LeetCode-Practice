7c27ef51748b6f9ecd86ab87f925b35ca601517e
Akash Jairam <Quasar2197@gmail.com>
2023-02-08 07:41:35 -0600

    Time: 50 ms (66.74%), Space: 15.3 MB (33.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        c_list = [c for c in s.lower() if c.isalpha() or c.isnumeric()] 
+        c_list = [c for c in s.lower() if c.isalnum()] 
         new_s = ''.join(c_list)
         return new_s == new_s[::-1]

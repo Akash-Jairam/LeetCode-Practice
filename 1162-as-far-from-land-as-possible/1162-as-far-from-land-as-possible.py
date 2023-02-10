571679596594fb8ed7857743201797fe29215ed7
Akash Jairam <Quasar2197@gmail.com>
2023-02-09 19:54:31 -0600

    Time: 580 ms (75.18%), Space: 15.5 MB (28.06%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,23 +1,30 @@
+from collections import deque
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
         queue = deque()
+        res = 0
+        m, n = len(grid), len(grid[0])
+        directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    queue.append([i, j])
         
-        for r,c in product(range(ROWS), range(COLS)): # prepopulate BFS queue with sources
-            if grid[r][c]: queue.append((r,c))
-
-        if not queue or ROWS * COLS == len(queue): # no land or all land
+        if not queue or len(queue) == m * n:
             return -1
         
         visited = set()
-        level = -1
-        while queue: # count number of BFS layers
-            for _ in range(len(queue)):
-                r,c = queue.popleft()
-                for dr,dc in (0,1),(1,0),(0,-1),(-1,0):
-                    nr,nc = r+dr,c+dc
-                    if ROWS > nr >= 0 <= nc < COLS and not grid[nr][nc] and (nr,nc) not in visited:
-                        visited.add((nr,nc))
-                        queue.append((nr,nc))
-            level += 1
-        return -1 if level == 0 else level
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r, c = queue.popleft()
+                for up, down in directions:
+                    curr_r = r + up
+                    curr_c = c + down
+                    if m > curr_r >= 0 <= curr_c < n and grid[curr_r][curr_c] == 0 and (curr_r, curr_c) not in visited:
+                        queue.append([curr_r, curr_c])
+                        visited.add((curr_r, curr_c))
+            
+            res += 1
+        
+        return res - 1

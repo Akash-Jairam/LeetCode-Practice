a77080d297e6bab147735e6f7611a126e7d4657c
Akash Jairam <Quasar2197@gmail.com>
2023-02-13 13:44:38 -0600

    Time: 41 ms (80.91%), Space: 14.2 MB (48.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,38 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binarySearch(arr, left, right, target):
-            while left <= right:
-                mid = left + (right - left) // 2
-                if nums[mid] > target:
-                    right = mid - 1
-                elif nums[mid] < target:
-                    left = mid + 1
-                else:
-                    return mid
-            
-            return -1
-        
-        minimum = nums[0]
-        minIdx = 0
-        l, r = 0, len(nums) - 1
+        l,r = 0, len(nums) - 1
         
         while l <= r:
-            if nums[l] < nums[r]:
-                if nums[l] < minimum:
-                    minimum = nums[l]
-                    minIdx = l
-                break
+            mid = l + (r-l) // 2
+            if nums[mid] == target:
+                return mid
             
-            
-            mid = l + (r - l) // 2
-            if nums[mid] < minimum:
-                    minimum = nums[mid]
-                    minIdx = mid
-            if nums[mid] >= nums[l]:
-                l = mid + 1
+            if nums[l] <= nums[mid]:
+                if nums[mid] < target or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
             else:
-                r = mid - 1
+                if nums[mid] > target or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
         
-        idx = binarySearch(nums, minIdx, len(nums) - 1, target)
-        
-        return idx if idx != -1 else binarySearch(nums, 0, minIdx - 1, target)
+        return -1

96cdd984482fb34e0f01b71f4309da0085b272f4
Akash Jairam <Quasar2197@gmail.com>
2023-02-25 10:00:55 -0600

    Time: 222 ms (29.97%), Space: 33.6 MB (6.40%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,10 +1,10 @@
 class Trie:
-
+    
     class Node:
         def __init__(self):
-            self.children = {}
+            self.children = [None] * 26
             self.word = None
-        
+            
     def __init__(self):
         self.root = self.Node()
 
@@ -12,32 +12,36 @@ class Trie:
         it = self.root
         
         for c in word:
-            it.children[c] = it.children.get(c, self.Node())
-            it = it.children[c]
-        
+            idx = ord(c) - ord('a')
+            if not it.children[idx]:
+                it.children[idx] = self.Node()
+            
+            it = it.children[idx]
         it.word = word
-        
 
     def search(self, word: str) -> bool:
         it = self.root
         
         for c in word:
-            it = it.children.get(c, None)
-            if not it:
+            idx = ord(c) - ord('a')
+            if not it.children[idx]:
                 return False
+            
+            it = it.children[idx]
         
         return it.word == word
 
     def startsWith(self, prefix: str) -> bool:
         it = self.root
-        
-        for c in prefix:
-            it = it.children.get(c, None)
-            if not it:
-                return False
-        
-        return len(it.children) > 0 or it.word != None
 
+        for c in prefix:
+            idx = ord(c) - ord('a')
+            if not it.children[idx]:
+                return False
+            
+            it = it.children[idx]
+        
+        return True if it else False
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()

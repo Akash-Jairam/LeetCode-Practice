65f4157c7c6dcbbf8730043a74f815e7cbce8bbf
Akash Jairam <Quasar2197@gmail.com>
2023-02-26 10:42:18 -0600

    Time: 636 ms (44.10%), Space: 14.6 MB (12.56%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -3,6 +3,14 @@ import heapq
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        # Group the tasks by their count
+        # Create a max heap to sort the tasks by highest count
+        # Create a queue to hold the tasks that are waiting for their cooldown to finish
+        # Use a while loop that continues while either the heap or queue has elements
+        # Pop the current element from the heap, reduce the abs value by 1, if it is not 0, append to queue for cooldown
+        # Check the queue and pop all elements whose cooldown are <= curr time and add them to heap for processing
+        # Increment time
+        # Return time once finished
         tasks_by_count = Counter(tasks)
         max_heap = [-count for count in tasks_by_count.values()]
         heapq.heapify(max_heap)

c633c0d42f9d8dec3d9facaec02893ddfe7ced56
Akash Jairam <Quasar2197@gmail.com>
2023-03-26 19:02:23 -0500

    Time: 96 ms (63.66%), Space: 15.5 MB (68.66%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,45 @@
+class Uf:
+    
+    def __init__(self, n):
+        self.rank = [1] * n
+        self.parent = [i for i in range(n)]
+        
+    def find(self, x):
+        p = self.parent[x]
+        
+        while p != self.parent[p]:
+            self.parent[p] = self.parent[self.parent[p]]
+            p = self.parent[p]
+        
+        return p
+    
+    def union(self, x, y):
+        p1, p2 = self.find(x), self.find(y)
+        
+        if p1 == p2:
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = self.parent[p1]
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.rank[p2] += self.rank[p1]
+            self.parent[p1] = self.parent[p2]
+            
+        return True
+        
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False 
+        
+        uf = Uf(n)
+        
+        for x, y in edges:
+            if not uf.union(x, y):
+                return False
+            
+        return True
+            
+        
+        

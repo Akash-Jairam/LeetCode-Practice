2175f1c8e69e6f9f7447cf2736b4b1eb9a24f8bd
Akash Jairam <Quasar2197@gmail.com>
2023-07-01 00:04:34 -0400

    Time: 684 ms (5.01%), Space: 41.5 MB (9.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,21 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Count sort the string
         s_map = defaultdict(list)
         
         for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            # Convert the array to a tuple and use that as a key
-            s_map[tuple(count)].append(s)
+            s_map[self.map_s(s)].append(s)
         
         return s_map.values()
+    
+    def map_s(self, s):
+        c_map = [0] * 26
         
+        for c in s:
+            i = ord(c) - ord('a')
+            c_map[i] += 1
         
+        res = []
+        for i in range(len(c_map)):
+            res.append(str(c_map))
+        return ''.join(res)

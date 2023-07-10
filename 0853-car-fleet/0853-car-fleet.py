fb68b54b0f0bfdfc5e45f00915f2d09c174bf982
Akash Jairam <Quasar2197@gmail.com>
2023-07-10 18:07:33 -0400

    Time: 949 ms (53.20%), Space: 38.5 MB (53.06%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -9,6 +9,8 @@ class Solution:
             # Calculate time needed to get to get to target
             # Time = Distance / Speed
             stack.append((target - p) / s)
+            # If the car behind the current car can take less time to 
+            # Get to the target, then it will catch up
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
                 

6362010f0a15a21d38b6f26b9199a7f46de39d98
Akash Jairam <Quasar2197@gmail.com>
2023-07-13 13:26:34 -0400

    Time: 110 ms (92.19%), Space: 16.3 MB (80.60%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # Use sliding window
-        # Keep track of when we are within the threshold
-        # When outside threshold, increment left and decrement map accordingly
+        count = {}
         l, r = 0, 0
-        c_map = [0] * 26
-        max_char = 0
-        res = 0
+        max_f = 0
+        
         while r < len(s):
-            c = ord(s[r]) - ord('A')
-            c_map[c] += 1
-            max_char = max(max_char, c_map[c])
-            if r - l + 1 - max_char <= k:
-                res = max(res, r - l + 1)
+            count[s[r]] = 1 + count.get(s[r], 0)
+            max_f = max(max_f, count[s[r]])
             
-            while r - l + 1  - max_char> k:
-                c_map[ord(s[l]) - ord('A')] -= 1
+            if (r - l + 1) - max_f > k:
+                count[s[l]] -= 1
                 l += 1
-                max_char = max(c_map)
-            r += 1
-        return res
             
+            r += 1
+            
+        return (r - l )

f6995f79a988c77f6bd0799f22301f233ff79fe4
Akash Jairam <Quasar2197@gmail.com>
2023-07-15 18:12:34 -0400

    Time: 76 ms (84.72%), Space: 16.6 MB (37.10%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,17 +1,15 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        tup_arr = [(f'{num:b}', num) for num in arr]
-        tup_arr = [(self.count1s(p[0]), p[1]) for p in tup_arr]
+        tup_arr = [(self.count1s(num), num) for num in arr]
         tup_arr.sort()
         return [p[1] for p in tup_arr]
     
     
     def count1s(self, num):
         count = 0
-        for c in num:
-            if c == '1':
-                count += 1
-        
+        while num:
+            num = num & num - 1
+            count += 1
         return count
     
     

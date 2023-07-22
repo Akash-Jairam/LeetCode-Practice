70f2d2d7db2efcb9a9012c8046bb1a41b192e649
Akash Jairam <Quasar2197@gmail.com>
2023-07-22 13:07:20 -0400

    Time: 1653 ms (74.14%), Space: 33.2 MB (41.63%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,24 +1,21 @@
 from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # Use a queue where the idx of highest number is the first element 
-        # Pop elements whenever the curr number is greater than elements at the back of the queue
-        # Append the max element to res
-        queue = deque()
-        l, r, res = 0,0, []
-        
-        while r < len(nums):
-            while queue and nums[queue[-1]] < nums[r]:
-                queue.pop()
+        res = []
+        l = 0
+        q = deque()
+        for r in range(len(nums)):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            
+            q.append(r)
+            # Remove left value from deque if it is out of the window
+            if l > q[0]:
+                q.popleft()
                 
-            queue.append(r)
-            
-            if l > queue[0]:
-                queue.popleft()
-            
             if r + 1 >= k:
-                res.append(nums[queue[0]])
+                res.append(nums[q[0]])
                 l += 1
-            
-            r += 1
+        
         return res
+        

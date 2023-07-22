21cc2a649a54d955ac23a61615e4227bca0b1f6f
Akash Jairam <Quasar2197@gmail.com>
2023-07-22 12:37:03 -0400

    Time: 123 ms (93.87%), Space: 17.1 MB (64.75%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,34 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # Sliding window
-        # Update window whenever we have all required chars to see how far we can go
-        window, t_map = {}, {}
-        l, res, reslen = 0, [-1, -1], float('inf')
-        have, need = 0, len(t)
-        for c in t:
-            t_map[c] = 1 + t_map.get(c, 0)
+        if t == "":
+            return t
+        
+        t_map = {}
+        for i in range(len(t)):
+            t_map[t[i]] = 1 + t_map.get(t[i], 0)
             
+        s_map = {}
+        res, resLen = [-1, -1], float('inf')
+        have, need = 0, len(t)
+        l = 0
+        
         for r in range(len(s)):
             c = s[r]
-            window[c] = 1 + window.get(c, 0)
+            s_map[c] =  1 + s_map.get(c, 0)
             
-            if c in t_map and window[c] <= t_map[c]:
+            if c in t_map and s_map[c] <= t_map[c]:
                 have += 1
             
             while have == need:
-                if r - l + 1 < reslen:
-                    reslen = r - l + 1
+                if r - l + 1 <= resLen:
                     res = [l, r]
+                    resLen = r - l + 1
+                
                 c = s[l]
-                window[c] -= 1
-                if c in t_map and window[c] < t_map[c]:
+                s_map[c] -= 1
+                if c in t_map and s_map[c] < t_map[c]:
                     have -= 1
-                
-                
                 l += 1
-            
+        
         l, r = res
-
-        return s[l:r+1] if reslen != float('inf') else ""
-                
+        return s[l : r + 1] if resLen != float('inf') else ""

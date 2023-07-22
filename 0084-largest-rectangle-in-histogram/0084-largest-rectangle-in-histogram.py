748e193a262fb4e1898f595748ed392efd3a8c2f
Akash Jairam <Quasar2197@gmail.com>
2023-07-21 21:07:16 -0400

    Time: 921 ms (83.06%), Space: 33.2 MB (44.00%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,5 +1,12 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        # Iterate through heights
+        # Set the initial start point to i
+        # While there are heights on the stack that are greater than the current height
+        # Pop them off and calculate their height since this is as far that they will go
+        # Update the start value since the current value can extend backwards
+        # After the loop is finished, pop off the remainder of the stack and calculate their values
+        # Relative to the length of the array
         maxArea = 0
         stack = []
         n = len(heights)

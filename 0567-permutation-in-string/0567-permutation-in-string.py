cfa2abee25d97041b6c4864a463ae2e249101f27
Akash Jairam <Quasar2197@gmail.com>
2023-07-22 11:45:55 -0400

    Time: 84 ms (73.23%), Space: 16.4 MB (60.23%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        m = len(s1)
+        if m > len(s2):
             return False
         
-        m = len(s1)
-        s1_map, s2_map = [0] * 26, [0] * 26
-        for i in range(len(s1)):
+        s1_map = [0] * 26
+        for i in range(m):
             s1_map[ord(s1[i]) - ord('a')] += 1
         
-        count = 0
-        for i in range(len(s2)):
-            s2_map[ord(s2[i]) - ord('a')] += 1
+        s2_map = [0] * 26
+        l = 0
+        for r in range(len(s2)):
+            s2_map[ord(s2[r]) - ord('a')] += 1
             
-            count += 1
-            
-            while count > m:
-                s2_map[ord(s2[i-m]) - ord('a')] -= 1
-                count -= 1
-                
+            while r - l + 1 > m:
+                s2_map[ord(s2[l]) - ord('a')] -= 1
+                l += 1
             if s1_map == s2_map:
                 return True
+            
         
         return False

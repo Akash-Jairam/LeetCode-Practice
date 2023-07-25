44a0e22f589fca3567d99a7bebb5680424d55889
Akash Jairam <Quasar2197@gmail.com>
2023-07-24 20:35:07 -0400

    Time: 614 ms (65.41%), Space: 31.1 MB (68.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,5 +1,11 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        '''
+            Classic tortoise and hare algorithm
+            Have two pointers traverse the array but one moves twice as fast until they both meet again
+            Set a new pointer at the beginning and have the new pointer and an old pointer both move at regular speed until they converge
+            The point of convergence is the duplicate
+        '''
         slow = fast = 0
         
         while True:

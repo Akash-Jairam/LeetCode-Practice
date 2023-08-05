37964915d9d04a57313c73fdcbca97c4f06dd2a9
Akash Jairam <Quasar2197@gmail.com>
2023-08-05 14:34:15 -0400

    Time: 49 ms (55.19%), Space: 16.6 MB (12.33%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,30 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        '''
-            We have to use backtracking starting a index 0, using a list to store the numbers
-            If index is greater than len(nums), we return
-            Otherwise, we append a copy of the curr list to our result array
-            We then iterate from the current index to the end of nums
-                Here we append the value at the current index to our curr list
-                We make a recursive call starting at the position after the current index
-                Then we pop the last index of our current list
-             
-        '''
         res = []
-        self.backtrack(res, 0, nums, [])
+        self.backtrack(0, nums, [], res)
         return res
     
-    def backtrack(self, res, i, nums, curr):
-        
-        if i > len(nums):
+    def backtrack(self, i, nums, curr, res):
+        if i >= len(nums):
+            res.append(curr.copy())
             return
         
-        res.append(list(curr))
-        for j in range(i, len(nums)):
-            curr.append(nums[j])
-            self.backtrack(res, j + 1, nums, curr)
-            curr.pop()
-        
-        
-        
-    
+        curr.append(nums[i])
+        self.backtrack(i + 1, nums, curr, res)
+        curr.pop()
+        self.backtrack(i + 1, nums, curr, res)

0b9f0f245c8833d6738eee73c6b90a6327d9e008
Akash Jairam <Quasar2197@gmail.com>
2023-08-05 13:20:28 -0400

    Time: 7203 ms (27.24%), Space: 18.6 MB (71.20%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,55 +1,43 @@
 class Solution:
     class Node:
         def __init__(self):
-            self.children = [None] * 26
+            self.children = {}
             self.word = None
-            
+    
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        m, n = len(board), len(board[0])
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         root = self.Node()
         res = []
-        
-        def dfs(curr_node, i, j, board):
-            if i < 0 or i >= m  or j < 0 or j >= n or not board[i][j] or len(res) == len(words):
-                return
-            
-            c = board[i][j]
-            curr_idx = ord(c) - ord('a')
-            if not curr_node.children[curr_idx]:
-                return
-            
-            curr_node = curr_node.children[curr_idx]
-            if curr_node.word:
-                res.append(curr_node.word)
-                curr_node.word = None
-                
-            
-            
-            board[i][j] = None
-            for d in directions:
-                dfs(curr_node, i + d[0], j + d[1], board)
-            
-            board[i][j] = c
-            
-           
-            
-        
-        
         for word in words:
             it = root
             for c in word:
-                idx = ord(c) - ord('a')
-                if not it.children[idx]:
-                    it.children[idx] = self.Node()
-                it = it.children[idx]
+                it.children[c] = it.children.get(c, self.Node())
+                it = it.children[c]
             it.word = word
         
-        for row in range(m):
-            for col in range(n):
-                val = ord(board[row][col]) - ord('a')
-                if root.children[val]:
-                    dfs(root, row, col, board)
+        for i in range(0, len(board)):
+            for j in range(0, len(board[0])):
+                c = board[i][j]
+                if root.children.get(c, None):
+                    self.dfs(i, j, root, res, board)
         
         return res
+    
+    def dfs(self, r, c, node, res, board):
+        if not 0 <= r < len(board) or not 0 <= c < len(board[0]) or not board[r][c]:
+            return
         
+        curr_char = board[r][c]
+        child = node.children.get(curr_char, None)
+        if not child:
+            return
+        
+        if child.word:
+            res.append(child.word)
+            child.word = None
+        
+        board[r][c] = None
+        self.dfs(r + 1 , c, child, res, board)
+        self.dfs(r , c + 1, child, res, board)
+        self.dfs(r - 1 , c, child, res, board)
+        self.dfs(r , c - 1, child, res, board)
+        board[r][c] = curr_char

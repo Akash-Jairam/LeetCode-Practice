7c84f4c71cd5a5d3594353dcae98d02c873a85b7
Akash Jairam <Quasar2197@gmail.com>
2023-08-06 11:05:00 -0400

    Time: 29 ms (99.67%), Space: 16.4 MB (96.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,22 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Every time we are backtracking, we are presented with two choices: take the current number or don't
-        # When we backtrack, append the current number and backtrack
-        # Then pop the last number and backtrack again
-        # Append a copy of the curr list to res when we can't backtrack anymore
-        # Return the res
         res = []
         self.backtrack(0, nums, [], res)
         return res
         
     def backtrack(self, i, nums, curr, res):
-        if i >= len(nums):
-            res.append(curr.copy())
+        if i == len(nums):
+            res.append(curr[::])
             return
         
         curr.append(nums[i])
         self.backtrack(i + 1, nums, curr, res)
-        
         curr.pop()
+        
         self.backtrack(i + 1, nums, curr, res)
         

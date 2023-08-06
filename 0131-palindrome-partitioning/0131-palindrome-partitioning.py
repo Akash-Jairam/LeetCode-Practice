d186b732569f018dba267d392f0a2087276eef57
Akash Jairam <Quasar2197@gmail.com>
2023-08-06 13:41:37 -0400

    Time: 524 ms (99.43%), Space: 33.8 MB (51.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,7 +1,6 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        part = []
+        res, part = [], []
         
         def dfs(i):
             if i >= len(s):
@@ -10,18 +9,19 @@ class Solution:
             
             for j in range(i, len(s)):
                 if self.isPal(s, i, j):
-                    part.append(s[i: j + 1])
-                    dfs(j + 1)
+                    part.append(s[i:j+1])
+                    dfs(j+1)
                     part.pop()
-            
-        
+                    
         dfs(0)
         return res
+        
     
     def isPal(self, s, l, r):
         while l < r:
             if s[l] != s[r]:
                 return False
+            
             l += 1
             r -= 1
         

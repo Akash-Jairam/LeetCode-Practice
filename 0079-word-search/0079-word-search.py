83309953e772c3abf06ae35554578b2afefd5479
Akash Jairam <Quasar2197@gmail.com>
2023-08-06 13:06:11 -0400

    Time: 4996 ms (80.50%), Space: 16.2 MB (95.52%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        def dfs(idx, row, col):
-            if idx >= len(word):
-                return True
-            
-            if row < 0 or row >= m or col < 0 or col >=n:
-                return False
-            
-            if board[row][col] != word[idx]:
-                return False
-            
-            c = board[row][col]
-            board[row][col] = ' '
-            
-            res = (dfs(idx + 1, row - 1, col) or
-                    dfs(idx + 1, row + 1, col) or
-                    dfs(idx + 1, row, col - 1) or   
-                    dfs(idx + 1, row, col + 1) )
-            board[row][col] = c
-            return res
-                    
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    if dfs(0, i, j):
-                        return True
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0] and self.backtrack(i, j, board, 0, word):
+                    return True
         
         return False
+    
+    def backtrack(self, r, c, board, idx, word):
+        if not 0 <= r < len(board) or not 0 <= c < len(board[0]) or board[r][c] != word[idx]:
+            return False
+        
+        idx += 1
+        if idx == len(word):
+            return True
+        
+        curr = board[r][c]
+        board[r][c] = None
+        
+        res = self.backtrack(r + 1, c, board, idx, word) or self.backtrack(r - 1, c, board, idx, word) or self.backtrack(r , c + 1, board, idx, word) or self.backtrack(r , c - 1, board, idx, word)
+        board[r][c] = curr
+        return res

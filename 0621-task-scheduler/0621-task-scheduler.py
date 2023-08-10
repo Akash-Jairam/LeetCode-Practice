e57466f3321ce2efb74ce78848e9c6078796c0d2
Akash Jairam <Quasar2197@gmail.com>
2023-08-10 18:39:48 -0500

    Time: 482 ms (73.42%), Space: 16.8 MB (95.11%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,33 +1,29 @@
-from collections import Counter, deque
+from collections import deque
 import heapq
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # Group the tasks by their count
-        # Create a max heap to sort the tasks by highest count
-        # Create a queue to hold the tasks that are waiting for their cooldown to finish
-        # Use a while loop that continues while either the heap or queue has elements
-        # Pop the current element from the heap, reduce the abs value by 1, if it is not 0, append to queue for cooldown
-        # Check the queue and pop all elements whose cooldown are <= curr time and add them to heap for processing
-        # Increment time
-        # Return time once finished
-        tasks_by_count = Counter(tasks)
-        max_heap = [-count for count in tasks_by_count.values()]
+        t_map = {}
+        for t in tasks:
+            t_map[t] = 1 + t_map.get(t, 0)
+            
+        max_heap = [-count for count in t_map.values()]
         heapq.heapify(max_heap)
-        queue = deque()
+        
+        q = deque()
         time = 0
         
-        while max_heap or queue:
-            if max_heap:
-                curr = heapq.heappop(max_heap)
-                curr += 1
-                if curr:
-                    queue.append([curr, time + n])
-            
-            while queue and queue[0][1] <= time:
-                heapq.heappush(max_heap, queue.popleft()[0])
-            
+        while q or max_heap:
             time += 1
+            
+            if not max_heap:
+                time = q[0][1]
+                
+            if max_heap:
+                count = 1 + heapq.heappop(max_heap)
+                if count:
+                    q.append([count, time + n])
+            
+            if q and q[0][1] == time:
+                heapq.heappush(max_heap, q.popleft()[0])
         
         return time
-        

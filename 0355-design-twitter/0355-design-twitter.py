b532ce58afe612618358b76bde5b191220a2bcc3
Akash Jairam <Quasar2197@gmail.com>
2023-08-12 11:02:58 -0500

    Time: 41 ms (74.21%), Space: 16.5 MB (69.68%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,34 +1,34 @@
 from collections import defaultdict
 import heapq
+
 class Twitter:
 
     def __init__(self):
         self.following = defaultdict(set)
         self.tweets = defaultdict(list)
         self.time = 0
-        
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.time -= 1
-        self.tweets[userId].append([self.time, tweetId])
+        self.tweets[userId].append((self.time, tweetId))
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        max_heap = []
-        res = []
-        
         self.following[userId].add(userId)
-        for followee in self.following[userId]:
-            if followee in self.tweets:
-                index = len(self.tweets[followee]) - 1
-                count, tweetId = self.tweets[followee][index]
-                heapq.heappush(max_heap, [count, tweetId, followee, index - 1])
+        res = []
+        max_heap = []
+        for user in self.following[userId]:
+            if len(self.tweets[user]) > 0:
+                idx = len(self.tweets[user]) - 1
+                time, tId = self.tweets[user][idx]
+                heapq.heappush(max_heap, [time, tId, user, idx - 1])
         
         while max_heap and len(res) < 10:
-            count, tweetId, followeeId, index = heapq.heappop(max_heap)
-            res.append(tweetId)
-            if index >= 0:
-                count, tweetId = self.tweets[followeeId][index]
-                heapq.heappush(max_heap, [count, tweetId, followeeId, index - 1])
+            time, tId, user, idx = heapq.heappop(max_heap)
+            res.append(tId)
+            
+            if idx >= 0:
+                time, tId = self.tweets[user][idx]
+                heapq.heappush(max_heap, [time, tId, user, idx - 1])
         
         return res
 

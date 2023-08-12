aea38799a491d5fc4b04b68789404e5bb1b4ccd6
Akash Jairam <Quasar2197@gmail.com>
2023-08-12 11:51:32 -0500

    Time: 444 ms (84.94%), Space: 37.8 MB (98.39%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -2,33 +2,34 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.max_heap, self.min_heap = [], []
+        self.min_heap = []
+        self.max_heap = []
 
     def addNum(self, num: int) -> None:
-        if self.min_heap and self.min_heap[0] < num:
-            heapq.heappush(self.min_heap, num)
-        else:
-            heapq.heappush(self.max_heap, -num)
-
-        if len(self.min_heap) > len(self.max_heap):
-            val = heapq.heappop(self.min_heap)
-            heapq.heappush(self.max_heap, -val)
+        heapq.heappush(self.max_heap, -num)
         
-        if len(self.max_heap) > len(self.min_heap):
+        if self.max_heap and self.min_heap and -self.max_heap[0] > self.min_heap[0]:
             val = -heapq.heappop(self.max_heap)
             heapq.heappush(self.min_heap, val)
         
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            val = -heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap, val)
+        
+        if len(self.min_heap) > len(self.max_heap) + 1:
+            val = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -val)
+        
+        
+
     def findMedian(self) -> float:
-        m, n = len(self.max_heap), len(self.min_heap)
-        
-        if m > n:
+        minh, maxh = self.max_heap, self.min_heap
+        if len(self.max_heap) > len(self.min_heap):
             return -self.max_heap[0]
-        elif n > m:
+        elif len(self.min_heap) > len(self.max_heap):
             return self.min_heap[0]
-        
-        return  (-self.max_heap[0] + self.min_heap[0]) / 2
-
-
+        else:
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)

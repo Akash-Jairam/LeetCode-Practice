ddb4289e6376febb789d4435fb344f23a22982c9
Akash Jairam <Quasar2197@gmail.com>
2023-08-13 11:01:23 -0500

    Time: 94 ms (95.52%), Space: 17.8 MB (83.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,37 @@
-from collections import defaultdict, deque
+from collections import deque, defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         inDegree = [0] * numCourses
         c_map = defaultdict(list)
-        numDependencies, edgesRemoved = 0, 0
         
-        for a, b in prerequisites:
-            inDegree[a] += 1
-            c_map[b].append(a)
-            numDependencies += 1
         
-        queue = deque()
+        for post, pre in prerequisites:
+            inDegree[post] += 1
+            c_map[pre].append(post)
+        
+        completed = []
+        zeroDegree = deque()
+        
         for i in range(numCourses):
             if inDegree[i] == 0:
-                queue.append(i)
+                zeroDegree.append(i)
         
-        res = []
-        if not queue:
-            return res
+        if len(zeroDegree) == 0:
+            return completed
         
-        while queue:
-            size = len(queue)
-            for _ in range(len(queue)):
-                curr = queue.popleft()
-                res.append(curr)
-                for c in c_map[curr]:
-                    inDegree[c] -= 1
-                    edgesRemoved += 1
-                    if inDegree[c] == 0:
-                        queue.append(c)
+        while zeroDegree:
+            size = len(zeroDegree)
+            for _ in range(size):
+                curr = zeroDegree.popleft()
+                completed.append(curr)
+                for post in c_map[curr]:
+                    inDegree[post] -= 1
+                    if inDegree[post] == 0:
+                        zeroDegree.append(post)
         
-        return res if numDependencies == edgesRemoved else []
+        for i in range(numCourses):
+            if inDegree[i] != 0:
+                return []
+            
+        return completed
+        

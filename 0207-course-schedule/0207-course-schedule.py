bb0b8447b38ebd5eef927aad77eff8216f5c7aa7
Akash Jairam <Quasar2197@gmail.com>
2023-08-13 10:48:54 -0500

    Time: 96 ms (94.19%), Space: 17.7 MB (95.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,32 @@
-from collections import deque
+from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        c_map = defaultdict(list)
         inDegree = [0] * numCourses
         
         for post, pre in prerequisites:
             inDegree[post] += 1
+            c_map[pre].append(post)
         
         zeroDegree = deque()
-        for i in range(numCourses):
+        for i in range(len(inDegree)):
             if inDegree[i] == 0:
                 zeroDegree.append(i)
         
         if len(zeroDegree) == 0:
             return False
         
-        completed = set()
-        numCompleted = 0
         while zeroDegree:
             size = len(zeroDegree)
             for _ in range(size):
                 curr = zeroDegree.popleft()
-                for post, pre in prerequisites:
-                    if pre == curr:
-                        inDegree[post] -= 1
-                        if inDegree[post] == 0:
-                            zeroDegree.append(post)
+                for post in c_map[curr]:
+                    inDegree[post] -= 1
+                    if inDegree[post] == 0:
+                        zeroDegree.append(post)
         
-        for i in range(len(inDegree)):
+        for i in range(numCourses):
             if inDegree[i] != 0:
                 return False
-            
+        
         return True

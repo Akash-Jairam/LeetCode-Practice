6320cb34e45b9390827cc0d0f11bccff3dfb167d
Akash Jairam <Quasar2197@gmail.com>
2023-08-23 21:17:55 -0500

    Time: 1065 ms (81.14%), Space: 22.2 MB (56.22%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,7 +1,6 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        totalGas = 0
-        remGas = 0
+        totalGas, remGas = 0, 0
         startIdx = 0
         
         for i in range(len(gas)):
@@ -10,7 +9,6 @@ class Solution:
             
             if remGas < 0:
                 remGas = 0
-                startIdx =  i + 1
-            
+                startIdx = i + 1
         
         return startIdx if totalGas >= 0 else -1

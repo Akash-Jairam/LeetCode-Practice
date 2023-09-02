b5071f57b1f746d2b79d0f7f0d4d5cdb8781a308
Akash Jairam <Quasar2197@gmail.com>
2023-09-02 18:00:41 -0500

    Time: 553 ms (18.86%), Space: 35.2 MB (44.64%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,24 +1,16 @@
 import heapq
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        # Use a map to track the numbers and their frequencies
-        # Push the number and freq to a min heap
-        # Decrease frequencies by popping from the heap until k is reduced to 0
-        # If a frequency is not reduced to 0, put it back in the heap
         n_map = {}
-        min_heap  = []
-        for n in arr:
-            n_map[n] = 1 + n_map.get(n, 0)
+        for num in arr:
+            n_map[num] = 1 + n_map.get(num, 0)
         
-        for key, v in n_map.items():
-            heapq.heappush(min_heap, (v, key))
-        
+        min_heap = [[v, k]for k,v in n_map.items()]
+        heapq.heapify(min_heap)
         while k > 0:
-            v, key = heapq.heappop(min_heap)
-            if k >= v:
-                k -= v
-            else:
-                v -= k
-                k = 0
-                heapq.heappush(min_heap, (v, key))
+            curr = heapq.heappop(min_heap)
+            curr[0] -= 1
+            if curr[0] > 0:
+                heapq.heappush(min_heap, curr)
+            k -= 1
         return len(min_heap)

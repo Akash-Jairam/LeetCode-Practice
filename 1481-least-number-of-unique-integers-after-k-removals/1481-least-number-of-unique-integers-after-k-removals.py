7eae60b6b29faa420ff50cd2ed7afe18198ef09f
Akash Jairam <Quasar2197@gmail.com>
2023-09-02 18:06:49 -0500

    Time: 360 ms (91.00%), Space: 33.3 MB (75.93%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -5,12 +5,13 @@ class Solution:
         for num in arr:
             n_map[num] = 1 + n_map.get(num, 0)
         
-        min_heap = [[v, k]for k,v in n_map.items()]
-        heapq.heapify(min_heap)
-        while k > 0:
-            curr = heapq.heappop(min_heap)
-            curr[0] -= 1
-            if curr[0] > 0:
-                heapq.heappush(min_heap, curr)
-            k -= 1
-        return len(min_heap)
+        vals = sorted(n_map.values())
+        res = len(vals)
+        for v in vals:
+            if k >= v:
+                k -= v
+                res -= 1
+            else:
+                break
+        
+        return res

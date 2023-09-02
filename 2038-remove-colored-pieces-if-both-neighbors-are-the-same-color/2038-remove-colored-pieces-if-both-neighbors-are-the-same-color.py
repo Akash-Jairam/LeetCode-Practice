7445fff79db979e9b8fad2c534beee4762c001fe
Akash Jairam <Quasar2197@gmail.com>
2023-09-02 18:47:13 -0500

    Time: 199 ms (71.57%), Space: 17.3 MB (76.97%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,5 +1,10 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
+        # Keep track of number of valid moves
+        # If both players play optimally, Alice will always win if she has more valid moves than Bob
+        # Playing optimally means that you will always make the choice that will increase your chances of winning
+        # Since Alice always goes first, she just needs to have more choices than Bob
+        # If she has less than or equal choices, will run out of choices first and lose the game
         a_count, b_count = 0, 0
         
         for i in range(1, len(colors) - 1):

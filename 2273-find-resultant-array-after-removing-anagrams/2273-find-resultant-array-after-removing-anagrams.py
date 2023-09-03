582071a535139fe2d1212e1fa3c3aea18e5a913f
Akash Jairam <Quasar2197@gmail.com>
2023-09-03 17:12:23 -0500

    Time: 53 ms (92.68%), Space: 16.5 MB (17.71%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        curr = self.mapToTup(words[0])
+        prev = 0
+        kept = [1] * len(words)
+        for i in range(1, len(words)):
+            nxt = self.mapToTup(words[i])
+            if curr == nxt:
+                kept[i] -= 1
+            else:
+                curr = nxt
+        
+        res = []
+        for i in range(len(kept)):
+            if kept[i]:
+                res.append(words[i])
+        return res
+        
+    
+    def mapToTup(self, s):
+        arr = [0] * 26
+        
+        for c in s:
+            i = ord(c) - ord('a')
+            arr[i] += 1
+        
+        return tuple(arr)
+    

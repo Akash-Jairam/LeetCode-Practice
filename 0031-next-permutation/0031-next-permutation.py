12149044e38111dd2f7dd505be7e405fc924c1eb
Akash Jairam <Quasar2197@gmail.com>
2023-09-03 18:07:26 -0500

    Time: 38 ms (98.10%), Space: 16.2 MB (94.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,26 +1,30 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        1. Find last peak
+        2. Pivot = last peak - 1
+        3. Find swap point aka index of the last number that is greater than the number at the pivot
+        4. Swap the numbers at the pivot and the swap point
+        5. Reverse all the numbers after the pivot
         """
         lastPeak = self.findLastPeak(nums)
         pivot = lastPeak - 1
         if pivot != -1:
-            swapPoint = self.findSwapPoint(nums, pivot)
-            nums[pivot], nums[swapPoint] = nums[swapPoint], nums[pivot]
+            swapPt = self.findSwapPt(nums, pivot)
+            nums[swapPt], nums[pivot] = nums[pivot], nums[swapPt]
         self.reverse(nums, pivot + 1)
         return nums
     
     def findLastPeak(self, nums):
         i = len(nums) - 1
-        
         while i > 0:
             if nums[i] > nums[i-1]:
                 return i
             i -= 1
         return 0
     
-    def findSwapPoint(self, nums, pivot):
+    
+    def findSwapPt(self, nums, pivot):
         i = len(nums) - 1
         while i >= 0:
             if nums[i] > nums[pivot]:
@@ -35,5 +39,3 @@ class Solution:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
-        
-        

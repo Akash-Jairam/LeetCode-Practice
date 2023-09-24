6609e62fbdc2c1a6f8083691d6f18988be02648f
Akash Jairam <Quasar2197@gmail.com>
2023-09-24 18:22:44 -0500

    Time: 174 ms (74.90%), Space: 17.9 MB (64.31%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.py
+++ 1797-design-authentication-manager/1797-design-authentication-manager.py
@@ -1,27 +1,32 @@
-import heapq
 class AuthenticationManager:
 
     def __init__(self, timeToLive: int):
+        self.ttl = timeToLive
         self.t_map = {}
-        self.live = timeToLive
+        
 
+    def clean(self, currentTime):
+        to_remove = []
+        for tId, time in self.t_map.items():
+            if time <= currentTime:
+                to_remove.append(tId)
+        
+        for tId in to_remove:
+            del self.t_map[tId]
+        
+            
     def generate(self, tokenId: str, currentTime: int) -> None:
-        self.t_map[tokenId] = currentTime + self.live
-        #self.min_heap
+        self.t_map[tokenId] = currentTime + self.ttl
 
     def renew(self, tokenId: str, currentTime: int) -> None:
-        time = self.t_map.get(tokenId, 0)
-        if time > currentTime:
-            self.t_map[tokenId] = currentTime + self.live
+        self.clean(currentTime)
+        if tokenId in self.t_map:
+            self.t_map[tokenId] = currentTime + self.ttl
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        res = 0
-        for k, v in self.t_map.items():
-            if v > currentTime:
-                res += 1
-        
-        return res
-  
+        self.clean(currentTime)
+        return len(self.t_map)
+
 
 # Your AuthenticationManager object will be instantiated and called as such:
 # obj = AuthenticationManager(timeToLive)

d8be460f1df14ef11cc33cdd1ce7519c891c8cf2
Akash Jairam <Quasar2197@gmail.com>
2023-12-31 11:40:49 -0600

    Time: 70 ms (10.73%), Space: 17.4 MB (5.61%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        c_map = {}
+        c_map = [-1] * 26
         longest = -1
         
         for i in range(len(s)):
-            if s[i] in c_map:
-                start = c_map[s[i]]
+            letterIdx = ord(s[i]) - ord('a')
+            if c_map[letterIdx] >= 0:
+                start = c_map[letterIdx]
                 longest = max(longest, i - start - 1)
             else:
-                c_map[s[i]] = i
+                c_map[letterIdx] = i
         
         return longest
                 

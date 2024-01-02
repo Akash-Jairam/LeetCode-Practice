ed74a216635ee09d1671c8dd9769dab138cda4b7
Akash Jairam <Quasar2197@gmail.com>
2024-01-01 19:28:51 -0600

    Time: 41 ms (99.68%), Space: 17.5 MB (5.17%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -2,15 +2,14 @@ from collections import defaultdict
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         n_map = [0] * 201
-        
+        res = {}
         for n in nums:
+            idx = n_map[n]
+            if idx not in res:
+                res[idx] = []
+            res[idx].append(n)
             n_map[n] += 1
         
-        idx = 0
-        toConvert = defaultdict(list)
         
-        for i in range(len(n_map)):
-            for j in range(n_map[i]):
-                toConvert[j].append(i)
         
-        return toConvert.values()
+        return res.values()

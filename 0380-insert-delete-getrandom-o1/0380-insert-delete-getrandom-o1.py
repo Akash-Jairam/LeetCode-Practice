6105e24481df430ba713510be7db9bba57092d8d
Akash Jairam <Quasar2197@gmail.com>
2024-01-15 19:34:10 -0600

    Time: 252 ms (95.10%), Space: 65.1 MB (6.72%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,33 +1,35 @@
 import random
+
 class RandomizedSet:
 
     def __init__(self):
-        self.num_map, self.num_list = {}, []
+        self.store = []  # To store the elements
+        self.store_s = {}  # To store the index of each element in the store list
 
     def insert(self, val: int) -> bool:
-        if val not in self.num_map:
-            self.num_list.append(val)
-            self.num_map[val] = len(self.num_list) - 1
+        if val not in self.store_s:
+            self.store.append(val)
+            self.store_s[val] = len(self.store) - 1
             return True
-        
         return False
 
     def remove(self, val: int) -> bool:
-        if val in self.num_map:
-            lastNum, valIdx = self.num_list[-1], self.num_map[val]
-            self.num_map[lastNum], self.num_list[valIdx] = valIdx, lastNum
-            self.num_list.pop()
-            del self.num_map[val]
+        if val in self.store_s:
+            idx = self.store_s[val]
+            last_element = self.store[-1]
+
+            # Update the index of the last element in the store_s dictionary
+            self.store_s[last_element] = idx
+
+            # Swap the element to be removed with the last element and pop it
+            self.store[idx], self.store[-1] = self.store[-1], self.store[idx]
+            self.store.pop()
+
+            # Remove the entry for the removed element from store_s
+            del self.store_s[val]
+
             return True
-        
         return False
 
     def getRandom(self) -> int:
-        return random.choice(self.num_list)
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        return random.choice(self.store)

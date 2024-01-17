b67682b82cfcf01d938fb5eb00c278dd7c18555a
Akash Jairam <Quasar2197@gmail.com>
2024-01-17 08:42:59 -0600

    Time: 49 ms (48.20%), Space: 17.3 MB (30.01%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        totalCosts = 0
+        totalCost = 0
         refunds = []
         
         for c1, c2 in costs:
-            totalCosts += c1
+            totalCost += c1
             refunds.append(c2 - c1)
-            
+        
         refunds.sort()
-        
         for i in range(len(refunds) // 2):
-            totalCosts += refunds[i]
+            totalCost += refunds[i]
         
-        return totalCosts
+        return totalCost

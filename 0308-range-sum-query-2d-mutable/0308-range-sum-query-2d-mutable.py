52b653d71f23b29360b163b13bb17ce1e89f33aa
Akash Jairam <Quasar2197@gmail.com>
2024-01-21 10:53:01 -0600

    Time: 6548 ms (5.01%), Space: 23.4 MB (6.79%) - LeetHub

--- 0308-range-sum-query-2d-mutable/0308-range-sum-query-2d-mutable.py
+++ 0308-range-sum-query-2d-mutable/0308-range-sum-query-2d-mutable.py
@@ -3,28 +3,28 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
-        self.dirs = [[0, 1], [1, 0]]
-        
+
     def update(self, row: int, col: int, val: int) -> None:
         self.matrix[row][col] = val
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        visited = set()
+        directions = [[1, 0], [0, 1]]
         q = deque()
         q.append((row1, col1))
-        total = 0
-        visited = set()
+        res = 0
         while q:
             size = len(q)
             for _ in range(size):
                 x, y = q.popleft()
-                total += self.matrix[x][y]
-                for i, j in self.dirs:
-                    if x + i <= row2 and y + j <= col2 and (x + i, y + j) not in visited:
-                        visited.add((x + i, y + j))
-                        q.append((x + i, y + j))
-        
-        return total
-
+                res += self.matrix[x][y]
+                for d in directions:
+                    row = x + d[0]
+                    col = y + d[1]
+                    if row <= row2 and col <= col2 and not (row, col) in visited:
+                        visited.add((row, col))
+                        q.append((row, col))
+        return res
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # obj.update(row,col,val)

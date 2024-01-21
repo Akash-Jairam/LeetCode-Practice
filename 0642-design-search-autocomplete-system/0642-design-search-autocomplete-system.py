a0355f0f3b7a6f2930d19017488eddede02e52c6
Akash Jairam <Quasar2197@gmail.com>
2024-01-21 10:02:08 -0600

    Time: 373 ms (86.01%), Space: 23.7 MB (53.10%) - LeetHub

---
+++ 0642-design-search-autocomplete-system/0642-design-search-autocomplete-system.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.sentences = defaultdict(int)
+        
+class AutocompleteSystem:
+
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.root = TrieNode()
+        for sentence, count in zip(sentences, times):
+            self.add_to_trie(sentence, count)
+        
+        self.curr_sentence = []
+        self.curr_node = self.root
+        self.dead  = TrieNode()
+        
+    def input(self, c: str) -> List[str]:
+        if c == '#':
+            current_sentence = "".join(self.curr_sentence)
+            self.add_to_trie(current_sentence, 1)
+            self.curr_sentence = []
+            self.curr_node = self.root
+            return []
+        
+        self.curr_sentence.append(c)
+        if c not in self.curr_node.children:
+            self.curr_node = self.dead
+            return []
+        
+        self.curr_node = self.curr_node.children[c]
+        sentences = self.curr_node.sentences
+        sorted_sentences = sorted(sentences.items(), key = lambda x: (-x[1], x[0]))
+        ans = []
+        for i in range(min(3, len(sorted_sentences))):
+            ans.append(sorted_sentences[i][0])
+        
+        return ans
+        
+    
+    def add_to_trie(self, sentence, count):
+        it = self.root
+        for c in sentence:
+            if c not in it.children:
+                it.children[c] = TrieNode()
+            it = it.children[c]
+            it.sentences[sentence] += count
+
+# Your AutocompleteSystem object will be instantiated and called as such:
+# obj = AutocompleteSystem(sentences, times)
+# param_1 = obj.input(c)

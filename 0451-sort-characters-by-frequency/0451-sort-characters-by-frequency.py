f3fb4c60d4c68d8561399d4b085edceec4d09cb8
Akash Jairam <Quasar2197@gmail.com>
2024-02-06 18:59:47 -0600

    Time: 44 ms (73.73%), Space: 17.9 MB (71.88%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,11 +1,19 @@
+import heapq
 class Solution:
     def frequencySort(self, s: str) -> str:
-        char_map = collections.Counter(s)
+        c_map = {}
         
-        sb = []
-        for letter, freq in char_map.most_common():
-            sb.append(letter * freq)
+        for c in s:
+            c_map[c] = 1 + c_map.get(c, 0)
         
+        max_heap = []
+        for k, v in c_map.items():
+            heapq.heappush(max_heap, (-v, k))
         
-        return "".join(sb)
+        res = []
+        while max_heap:
+            v,c = heapq.heappop(max_heap)
+            res.append( c * -v)
+        
+        return ''.join(res)
         

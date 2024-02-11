dc583be13f72ce9e034990058f2d25ec0991087a
Akash Jairam <Quasar2197@gmail.com>
2024-02-11 11:18:35 -0600

    Time: 95 ms (67.94%), Space: 17.2 MB (95.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -7,8 +7,6 @@ class Solution:
         # Iterate through s making sure to update the corresponding map and have count
         # While have == need, we check to see if we need to update res and resLength,then we slide to the right making sure to decrement have if needed
         # Finally, we return the substring if the resLength is not infinity, otherwise, we return ""
-        if t == "":
-            return ""
         
         t_map = {}
         for c in t:
@@ -27,17 +25,16 @@ class Solution:
                 have += 1
             
             while have == need:
-                if r - l + 1 <= resLen:
-                    res = [l, r]
+                if r - l + 1 < resLen:
                     resLen = r - l + 1
+                    res = [l, r]
                 
                 c = s[l]
                 s_map[c] -= 1
-
                 if c in t_map and s_map[c] < t_map[c]:
                     have -= 1
-                l += 1
                 
-             
+                l += 1
+        
         l, r = res
-        return s[l : r + 1] if resLen != float('inf') else ""
+        return s[l:r+1] if resLen != float('inf') else ""

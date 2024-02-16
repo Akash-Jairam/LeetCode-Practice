902c5ac3c35de3ef9f0558a4e458188642039ecf
Akash Jairam <Quasar2197@gmail.com>
2024-02-15 18:46:34 -0600

    Time: 301 ms (99.53%), Space: 33.3 MB (94.30%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,19 +1,27 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        n_map = {}
-        for n in arr:
-            n_map[n] = 1 + n_map.get(n, 0)
-        
-        n = len(n_map.keys())
-        for v in sorted(n_map.values()):
-            k -= v
-            if k < 0:
-                break
-            elif k > 0:
-                n -=1
-            else:
-                n -= 1
-                break
-        
-        return n
-            
+        # Dictionary to track the frequencies of elements
+        freq_map = Counter(arr)
+
+        # List to track all the frequencies
+        frequencies = list(freq_map.values())
+
+        # Sorting the frequencies
+        frequencies.sort()
+
+        # Tracking the number of elements removed
+        elements_removed = 0
+
+        for i in range(len(frequencies)):
+            # Removing frequencies[i] elements, which equates to
+            # removing one unique element
+            elements_removed += frequencies[i]
+
+            # If the number of elements removed exceeds k, return
+            # the remaining number of unique elements
+            if elements_removed > k:
+                return len(frequencies) - i
+
+        # We have removed all elements, so no unique integers remain
+        # Return 0 in this case
+        return 0

4daac532f1d5b2654d75b657326f39873f1bf4ce
Akash Jairam <Quasar2197@gmail.com>
2024-02-25 09:09:54 -0600

    Time: 34 ms (80.09%), Space: 16.7 MB (58.39%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,15 +3,14 @@ class Solution:
         res = []
         self.backtrack(0, nums, [], res)
         return res
-        
-    def backtrack(self, i, nums, curr, res):
-        if i == len(nums):
+    
+    def backtrack(self, i, arr, curr, res):
+        if i == len(arr):
             res.append(curr[::])
             return
         
-        curr.append(nums[i])
-        self.backtrack(i + 1, nums, curr, res)
+        curr.append(arr[i])
+        self.backtrack(i + 1, arr, curr, res)
         curr.pop()
         
-        self.backtrack(i + 1, nums, curr, res)
-        
+        self.backtrack(i + 1, arr, curr, res)

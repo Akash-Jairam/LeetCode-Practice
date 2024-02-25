8dd3d762ce63992cc009aab68b63fb9f0a5f06d2
Akash Jairam <Quasar2197@gmail.com>
2024-02-25 09:19:03 -0600

    Time: 50 ms (74.01%), Space: 16.6 MB (91.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,26 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        '''
-            Backtrack 
-            We have two choices, either take the current number and backtrack from the same position
-            Or don't take the current number and backtrack from the next position
-            We return if we hit the target, if we are out of bounds or if the curr sum is greater than the target
-        '''
         res = []
-        self.backtrack(0, candidates, [], 0, target, res)
+        self.backtrack(candidates, 0, [], target, res)
         return res
-        
     
-    def backtrack(self, i, candidates, curr, curr_sum, target, res):
-        if curr_sum == target:
-            res.append(curr.copy())
+    def backtrack(self, cands, index, curr, target, res):
+        if target == 0:
+            res.append(curr[::])
             return
         
-        if curr_sum > target or i >= len(candidates):
+        if target < 0 or index == len(cands):
             return
         
-        curr.append(candidates[i])
-        self.backtrack(i, candidates, curr, curr_sum + candidates[i], target, res)
+        curr.append(cands[index])
+        self.backtrack(cands, index, curr, target - cands[index], res)
         curr.pop()
         
-        self.backtrack(i + 1, candidates, curr, curr_sum, target, res)
+        self.backtrack(cands, index + 1, curr, target, res)

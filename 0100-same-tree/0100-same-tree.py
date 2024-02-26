e040f86caab640453575d53f0e059fcd32aabbd8
Akash Jairam <Quasar2197@gmail.com>
2024-02-25 19:14:57 -0600

    Time: 38 ms (43.56%), Space: 16.6 MB (45.81%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,23 +6,10 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        '''
-            Use dfs to check the whether the trees are the same
-            If both trees are None, return True
-            Else if either tree is None, return False
-            Now we use dfs to see if the left sides are equal
-            Then do dfs to see if the right sides are equal
-            Finally, return whether both sides of both sides are equal and whether the value of the current nodes are the same
-            TC: O(N) or O(V + E)
-            SC: O(N) or O(V + E)
-        '''
         if not p and not q:
             return True
         
         if not p or not q:
             return False
         
-        isLeftSame = self.isSameTree(p.left, q.left)
-        isRightSame = self.isSameTree(p.right, q.right)
-        
-        return p.val == q.val and isLeftSame and isRightSame
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) and p.val == q.val

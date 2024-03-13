7a8a3bf3b6d850bc5a966155603267251d514206
Akash Jairam <Quasar2197@gmail.com>
2024-03-13 18:59:57 -0500

    Time: 153 ms (60.30%), Space: 32.3 MB (83.10%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,42 +1,73 @@
+class ListNode:
+    def __init__(self, val):
+        self.keys = set()  # Holds the keys with the current count
+        self.prev = None   # Previous ListNode in the doubly linked list
+        self.next = None   # Next ListNode in the doubly linked list
+        self.val = val     # The count value of this node
+
+    def insertAfter(self, newNode):
+        # Inserts newNode after this node
+        newNode.prev = self
+        newNode.next = self.next
+        if self.next:
+            self.next.prev = newNode
+        self.next = newNode
+
+    def insertBefore(self, newNode):
+        # Inserts newNode before this node
+        newNode.prev = self.prev
+        newNode.next = self
+        if self.prev:
+            self.prev.next = newNode
+        self.prev = newNode
+
+    def delete(self):
+        # Removes this node from the list, connecting previous and next nodes
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
 class AllOne:        
     def __init__(self):
-        self.head = ListNode(0)
-        self.tail = ListNode(float('inf'))
+        self.head = ListNode(0)  # Sentinel node for the head
+        self.tail = ListNode(float('inf'))  # Sentinel node for the tail
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.nodes = {}
-        
+        self.nodes = {}  # Maps keys to their corresponding ListNode
+
     def inc(self, key: str) -> None:
-        node = self.nodes.get(key, self.head)
-        if key in node.keys:
+        if key in self.nodes:
+            node = self.nodes[key]
             node.keys.remove(key)
+        else:
+            node = self.head
+
         if node.next.val != node.val + 1:
-            node.insertAfter(ListNode(node.val + 1))
-            
-        node = node.next
-        node.keys.add(key)
-        self.nodes[key] = node
+            newNode = ListNode(node.val + 1)
+            node.insertAfter(newNode)
+        else:
+            newNode = node.next
 
+        newNode.keys.add(key)
+        self.nodes[key] = newNode
 
-        if node.prev != self.head and len(node.prev.keys) == 0:
-            node.prev.delete()
+        if node != self.head and len(node.keys) == 0:
+            node.delete()
 
     def dec(self, key: str) -> None:
         if key not in self.nodes:
-            return  # Key does not exist, so we can't decrement it
+            return
 
         node = self.nodes[key]
         node.keys.remove(key)
 
         if node.val == 1:
-            # If after removal, the node is empty and it's not the head, delete it
             if len(node.keys) == 0:
                 node.delete()
-            # Remove the key from the nodes dictionary if its count is 1 and now has no keys
             del self.nodes[key]
             return
 
-        # If the previous node does not match the decremented value, create a new node with the decremented value
         if node.prev.val != node.val - 1:
             newNode = ListNode(node.val - 1)
             node.insertBefore(newNode)
@@ -44,56 +75,24 @@ class AllOne:
             newNode = node.prev
 
         newNode.keys.add(key)
-        self.nodes[key] = newNode  # Correctly update the reference to the new node
+        self.nodes[key] = newNode
 
-        # If the current node is empty after removing the key, delete it
         if len(node.keys) == 0:
             node.delete()
 
     def getMaxKey(self) -> str:
-        print(self.tail.prev.val)
-        return next(iter(self.tail.prev.keys)) if len(self.tail.prev.keys) else ""
+        if self.tail.prev != self.head:
+            return next(iter(self.tail.prev.keys), "")
+        return ""
 
     def getMinKey(self) -> str:
-        return next(iter(self.head.next.keys)) if len(self.head.next.keys) else ""
+        if self.head.next != self.tail:
+            return next(iter(self.head.next.keys), "")
+        return ""
 
-
-class ListNode:
-        def __init__(self, val):
-            self.keys = set()
-            self.prev = None
-            self.next = None
-            self.val = val
-        
-        def insertAfter(self, node):
-            nxt = self.next
-            
-            self.next = node
-            node.prev = self
-            
-            node.next = nxt
-            nxt.prev = node
-        
-        def insertBefore(self, node):
-            prev = self.prev
-            
-            prev.next = node
-            node.prev = prev
-            
-            node.next = self
-            self.prev = node
-            
-        def delete(self):
-            nxt, prev = self.next, self.prev
-            prev.next = nxt
-            nxt.prev = prev
-            
-        def getKeys(self):
-            return self.keys
-
-# Your AllOne object will be instantiated and called as such:
+# Example of how to use the AllOne object
 # obj = AllOne()
-# obj.inc(key)
-# obj.dec(key)
-# param_3 = obj.getMaxKey()
-# param_4 = obj.getMinKey()
+# obj.inc("key1")
+# obj.dec("key1")
+# maxKey = obj.getMaxKey()
+# minKey = obj.getMinKey()

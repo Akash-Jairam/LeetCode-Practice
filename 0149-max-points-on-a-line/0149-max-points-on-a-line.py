e859b6f54265cd9d5fef0957b0beaaae1e822e87
Akash Jairam <Quasar2197@gmail.com>
2024-03-15 06:20:47 -0500

    Time: 53 ms (97.12%), Space: 16.6 MB (86.13%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,21 +1,22 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        def helper(currentPoint, points):
-            slopes,duplicates,ans = {},0,0
-            x1, y1 = currentPoint
+        def helper(currPt, points):
+            x1, y1 = currPt
+            grad_map, ans, duplicates = {}, 0, 0
+            
             for x2, y2 in points:
-                # If the points are same inc duplicate counter
                 if x1 == x2 and y1 == y2:
                     duplicates += 1
-                # else find the slop and add in dic
                 else:
-                    slope = (x2 - x1) / (y2 - y1) if y2 != y1 else 'inf'
-                    count = slopes.get(slope, 0) + 1
-                    slopes[slope] = count
+                    grad = (y2 - y1) / (x2 - x1) if x2 - x1 != 0 else float('inf')
+                    count = 1 + grad_map.get(grad, 0)
+                    grad_map[grad] = count
                     ans = max(ans, count)
+            
             return ans + 1 + duplicates
-        ans = 0
+        res = 0
         while points:
-            currentPoint = points.pop()
-            ans = max(ans, helper(currentPoint, points))
-        return ans
+            currPt = points.pop()
+            res = max(res, helper(currPt, points))
+        
+        return res

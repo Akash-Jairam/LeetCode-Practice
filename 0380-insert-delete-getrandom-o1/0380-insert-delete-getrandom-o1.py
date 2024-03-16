80692d33752f277a582734563408c644ba7fa15e
Akash Jairam <Quasar2197@gmail.com>
2024-03-16 16:44:18 -0500

    Time: 230 ms (98.92%), Space: 64 MB (63.78%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,35 +1,44 @@
 import random
-
 class RandomizedSet:
 
     def __init__(self):
-        self.store = []  # To store the elements
-        self.store_s = {}  # To store the index of each element in the store list
+        self.store = []
+        self.i_map = {}
 
     def insert(self, val: int) -> bool:
-        if val not in self.store_s:
-            self.store.append(val)
-            self.store_s[val] = len(self.store) - 1
-            return True
-        return False
+        curr = self.i_map
+        if val in self.i_map:
+            return False
+        
+        self.store.append(val)
+        self.i_map[val] = len(self.store) - 1
+        return True
 
     def remove(self, val: int) -> bool:
-        if val in self.store_s:
-            idx = self.store_s[val]
-            last_element = self.store[-1]
+        curr = self.i_map
+        if val in self.i_map:
+            if len(self.store) > 1:
+                valIdx = self.i_map[val]
+                lastValue = self.store[-1]
+                self.i_map[lastValue] = valIdx
 
-            # Update the index of the last element in the store_s dictionary
-            self.store_s[last_element] = idx
-
-            # Swap the element to be removed with the last element and pop it
-            self.store[idx], self.store[-1] = self.store[-1], self.store[idx]
-            self.store.pop()
-
-            # Remove the entry for the removed element from store_s
-            del self.store_s[val]
-
+                self.store[valIdx], self.store[-1] = self.store[-1], self.store[valIdx]
+                self.store.pop()
+                del self.i_map[val]
+            else:
+                self.store = []
+                self.i_map = {}
             return True
+        
         return False
+        
 
     def getRandom(self) -> int:
         return random.choice(self.store)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()

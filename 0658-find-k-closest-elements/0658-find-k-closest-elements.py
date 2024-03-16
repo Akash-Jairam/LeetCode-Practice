79c5ed824d225b501c2836e3dff544330b217bce
Akash Jairam <Quasar2197@gmail.com>
2024-03-16 12:00:47 -0500

    Time: 206 ms (94.63%), Space: 18.3 MB (36.98%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,33 +1,12 @@
-from collections import deque
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        if len(arr) <= k:
-            return arr
+        l, r = 0, len(arr) - k
         
-        closest = 0
-        closestDiff = abs(x - arr[0])
+        while l < r:
+            mid = l + (r - l) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                l = mid + 1
+            else:
+                r = mid
         
-        for i in range(1, len(arr)):
-            diff = abs(x - arr[i])
-            if diff < closestDiff:
-                closestDiff = diff
-                closest = i
-        
-        l = closest - 1
-        r = closest
-        res = deque()
-        
-        while len(res) < k:
-            lVal = abs(arr[l] - x) if l >= 0 else float('inf')
-            rVal = abs(arr[r] - x) if r < len(arr) else float('inf')
-            
-            if lVal < rVal or lVal == rVal:
-                res.appendleft(arr[l])
-                l -= 1
-            elif rVal < lVal:
-                res.append(arr[r])
-                r += 1
-            
-        
-        return res
-            
+        return arr[l: l + k]

a5a77985d105167c9838c1a88f57dbb330b361b1
Akash Jairam <Quasar2197@gmail.com>
2024-03-17 12:27:35 -0500

    Time: 50 ms (13.73%), Space: 16.5 MB (55.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,41 +1,48 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
-        1. Find last peak
-        2. Pivot = last peak - 1
-        3. Find swap point aka index of the last number that is greater than the number at the pivot
-        4. Swap the numbers at the pivot and the swap point
-        5. Reverse all the numbers after the pivot
+        Do not return anything, modify nums in-place instead.
+        Find the last peak where the number is greater than the number before it
+        The pivot = last peak - 1
+        Find swap point which is index of the last number that is greater than the pivot
+        swap everything between pivot and swap point
         """
         lastPeak = self.findLastPeak(nums)
         pivot = lastPeak - 1
-        if pivot != -1:
+        swapPt = len(nums) - 1
+        if pivot > -1:
             swapPt = self.findSwapPt(nums, pivot)
-            nums[swapPt], nums[pivot] = nums[pivot], nums[swapPt]
-        self.reverse(nums, pivot + 1)
-        return nums
+            nums[pivot], nums[swapPt] = nums[swapPt], nums[pivot]
+        self.reverse(pivot + 1, nums)
+    
     
     def findLastPeak(self, nums):
         i = len(nums) - 1
         while i > 0:
-            if nums[i] > nums[i-1]:
+            if nums[i] > nums[i - 1]:
                 return i
+            
             i -= 1
+            
         return 0
-    
     
     def findSwapPt(self, nums, pivot):
         i = len(nums) - 1
+        
         while i >= 0:
             if nums[i] > nums[pivot]:
                 return i
+            
             i -= 1
         
-        return pivot + 1
+        return i
     
-    def reverse(self, nums, i):
-        j = len(nums) - 1
-        while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-            j -= 1
+    def reverse(self, l,  nums):
+        r = len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+    
+    
+    

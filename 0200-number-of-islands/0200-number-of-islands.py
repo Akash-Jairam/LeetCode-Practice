e95a47da165e04bf9d1ebf8c4e198fe03ad5a4fa
Akash Jairam <Quasar2197@gmail.com>
2024-03-17 08:43:28 -0500

    Time: 224 ms (91.46%), Space: 18.9 MB (62.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        def backtrack(row, col):
-            if not 0 <= row < m or not 0 <= col < n or grid[row][col] == '0':
+        def dfs(r, c):
+            if not 0 <= r < m or not 0 <= c < n or grid[r][c] == '0':
                 return
             
-            grid[row][col] = '0'
-            backtrack(row + 1, col)
-            backtrack(row - 1, col)
-            backtrack(row , col + 1)
-            backtrack(row , col - 1)
-            
+            grid[r][c] = '0'
+            for x, y in directions:
+                dfs(r +x, c + y)
+        
+        
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        m, n = len(grid), len(grid[0])
         res = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
                     res += 1
-                    backtrack(i, j)
+                    dfs(i, j)
         
         return res
-            

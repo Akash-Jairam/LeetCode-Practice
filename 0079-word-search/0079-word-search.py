8d3b01adeb41941255ed4cd87e566f40a00905a3
Akash Jairam <Quasar2197@gmail.com>
2024-04-02 20:40:07 -0500

    Time: 3040 ms (86.22%), Space: 16.8 MB (7.45%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0] and self.backtrack(i, j, board, 0, word):
+        m, n = len(board), len(board[0])
+        
+        def dfs(r, c, idx):
+            if not 0 <= r < m or not 0 <= c < n or board[r][c] == '*':
+                return False
+            
+            curr = board[r][c]
+            if curr != word[idx]:
+                return False
+            
+            idx += 1
+            if idx == len(word):
+                return True
+            board[r][c] = '*'
+            
+            found = dfs(r + 1, c, idx) or dfs(r - 1, c, idx) or dfs(r, c+1, idx) or dfs(r, c - 1, idx)
+            
+            board[r][c] = curr
+            return found
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0] and dfs(i, j, 0):
                     return True
-        
+                
         return False
-    
-    def backtrack(self, r, c, board, idx, word):
-        if not 0 <= r < len(board) or not 0 <= c < len(board[0]) or board[r][c] != word[idx]:
-            return False
         
-        idx += 1
-        if idx == len(word):
-            return True
         
-        curr = board[r][c]
-        board[r][c] = None
-        
-        res = self.backtrack(r + 1, c, board, idx, word) or self.backtrack(r - 1, c, board, idx, word) or self.backtrack(r , c + 1, board, idx, word) or self.backtrack(r , c - 1, board, idx, word)
-        board[r][c] = curr
-        return res

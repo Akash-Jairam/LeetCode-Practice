70f919ecf7f1ec74d22fa59ebc57b26108dc143c
Akash Jairam <Quasar2197@gmail.com>
2024-05-18 07:50:21 -0500

    Time: 104 ms (28.70%), Space: 18 MB (47.99%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -1,26 +1,24 @@
 class Solution:
     def largestPalindromic(self, num: str) -> str:
+        half = []
+        mid = ''
         n_map = [0] * 10
         
         for c in num:
             n_map[int(c)] += 1
         
-        odd = None        
-        i = 9
-        res = []
-        while i >= 0:
-            if n_map[i] > 0:
-                if i == 0 and len(res) == 0:
-                    break
-                
-                if n_map[i] % 2 == 1 and not odd:
-                    odd = i
-                numAppend = n_map[i] // 2
-                if numAppend:
-                    res.append(str(i) * numAppend)
-            i -= 1
-            
-            if odd == None:
-                odd = ''
-        return "0" if len(res) == 0 and not odd else ''.join(res) + str(odd) + ''.join(res[::-1])
+        idx = 9
         
+        while idx >= 0:
+            if not len(half) and idx == 0:
+                break
+            
+            if not mid and n_map[idx] % 2 == 1:
+                mid = str(idx)
+            
+            if n_map[idx] // 2 > 0:
+                half.append(str(idx) * (n_map[idx] // 2))
+        
+            idx -= 1
+        
+        return ''.join(half) + mid + ''.join(half[::-1]) if len(half) or mid else "0"

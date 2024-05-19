acaf1b36079a9e8ecaa0f5b096bdac914af32c66
Akash Jairam <Quasar2197@gmail.com>
2024-05-19 09:16:37 -0500

    Time: 40 ms (26.84%), Space: 16.5 MB (49.73%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,5 +1,11 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
+        # Use two variables to keep track of the min and max opening braces
+        # When we encounter an opening brace, we increase the variables
+        # When we encounter a closing brace, we decrease them
+        # When we encounter a *, we decrease the leftMin and increase the leftMax per their names
+        # If the leftMin < 0, we reset to 0 to account for the * being an empty string
+        # If the leftMax < 0, we return False because that means there are more closing braces than the max possible number of open braces
         leftMin = leftMax = 0
         
         for c in s:

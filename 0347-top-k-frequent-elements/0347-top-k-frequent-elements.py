d20c289e88a10bbae8872988674504ba5c62d627
Akash Jairam <Quasar2197@gmail.com>
2024-05-19 08:43:55 -0500

    Time: 95 ms (26.33%), Space: 21.1 MB (56.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,15 @@
-from collections import defaultdict
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n_map = defaultdict(int)
-        for n in nums:
-            n_map[n] += 1
-            
-        heap = []
+        n_map = {}
         
-        for num, v in n_map.items():
-            heapq.heappush(heap, (v, num))
-            
-            
-            while len(heap) > k:
-                heapq.heappop(heap)
-                
-        return [num for v, num in heap]
+        for n in nums:
+            n_map[n] = 1 + n_map.get(n, 0)
+        
+        min_heap = []
+        for key, v in n_map.items():
+            heapq.heappush(min_heap, (v, key))
+            while len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        return [t[1] for t in min_heap]
